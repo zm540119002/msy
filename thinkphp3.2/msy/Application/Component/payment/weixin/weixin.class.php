c3a6227d736885ba5319b3e98df064d4415664c3
yangguanbao <yangguanbao@qq.com>
2017-12-04 09:20:59 +0800

    ygb

--- thinkphp3.2/msy/Application/Component/payment/weixin/weixin.class.php
+++ thinkphp3.2/msy/Application/Component/payment/weixin/weixin.class.php
@@ -94,7 +94,7 @@ class weixin
 
 //		$go_url = MODULE + '/recharge/payComplete';
 
-		$tools = new \example\JsApiPay();
+		$tools = new example\JsApiPay();
 		var_dump($tools);exit;
 		$openId = $tools->GetOpenid();
 		var_dump($openId);exit;
