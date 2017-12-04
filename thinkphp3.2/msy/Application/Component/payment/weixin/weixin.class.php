04142f9989045af498f018eae51a1b99da71747f
webserver <webserver@qq.com>
2017-12-04 09:32:04 +0800

    zm

--- thinkphp3.2/msy/Application/Component/payment/weixin/weixin.class.php
+++ thinkphp3.2/msy/Application/Component/payment/weixin/weixin.class.php
@@ -94,7 +94,7 @@ class weixin
 
 //		$go_url = MODULE + '/recharge/payComplete';
 
-		$tools = new example\JsApiPay();
+		$tools = new \JsApiPay();
 		var_dump($tools);exit;
 		$openId = $tools->GetOpenid();
 		var_dump($openId);exit;
