785fcdc17d5f5e8c06c88bc07d3b38f2ecf3d79b
webserver <webserver@qq.com>
2017-12-07 14:38:29 +0800

    zm

--- thinkphp3.2/msy/Public/js/purchase/footerCart.js
+++ thinkphp3.2/msy/Public/js/purchase/footerCart.js
@@ -23,7 +23,7 @@ $(function () {
 
     //进入采购
     $('body').on('click','.go_to_purchase',function(){
-        var url = MODULE + '/Cart/jointPurchasing';
+        var url = MODULE + '/Cart/jointPurchase';
         url += '/category_id_1/'+category_id_1;
         url += '/category_id_2/' + $('.purchase_package_nav').find('li.current').data('category_id_2');
         location.href = url;
