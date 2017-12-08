832969e8d2bea6fb343a380ca10acd698e815a33
yangguanbao <yangguanbao@qq.com>
2017-12-08 10:16:54 +0800

    ygb

--- thinkphp3.2/msy/Application/Component/payment/weixin/weixin.class.php
+++ thinkphp3.2/msy/Application/Component/payment/weixin/weixin.class.php
@@ -148,7 +148,6 @@ class weixin
 //		$go_url = MODULE + '/recharge/payComplete';
 		$tools = new \JsApiPay();
 		$openId = $tools->GetOpenid();
-		var_dump($openId);exit;
 		$input = new \WxPayUnifiedOrder();
 		$input->SetBody('美尚云');					//商品名称
 		$input->SetAttach('weixin');					//附加参数,可填可不填,填写的话,里边字符串不能出现空格
