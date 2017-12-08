4a92238bcc9cd2a8e97055c0ccfb1a9c63cffd06
yangguanbao <yangguanbao@qq.com>
2017-12-08 13:46:42 +0800

    ygb

--- thinkphp3.2/msy/Application/Component/payment/weixin/example/WxPay.JsApiPay.php
+++ thinkphp3.2/msy/Application/Component/payment/weixin/example/WxPay.JsApiPay.php
@@ -43,14 +43,14 @@ class JsApiPay
 		//通过code获得openid
 		if (!isset($_GET['code'])){
 			//触发微信返回code码
-//			$baseUrl = urlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$_SERVER['QUERY_STRING']);
+//			$baseUrl = urlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].$_SERVER['QUERY_STRING']);
 			$baseUrl = urlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
 			$url = $this->__CreateOauthUrlForCode($baseUrl);
 			Header("Location: $url");
 			exit();
 		} else {
 			//获取code码，以获取openid
-			$code = $_GET['code'];
+		    $code = $_GET['code'];
 			$openid = $this->getOpenidFromMp($code);
 			return $openid;
 		}
