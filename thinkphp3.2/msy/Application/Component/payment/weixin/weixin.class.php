7524a96d014eee248edb1fd84ecabfa3891fa812
yangguanbao <yangguanbao@qq.com>
2017-12-08 11:58:35 +0800

    ygb

--- thinkphp3.2/msy/Application/Component/payment/weixin/weixin.class.php
+++ thinkphp3.2/msy/Application/Component/payment/weixin/weixin.class.php
@@ -137,6 +137,7 @@ class weixin
     }
 
     function getJSAPI($order){
+		var_dump(111);exit;
 //		if(stripos($order['order_sn'],'recharge') !== false){
 //			$go_url = U('Mobile/User/points',array('type'=>'recharge'));
 //			$back_url = U('Mobile/User/recharge',array('order_id'=>$order['order_id']));
