46105d1aea72fd7b56c7affb39af009f120febda
yangguanbao <yangguanbao@qq.com>
2017-12-08 17:20:11 +0800

    ygb

--- thinkphp3.2/msy/Application/Purchase/Controller/PaymentController.class.php
+++ thinkphp3.2/msy/Application/Purchase/Controller/PaymentController.class.php
@@ -25,9 +25,7 @@ class PaymentController extends Controller {
      */
     public function  __construct() {
         parent::__construct();
-
         // 订单支付提交
-
         if(IS_POST)
         {
             $this->pay_code = $_POST['pay_code']; // 支付 code
@@ -38,9 +36,6 @@ class PaymentController extends Controller {
             $this->pay_code = I('get.pay_code');
             unset($_GET['pay_code']); // 用完之后删除, 以免进入签名判断里面去 导致错误
         }
-        //获取通知的数据
-//        $xml = $GLOBALS['HTTP_RAW_POST_DATA'];
-
         // 导入具体的支付类文件
         if( $this->pay_code  == 'weixin'){
             $this->payment = new \Component\payment\weixin\weixin();
@@ -78,11 +73,10 @@ class PaymentController extends Controller {
     }
 
     /**
-     *  提交支付方式
+     *  其他支付提交支付方式
      */
     public function getCode2(){
         //  订单支付提交
-
         header("Content-type:text/html;charset=utf-8");
         $order = array(
             'sn' => generateSN(),
@@ -91,75 +85,15 @@ class PaymentController extends Controller {
         $code_str = $this->payment->get_code($order,$config_value='');
     }
 
+    // 服务器点对点 // http://www.a.cn/index.php/Home/Payment/notifyUrl
+    public function notifyUrl(){
+        $this->payment->response();
+        exit();
+    }
 
-//    public function getPay(){
-//    	//手机端在线充值
-//        //C('TOKEN_ON',false); // 关闭 TOKEN_ON
-//        header("Content-type:text/html;charset=utf-8");
-//        $order_id = I('order_id/d'); //订单id
-//        $user = session('user');
-//        $data['account'] = I('account');
-//        if($order_id>0){
-//        	M('recharge')->where(array('order_id'=>$order_id,'user_id'=>$user['user_id']))->save($data);
-//        }else{
-//        	$data['user_id'] = $user['user_id'];
-//        	$data['nickname'] = $user['nickname'];
-//        	$data['order_sn'] = 'recharge'.get_rand_str(10,0,1);
-//        	$data['ctime'] = time();
-//        	$order_id = M('recharge')->add($data);
-//        }
-//        if($order_id){
-//        	$order = M('recharge')->where("order_id", $order_id)->find();
-//        	if(is_array($order) && $order['pay_status']==0){
-//        		$order['order_amount'] = $order['account'];
-//        		$pay_radio = $_REQUEST['pay_radio'];
-//        		$config_value = get_url_param($pay_radio); // 类似于 pay_code=alipay&bank_code=CCB-DEBIT 参数
-//        		$payment_arr = M('Plugin')->where("`type` = 'payment'")->getField("code,name");
-//        		M('recharge')->where("order_id", $order_id)->save(array('pay_code'=>$pay_code,'pay_name'=>$payment_arr[$pay_code]));
-//        		//微信JS支付
-//        		if($pay_code == 'WeiXin' && $_SESSION['openid'] && strstr($_SERVER['HTTP_USER_AGENT'],'MicroMessenger')){
-//        			$code_str = $this->payment->getJSAPI($order);
-//        			exit($code_str);
-//        		}else{
-//        			$code_str = $this->payment->get_code($order,$config_value);
-//        		}
-//        	}else{
-//        		$this->error('此充值订单，已完成支付!');
-//        	}
-//        }else{
-//        	$this->error('提交失败,参数有误!');
-//        }
-//        $this->assign('code_str', $code_str);
-//        $this->assign('order_id', $order_id);
-//    	return $this->fetch('recharge'); //分跳转 和不 跳转
-//    }
-
-        // 服务器点对点 // http://www.a.cn/index.php/Home/Payment/notifyUrl
-        public function notifyUrl(){
-            $this->payment->response();            
-            exit();
-        }
-
-        // 页面跳转 // http://www.a.cn/index.php/Home/Payment/returnUrl
-        public function returnUrl(){
-            $this->redirect('Index/payComplete');
-//            $result = $this->payment->respond2(); // $result['order_sn'] = '201512241425288593';
-//            if(stripos($result['order_sn'],'recharge') !== false)
-//            {
-//            	$order = M('recharge')->where("order_sn", $result['order_sn'])->find();
-//            	$this->assign('order', $order);
-//            	if($result['status'] == 1)
-//            		return $this->fetch('recharge_success');
-//            	else
-//            		return $this->fetch('recharge_error');
-//            	exit();
-//            }
-//            $order = M('order')->where("order_sn", $result['order_sn'])->find();
-//            $this->assign('order', $order);
-//            if($result['status'] == 1)
-//                return $this->fetch('success');
-//            else
-//                return $this->fetch('error');
+    // 页面跳转 // http://www.a.cn/index.php/Home/Payment/returnUrl
+    public function returnUrl(){
+        $this->redirect('Index/payComplete');
         }
 
     //退款
