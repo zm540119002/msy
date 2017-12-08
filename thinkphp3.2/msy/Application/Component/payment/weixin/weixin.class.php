c4b56f32b545e3f9135d34291359dd9a9251c24c
yangguanbao <yangguanbao@qq.com>
2017-12-08 17:02:12 +0800

    ygb

--- thinkphp3.2/msy/Application/Component/payment/weixin/weixin.class.php
+++ thinkphp3.2/msy/Application/Component/payment/weixin/weixin.class.php
@@ -18,8 +18,9 @@ namespace Component\payment\weixin;
  * Class 
  * @package Home\Payment
  */
-
 use Vendor\Qrcode\Qrcode;
+
+
 class weixin
 {
 	/**
@@ -34,7 +35,6 @@ class weixin
 		$config_value = unserialize($paymentPlugin['config_value']); // 配置反序列化
 		\WxPayConfig::$appid = $config_value['appid']; // * APPID：绑定支付的APPID（必须配置，开户邮件中可查看）
 		\WxPayConfig::$mchid = $config_value['mchid']; // * MCHID：商户号（必须配置，开户邮件中可查看）
-//		\WxPayConfig::$smchid = isset($config_value['smchid']) ? $config_value['smchid'] : ''; // * SMCHID：服务商商户号（必须配置，开户邮件中可查看）
 		\WxPayConfig::$key = $config_value['key']; // KEY：商户支付密钥，参考开户邮件设置（必须配置，登录商户平台自行设置）
 		\WxPayConfig::$appsecret = $config_value['appsecret']; // 公众帐号secert（仅JSAPI支付的时候需要配置)，
 	}
@@ -48,8 +48,6 @@ class weixin
 	function pc_pay($order, $config_value)
 	{
 		$notify_url = SITE_URL.'/index.php/Home/Payment/notifyUrl/pay_code/weixin'; // 接收微信支付异步通知回调地址，通知url必须为直接可访问的url，不能携带参数。
-		//$notify_url = C('site_url').U('Home/Payment/notifyUrl',array('pay_code'=>'weixin')); // 接收微信支付异步通知回调地址，通知url必须为直接可访问的url，不能携带参数。
-		//$notify_url = C('site_url')."/index.php?m=Home&c=Payment&a=notifyUrl&pay_code=weixin";
 		$input = new \WxPayUnifiedOrder();
 		$input->SetBody("美尚云"); // 商品描述
 		$input->SetAttach("weixin"); // 附加数据，在查询API和支付通知中原样返回，该字段主要用于商户携带订单的自定义数据
@@ -60,31 +58,22 @@ class weixin
 		$input->SetProduct_id("123456789"); // 商品ID trade_type=NATIVE，此参数必传。此id为二维码中包含的商品ID，商户自行定义。
 		$notify = new \NativePay();
 		$result = $notify->GetPayUrl($input); // 获取生成二维码的地址
-
 		$url2 = $result["code_url"];
 		$this->payQRcode($url2);
-//		return '<img alt="模式二扫码支付" src="/index.php?m=Home&c=Index&a=qr_code&data='.urlencode($url2).'" style="width:110px;height:110px;"/>';
 	}
 
 	//生成支付二维码
 	public function payQRcode($url){
-
 		//生成二维码图片
 		$object = new Qrcode();
 		$qrcodePath = WEB_URL.'Public/images/qrcode/';//保存文件路径
 		$fileName = time().'.png';//保存文件名
-
 		$outFile = $qrcodePath.$fileName;
 		$level = 'L'; //容错级别
 		$size = 10; //生成图片大小
 		$frameSize = 2; //边框像素
 		$saveAndPrint = true;
 		$object->png($url, $outFile, $level, $size, $frameSize,$saveAndPrint);
-
-		exit;
-		$data = base64_encode(file_get_contents($outFile));
-		echo '<img src="data:image/png;base64,'.$data.'" />';
-		exit;
 	}
 
 	function h5_pay($order){
@@ -103,17 +92,6 @@ class weixin
 		$order2 = \WxPayApi::unifiedOrder($input);	//统一下单
 		$url = $order2['mweb_url'];
 		return $url;
-//		$html = <<<EOF
-//		<script type="text/javascript">
-//         	<script type="text/javascript" src="/Public/js/common/jquery-1.9.1.min.js"></script>
-//         	function h5pay(){
-//         		location.href = $url;
-//         	}
-//         	h5pay();
-//        </script>
-//EOF;
-
-		//return $html;
 
 	}
 
@@ -137,15 +115,6 @@ class weixin
     }
 
     function getJSAPI($order){
-//		if(stripos($order['order_sn'],'recharge') !== false){
-//			$go_url = U('Mobile/User/points',array('type'=>'recharge'));
-//			$back_url = U('Mobile/User/recharge',array('order_id'=>$order['order_id']));
-//		}else{
-//			$go_url = U('Mobile/User/order_detail',array('id'=>$order['order_id']));
-//			$back_url = U('Mobile/Cart/cart4',array('order_id'=>$order['order_id']));
-//		}
-
-//		$go_url = MODULE + '/recharge/payComplete';
 		$tools = new \JsApiPay();
 		$openId = $tools->GetOpenid();
 		$input = new \WxPayUnifiedOrder();
@@ -212,7 +181,7 @@ EOF;
     			'mch_appid' => $wxchat['appid'],
     			'mchid'     => $wxchat['smchid'],
     			'nonce_str' => md5(time()),
-    			//'device_info' => '1000',
+
     			'partner_trade_no'=> $data['pay_code'], //商户订单号，需要唯一
     			'openid' => $data['openid'],//转账用户的openid
     			'check_name'=> 'NO_CHECK', //OPTION_CHECK不强制校验真实姓名, FORCE_CHECK：强制 NO_CHECK：
