516e525da0f1ed1ca5622345fd8ac839830be16e
yangguanbao <yangguanbao@qq.com>
2017-12-08 14:10:43 +0800

    ygb

--- thinkphp3.2/msy/Application/Purchase/Controller/PaymentController.class.php
+++ thinkphp3.2/msy/Application/Purchase/Controller/PaymentController.class.php
@@ -79,7 +79,9 @@ class PaymentController extends Controller {
 //                    $code_str = $this->payment->get_code($order, $config_value = '');
 //                }
 //            }
-
+        $this->payment = new \Component\payment\weixin\weixin();
+        $code_str = $this->payment->getJSAPI($order);
+        exit($code_str);
         if ($this->pay_code == 'weixin' && strstr($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger')) {
                     $this->payment = new \Component\payment\weixin\weixin();
                     $code_str = $this->payment->getJSAPI($order);
