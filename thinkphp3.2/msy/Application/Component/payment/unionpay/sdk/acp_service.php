793f11a8407986051349ad2316d476babc4a1d65
yangguanbao <yangguanbao@qq.com>
2017-12-09 10:31:38 +0800

    ygb

--- thinkphp3.2/msy/Application/Component/payment/unionpay/sdk/acp_service.php
+++ thinkphp3.2/msy/Application/Component/payment/unionpay/sdk/acp_service.php
@@ -17,10 +17,10 @@ class AcpService {
 	 */
 	static function sign(&$params, $cert_path=SDK_SIGN_CERT_PATH, $cert_pwd=SDK_SIGN_CERT_PWD) {
 		$params ['certId'] = getSignCertId ($cert_path, $cert_pwd); //证书ID
-		
+
 		sign($params, $cert_path, $cert_pwd);
 	}
-	
+
 	/**
 	 * 验签
 	 * @param $params 应答数组
@@ -29,7 +29,7 @@ class AcpService {
 	static function validate($params) {
 		return verify($params);
 	}
-	
+
 	/**
 	 * 对控件支付成功返回的结果信息中data域进行验签
 	 * @param $jsonData json格式数据，例如：{"sign" : "J6rPLClQ64szrdXCOtV1ccOMzUmpiOKllp9cseBuRqJ71pBKPPkZ1FallzW18gyP7CvKh1RxfNNJ66AyXNMFJi1OSOsteAAFjF5GZp0Xsfm3LeHaN3j/N7p86k3B1GrSPvSnSw1LqnYuIBmebBkC1OD0Qi7qaYUJosyA1E8Ld8oGRZT5RR2gLGBoiAVraDiz9sci5zwQcLtmfpT5KFk/eTy4+W9SsC0M/2sVj43R9ePENlEvF8UpmZBqakyg5FO8+JMBz3kZ4fwnutI5pWPdYIWdVrloBpOa+N4pzhVRKD4eWJ0CoiD+joMS7+C0aPIEymYFLBNYQCjM0KV7N726LA==",  "data" : "pay_result=success&tn=201602141008032671528&cert_id=68759585097"}
@@ -45,25 +45,25 @@ class AcpService {
 		$signature = base64_decode ( $sign );
 		$params_sha1x16 = sha1 ( $data, FALSE );
 		$isSuccess = openssl_verify ( $params_sha1x16, $signature,$public_key, OPENSSL_ALGO_SHA1 );
-		
+
 		return $isSuccess;
 	}
-	
+
 	/**
 	 * 后台交易 HttpClient通信
 	 *
-	 * @param unknown_type $params        	
-	 * @param unknown_type $url        	
+	 * @param unknown_type $params
+	 * @param unknown_type $url
 	 * @return mixed
 	 */
 	 static function post($params, $url) {
 
 	 	global $log;
-	 	
+
 		$opts = createLinkString ( $params, false, true );
  	    $log->LogInfo ( "后台请求地址为>" . $url );
  	    $log->LogInfo ( "后台请求报文为>" . $opts );
- 	    
+
 		$ch = curl_init ();
 		curl_setopt ( $ch, CURLOPT_URL, $url );
 		curl_setopt ( $ch, CURLOPT_POST, 1 );
@@ -71,13 +71,13 @@ class AcpService {
 		curl_setopt ( $ch, CURLOPT_SSL_VERIFYHOST, false ); // 不验证HOST
 		curl_setopt ( $ch, CURLOPT_SSLVERSION, 1 ); // http://php.net/manual/en/function.curl-setopt.php页面搜CURL_SSLVERSION_TLSv1
 		curl_setopt ( $ch, CURLOPT_HTTPHEADER, array (
-				'Content-type:application/x-www-form-urlencoded;charset=UTF-8' 
+				'Content-type:application/x-www-form-urlencoded;charset=UTF-8'
 		) );
 		curl_setopt ( $ch, CURLOPT_POSTFIELDS, $opts );
 		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
 		$html = curl_exec ( $ch );
 		$log->LogInfo ( "后台返回结果为>" . $html );
-		
+
 		if(curl_errno($ch)){
 			$errmsg = curl_error($ch);
 			curl_close ( $ch );
@@ -94,7 +94,7 @@ class AcpService {
 		$result_arr = convertStringToArray ( $html );
 		return $result_arr;
 	}
-	
+
 	/**
 	 * 后台交易 HttpClient通信
 	 *
@@ -105,11 +105,11 @@ class AcpService {
 	static function get($params, $url) {
 
 		global $log;
-		
+
 		$opts = createLinkString ( $params, false, true );
 		$log->LogDebug( "后台请求地址为>" . $url ); //get的日志太多而且没啥用，设debug级别
 		$log->LogDebug ( "后台请求报文为>" . $opts );
-		
+
 		$ch = curl_init ();
 		curl_setopt ( $ch, CURLOPT_URL, $url );
 		curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, false ); // 不验证证书
@@ -137,7 +137,7 @@ class AcpService {
 		curl_close ( $ch );
 		return $html;
 	}
-	
+
 	static function createAutoFormHtml($params, $reqUrl) {
 		// <body onload="javascript:document.pay_form.submit();">
 		$encodeType = isset ( $params ['encoding'] ) ? $params ['encoding'] : 'UTF-8';
@@ -163,15 +163,15 @@ eot;
 		$log->LogInfo ( "自动跳转html>" . $html );
 		return $html;
 	}
-	
-	
-	
+
+
+
 	static function getCustomerInfo($customerInfo) {
 	  if($customerInfo == null || count($customerInfo) == 0 )
 	  	return "";
 		return base64_encode ( "{" . createLinkString ( $customerInfo, false, false ) . "}" );
 	}
-	
+
 	/**
 	 * map转换string，按新规范加密
 	 *
@@ -196,8 +196,8 @@ eot;
 		}
 		return base64_encode ( "{" . createLinkString ( $customerInfo, false, false ) . "}" );
 	}
-	
-	
+
+
 	/**
 	 * 解析customerInfo。
 	 * 为方便处理，encryptedInfo下面的信息也均转换为customerInfo子域一样方式处理，
@@ -219,12 +219,12 @@ eot;
 		}
 		return $customerInfo;
 	}
-	
-	
+
+
 	static function getEncryptCertId() {
 		return getCertIdByCerPath ( SDK_ENCRYPT_CERT_PATH );
 	}
-	
+
 	/**
 	 * 加密数据
 	 * @param string $data数据
@@ -236,7 +236,7 @@ eot;
 		openssl_public_encrypt ( $data, $crypted, $public_key );
 		return base64_encode ( $crypted );
 	}
-	
+
 	/**
 	 * 解密数据
 	 * @param string $data数据
@@ -249,8 +249,8 @@ eot;
 		openssl_private_decrypt ( $data, $crypted, $private_key );
 		return $crypted;
 	}
-	
-	
+
+
 	/**
 	 * 处理报文中的文件
 	 *
@@ -261,7 +261,7 @@ eot;
 		if (isset ( $params ['fileContent'] )) {
 			$log->LogInfo ( "---------处理后台报文返回的文件---------" );
 			$fileContent = $params ['fileContent'];
-	
+
 			if (empty ( $fileContent )) {
 				$log->LogInfo ( '文件内容为空' );
 				return false;
@@ -290,16 +290,16 @@ eot;
 			return false;
 		}
 	}
-	
-	
+
+
 	static function enCodeFileContent($path){
-	
+
 		$file_content_base64 = '';
 		if(!file_exists($path)){
 			echo '文件没找到';
 			return false;
 		}
-	
+
 		$file_content = file_get_contents ( $path );
 		//UTF8 去掉文本中的 bom头
 		$BOM = chr(239).chr(187).chr(191);
