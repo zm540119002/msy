d4b26c0189472de69c80c99e3b9a97300fe921b7
zhangmin <540119002@qq.com>
2017-12-12 11:27:42 +0800

    zm

--- thinkphp3.2/msh/Application/Common/Common/function.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once(APP_PATH . '/Common/Common/ext_function.php');
-require_once(WEB_PATH . '../web/all/Common/page.php');
-require_once(WEB_PATH . '../web/all/Common/function.php');
