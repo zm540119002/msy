16c48234355f8767b6a57613f3f424428498dd71
yangguanbao <yangguanbao@qq.com>
2017-12-13 15:58:09 +0800

    ygb

--- thinkphp3.2/web/all/Component/payment/unionpay/sdk/acp_service.php
+++ thinkphp3.2/web/all/Component/payment/unionpay/sdk/acp_service.php
@@ -138,6 +138,7 @@ class AcpService {
 	}
 	
 	static function createAutoFormHtml($params, $reqUrl) {
+		echo ($reqUrl);
 
 		// <body onload="javascript:document.pay_form.submit();">
 		$encodeType = isset ( $params ['encoding'] ) ? $params ['encoding'] : 'UTF-8';
