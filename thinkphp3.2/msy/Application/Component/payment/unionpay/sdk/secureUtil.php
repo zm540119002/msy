9202febabbd14b33531e31d4cf0453d35418f121
yangguanbao <yangguanbao@qq.com>
2017-12-14 11:57:36 +0800

    ygb

--- thinkphp3.2/msy/Application/Component/payment/unionpay/sdk/secureUtil.php
+++ thinkphp3.2/msy/Application/Component/payment/unionpay/sdk/secureUtil.php
@@ -103,9 +103,9 @@ function getSignCertId($cert_path, $cert_pwd) {
 	$pkcs12certdata = file_get_contents ( $cert_path );
 	openssl_pkcs12_read ( $pkcs12certdata, $certs, $cert_pwd );
 	$x509data = $certs ['cert'];
-	print_r($x509data);exit;
 	openssl_x509_read ( $x509data );
 	$certdata = openssl_x509_parse ( $x509data );
+	print_r($certdata);exit;
 	$cert_id = $certdata ['serialNumber'];
 
 	return $cert_id;
