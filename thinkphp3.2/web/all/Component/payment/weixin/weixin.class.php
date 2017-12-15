3a0eacd8b1139a469219d5049249bb98bb055ab8
yangguanbao <yangguanbao@qq.com>
2017-12-15 18:14:23 +0800

    ygb

--- thinkphp3.2/web/all/Component/payment/weixin/weixin.class.php
+++ thinkphp3.2/web/all/Component/payment/weixin/weixin.class.php
@@ -116,14 +116,13 @@ EOF;
     }
 
     function getJSAPI($order_sn,$actually_amount,$notify_url){
-		echo $order_sn;exit;
 		//echo $order_sn;exit;
 		$tools = new \JsApiPay();
 		$openId = $tools->GetOpenid();
 		$input = new \WxPayUnifiedOrder();
 		$input->SetBody('美尚云');					//商品名称
 		$input->SetAttach('weixin');					//附加参数,可填可不填,填写的话,里边字符串不能出现空格
-		$input->SetOut_trade_no($order_sn);			//订单号
+		$input->SetOut_trade_no(201712151814202759638246);			//订单号
 		$input->SetTotal_fee($actually_amount*100);			//支付金额,单位:分
 		$input->SetTime_start(date("YmdHis"));		//支付发起时间
 		$input->SetTime_expire(date("YmdHis", time() + 600));//支付超时
