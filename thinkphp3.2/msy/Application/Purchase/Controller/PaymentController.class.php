57156369971485f2b4dbd171aa859255a5868ff5
yangguanbao <yangguanbao@qq.com>
2017-12-15 17:49:13 +0800

    ygb

--- thinkphp3.2/msy/Application/Purchase/Controller/PaymentController.class.php
+++ thinkphp3.2/msy/Application/Purchase/Controller/PaymentController.class.php
@@ -63,13 +63,13 @@ class PaymentController extends AuthCompanyAuthoriseController {
 //            'actually_amount' => 0.01
 //        );
         $order = $this->getOrderInfoByOrderType();
-       
         if (!isPhoneSide()) {//pc端微信扫码支付
             $code_str = $this->payment->pc_pay($order);
         }elseif(strpos($_SERVER['HTTP_USER_AGENT'],'MicroMessenger') == false && $this->pay_code == 'weixin'){//手机端非微信浏览器
             $code_str = $this->payment->h5_pay($order);
         }else{//微信浏览器
             $this->payment = new \web\all\Component\payment\weixin\weixin();
+            print_r($order);
             $code_str = $this->payment->getJSAPI($order);
         }
 
