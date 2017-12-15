c9a375da3b6eb2cdf8d21e8343722f2b08c0b7da
yangguanbao <yangguanbao@qq.com>
2017-12-15 19:02:28 +0800

    ygb

--- thinkphp3.2/web/all/Component/payment/weixin/weixin.class.php
+++ thinkphp3.2/web/all/Component/payment/weixin/weixin.class.php
@@ -113,18 +113,19 @@ EOF;
         // 微信扫码支付这里没有页面返回
     }
 
-    function getJSAPI($order1){
+    function getJSAPI($order){
+		var_dump($order);exit;
 		$tools = new \JsApiPay();
 		$openId = $tools->GetOpenid();
 		$input = new \WxPayUnifiedOrder();
 		$input->SetBody('美尚云');					//商品名称
 		$input->SetAttach('weixin');					//附加参数,可填可不填,填写的话,里边字符串不能出现空格
-		$input->SetOut_trade_no($order1['sn']);			//订单号
-		$input->SetTotal_fee($order1['actually_amount'] *100);			//支付金额,单位:分
+		$input->SetOut_trade_no($order['sn']);			//订单号
+		$input->SetTotal_fee($order['actually_amount'] *100);			//支付金额,单位:分
 		$input->SetTime_start(date("YmdHis"));		//支付发起时间
 		$input->SetTime_expire(date("YmdHis", time() + 600));//支付超时
 		$input->SetGoods_tag("test3");
-		$input->SetNotify_url($order1['notify_url']);//支付回调验证地址
+		$input->SetNotify_url($order['notify_url']);//支付回调验证地址
 		$input->SetTrade_type("JSAPI");				//支付类型
 		$input->SetOpenid($openId);					//用户openID
 		$order2 = \WxPayApi::unifiedOrder($input);	//统一下单
