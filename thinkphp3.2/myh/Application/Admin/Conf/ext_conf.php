f298828cec4ca98825aedc814af90751ac37238e
yangguanbao <yangguanbao@qq.com>
2017-12-20 11:09:43 +0800

    ygb

--- thinkphp3.2/myh/Application/Admin/Conf/ext_conf.php
+++ thinkphp3.2/myh/Application/Admin/Conf/ext_conf.php
@@ -8,7 +8,7 @@ return array(
     'PURCHASER_LEVEL_IMG' => 'purchase/purchaser-level/',//采购商级别图
     'ON_OFF_LINE' => array('保留','已上架','已下架'),
 
-    'MENU' => @include_once(APP_PATH . 'Admin/Conf/menu_conf.php'),
     'ARR' => @include_once(APP_PATH . 'Admin/Conf/array_conf.php'),
-    'MENU1' => @include_once(APP_PATH . 'Admin/Conf/menu1_conf.php'),
+    'MENU' => @include_once(APP_PATH . 'Admin/Conf/menu_conf.php'),
+    'BUY_TYPE' => @include_once(APP_PATH . 'Admin/Conf/buy_type_conf.php'),
 );
