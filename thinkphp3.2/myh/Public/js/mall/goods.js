89f2a4ba38f1d146a4c1171d433fdd5787241398
webserver <webserver@qq.com>
2017-12-20 09:11:20 +0800

    zm

---
+++ thinkphp3.2/myh/Public/js/mall/goods.js
@@ -0,0 +1,38 @@
+var currentPage = 1;//记录当前页
+var requestEnd = false;
+//获取分类商品-图形形式-列表
+function getGoodsList(config) {
+    var postData = $.extend({},config);
+    postData.p = currentPage?currentPage:1;
+    postData.pageSize = postData.pageSize?postData.pageSize:2;
+    postData.templateType = postData.templateType?postData.templateType:'list';
+    //请求结束标志
+    if(requestEnd){
+        dialog.error('没有更多啦');
+        return false;
+    }
+    $.ajax({
+        url: MODULE + '/Goods/goodsList',
+        data: postData,
+        type: 'get',
+        beforeSend: function(){
+            $('.loading').show();
+        },
+        error:function (xhr) {
+            $('.loading').hide();
+            dialog.error('AJAX错误');
+        },
+        success: function(data){
+            $('.loading').hide();
+            if(currentPage == 1){
+                $('ul.goodsListContent').empty().html(data);
+            }else{
+                $('ul.goodsListContent').append(data);
+            }
+            if($(data).length<postData.pageSize){
+                requestEnd = true;
+            }
+            currentPage ++;
+        }
+    });
+}
