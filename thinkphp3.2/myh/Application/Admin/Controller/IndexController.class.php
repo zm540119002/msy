51dcf54f031e54bb47ae68da97d6dc9aa54d8965
yangguanbao <yangguanbao@qq.com>
2017-12-25 15:23:58 +0800

    ygb

--- thinkphp3.2/myh/Application/Admin/Controller/IndexController.class.php
+++ thinkphp3.2/myh/Application/Admin/Controller/IndexController.class.php
@@ -2,7 +2,7 @@
 namespace Admin\Controller;
 use Think\Controller;
 use Admin\Controller\BaseAuthUserController;
-class IndexController extends BaseAuthUserController {
+class IndexController extends Controller {
     //后台首页
     public function index(){
         $this->display();
