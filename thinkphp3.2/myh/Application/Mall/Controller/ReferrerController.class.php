0f2097e9eaf30ecdfb0fefbf68301654237bd771
yangguanbao <yangguanbao@qq.com>
2017-12-26 11:08:58 +0800

    ygb

--- thinkphp3.2/myh/Application/Mall/Controller/ReferrerController.class.php
+++ thinkphp3.2/myh/Application/Mall/Controller/ReferrerController.class.php
@@ -18,14 +18,19 @@ class ReferrerController extends BaseController{
         $url = $_POST['url'];
         $url = substr($url,0,strrpos($url,'/footerType/share.html'));
         $url = $url.'/userId/'.$userId;
-        $shareQRCodes = createQRcode($url);
+        $logPath =  realpath('Public/img/home') . '/';
+        $logo = $logPath.'logo.png';
+        $newRelativePath = C('USER_LOGO');
+        $shareQRCodes = createLogoQRcode($url,$logo,$newRelativePath,$eclevel = "H", $pixelPerPoint = 8);
         $this->ajaxReturn($shareQRCodes);
     }
     public function aa(){
-        
-        $shareQRCodes = createLogoQRcode($url,$logo,$filename,$eclevel = "H", $pixelPerPoint = 8);
-
-       
+        $url = 'https://www.baidu.com';
+        $newRelativePath = C('USER_LOGO');
+        $logPath =  realpath('Public/img/home') . '/';
+        $logo = $logPath.'logo.png';
+        $shareQRCodes = createLogoQRcode($url,$logo,$newRelativePath,$eclevel = "H", $pixelPerPoint = 8);
+        echo $shareQRCodes;
     }
 
 }
