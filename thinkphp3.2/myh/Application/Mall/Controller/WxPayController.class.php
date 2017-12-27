eb3c18567b5f0024993195b969c81aa312ff5f9d
yangguanbao <yangguanbao@qq.com>
2017-12-27 15:26:43 +0800

    ygb

--- thinkphp3.2/myh/Application/Mall/Controller/WxPayController.class.php
+++ thinkphp3.2/myh/Application/Mall/Controller/WxPayController.class.php
@@ -8,7 +8,7 @@ class WxPayController extends AuthUserController {
     /**
      * 微信支付
      */
-    public function wxPay($payInfo){
+    public function wxPay1($payInfo){
         Pay::wxPay($payInfo);
     }
     
@@ -37,7 +37,7 @@ class WxPayController extends AuthUserController {
                     'actually_amount'=>$orderInfo['actually_amount'],
                     'notify_url'=>C('WX_CONFIG')['CALL_BACK_URL_ORDER'],
                 );
-                $this->wxPay($payInfo);
+                $this->wxPay1($payInfo);
                 //Pay::wxPay($payInfo);
             }
         }
