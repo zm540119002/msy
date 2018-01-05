3230df9237faec174beb1d03f74860963dfd468d
zhangmin <540119002@qq.com>
2018-01-05 13:44:17 +0800

    zm

--- thinkphp3.2/myh/Application/Mall/Controller/GoodsController.class.php
+++ thinkphp3.2/myh/Application/Mall/Controller/GoodsController.class.php
@@ -1,6 +1,7 @@
 <?php
 namespace Mall\Controller;
 use  web\all\Controller\BaseController;
+use  web\all\Lib\AuthUser;
 class GoodsController extends BaseController {
     //商品信息
     public function goodsInfo(){
