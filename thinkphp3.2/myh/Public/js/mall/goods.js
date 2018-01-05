fccf3581886accacae99e254282bc12144fbd9fd
zhuguangfu <zhuguangfu@qq.com>
2018-01-05 12:05:44 +0800

    zgf

--- thinkphp3.2/myh/Public/js/mall/goods.js
+++ thinkphp3.2/myh/Public/js/mall/goods.js
@@ -26,7 +26,6 @@ function getGoodsList(config) {
         },
         success: function(data){
             finished=true;
-            
             $('.loading').hide();
             if(currentPage == 1){
                 $('ul.goodsListContent').empty().append(data);
@@ -52,13 +51,9 @@ function getGoodsList(config) {
 //初始化
 getGoodsList(config);
 //上拉加载更多
-
-// function loadGoods(config){
 $(window).on('scroll',function(){
-    console.log(finished);
     if(finished && $(document).scrollTop()+$(window).height()>=$(document).height()){
         finished=false;
         getGoodsList(config); 
     }
 });
-// }
