cbca617585cf0e3cbe52ba3c334947391c0d86bb
zhuguangfu <zhuguangfu@qq.com>
2018-01-08 16:17:08 +0800

    zgf

--- thinkphp3.2/myh/Public/js/mall/goods.js
+++ thinkphp3.2/myh/Public/js/mall/goods.js
@@ -37,9 +37,41 @@ function getGoodsList(config) {
     });
 }
 //上拉加载更多
-$(window).on('scroll',function(){
-    if($(document).scrollTop()+$(window).height()>=$(document).height()){
+// $(window).on('scroll',function(){
+//     if($(document).scrollTop()+$(window).height()>=$(document).height()){
+//         // alert(1);
+//         getGoodsList(config);
+//     }
+// });
+
+// 简单的节流函数
+function throttle(func, wait, mustRun) {
+	var timeout,
+		startTime = new Date();
+ 
+	return function() {
+		var context = this,
+			args = arguments,
+			curTime = new Date();
+ 
+		clearTimeout(timeout);
+		// 如果达到了规定的触发时间间隔，触发 handler
+		if(curTime - startTime >= mustRun){
+			func.apply(context,args);
+			startTime = curTime;
+		// 没达到触发间隔，重新设定定时器
+		}else{
+			timeout = setTimeout(func, wait);
+		}
+	};
+};
+// 实际想绑定在 scroll 事件上的 handler
+function realFunc(){
+	console.log("Success");
+     if($(document).scrollTop()+$(window).height()>=$(document).height()){
         // alert(1);
         getGoodsList(config);
     }
-});
+}
+// 采用了节流函数
+window.addEventListener('scroll',throttle(realFunc,500,1000));
