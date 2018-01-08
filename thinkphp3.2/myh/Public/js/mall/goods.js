d0eb4e51c6c85fcfc9be3107af78d0b15b88fd78
zhuguangfu <zhuguangfu@qq.com>
2018-01-08 16:27:53 +0800

    zgf

--- thinkphp3.2/myh/Public/js/mall/goods.js
+++ thinkphp3.2/myh/Public/js/mall/goods.js
@@ -44,34 +44,23 @@ function getGoodsList(config) {
 //     }
 // });
 
-// 简单的节流函数
-function throttle(func, wait, mustRun) {
-	var timeout,
-		startTime = new Date();
+var ticking = false; // rAF 触发锁
  
-	return function() {
-		var context = this,
-			args = arguments,
-			curTime = new Date();
+function onScroll(){
+  if(!ticking) {
+    requestAnimationFrame(realFunc);
+    ticking = true;
+  }
+}
  
-		clearTimeout(timeout);
-		// 如果达到了规定的触发时间间隔，触发 handler
-		if(curTime - startTime >= mustRun){
-			func.apply(context,args);
-			startTime = curTime;
-		// 没达到触发间隔，重新设定定时器
-		}else{
-			timeout = setTimeout(func, wait);
-		}
-	};
-};
-// 实际想绑定在 scroll 事件上的 handler
 function realFunc(){
-	console.log("Success");
-     if($(document).scrollTop()+$(window).height()>=$(document).height()){
+	// do something...
+ if($(document).scrollTop()+$(window).height()>=$(document).height()){
         // alert(1);
         getGoodsList(config);
     }
+	console.log("Success");
+	ticking = false;
 }
-// 采用了节流函数
-window.addEventListener('scroll',throttle(realFunc,500,1000));
+// 滚动事件监听
+window.addEventListener('scroll', onScroll, false);
