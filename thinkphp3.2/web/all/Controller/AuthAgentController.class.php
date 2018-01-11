4789c197ef429671d5931ed5dd639d3c294c21c4
zhangmin <540119002@qq.com>
2018-01-11 14:44:54 +0800

    zm

--- thinkphp3.2/web/all/Controller/AuthAgentController.class.php
+++ thinkphp3.2/web/all/Controller/AuthAgentController.class.php
@@ -13,9 +13,9 @@ class AuthAgentController extends AuthUserController{
         parent::__construct();
         AgentCache::remove($this->user['id']);
         $this->agent = AgentCache::get($this->user['id']);
-        //机构登记
-        if(!$this->agent || $this->agent['auth_status'] == 0){
-            $this->error(C('ERROR_COMPANY_REGISTER_REMIND'),U($this->agentAuthoriseUrl));
+        //代理商认证
+        if(!$this->agent || $this->agent['auth_status'] != 1){
+            $this->error('您还不是代理商！',U($this->agentAuthoriseUrl));
         }
     }
 }
--- thinkphp3.2/web/all/Controller/AuthPartnerController.class.php
+++ thinkphp3.2/web/all/Controller/AuthPartnerController.class.php
@@ -1,19 +1,19 @@
 <?php
 namespace web\all\Controller;
 
-use web\all\Cache\AgentCache;
+use web\all\Cache\PartnerCache;
 
 class AuthPartnerController extends AuthUserController{
     protected $partner = null;
-    protected $partnerAuthoriseUrl = 'Home/PartnerAuthorise/index';//机构登记URL
+    protected $partnerAuthoriseUrl = 'Home/PartnerAuthorise/index';
 
     public function __construct(){
         parent::__construct();
-        AgentCache::remove($this->user['id']);
-        $this->partner = AgentCache::get($this->user['id']);
-        //机构登记
-        if(!$this->partner || $this->partner['auth_status'] == 0){
-            $this->error(C('ERROR_COMPANY_REGISTER_REMIND'),U($this->partnerAuthoriseUrl));
+        PartnerCache::remove($this->user['id']);
+        $this->partner = PartnerCache::get($this->user['id']);
+        //合伙人认证
+        if(!$this->partner || $this->partner['auth_status'] != 3){
+            $this->error('您还不是合伙人！',U($this->partnerAuthoriseUrl));
         }
     }
 }
