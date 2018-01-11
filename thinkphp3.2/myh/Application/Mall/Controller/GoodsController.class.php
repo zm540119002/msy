79d598b42c160b8f7141a1725ebb362aa912e671
yangguanbao <yangguanbao@qq.com>
2018-01-11 14:21:55 +0800

    ygb

--- thinkphp3.2/myh/Application/Mall/Controller/GoodsController.class.php
+++ thinkphp3.2/myh/Application/Mall/Controller/GoodsController.class.php
@@ -151,8 +151,9 @@ class GoodsController extends BaseController {
 
     public function aa(){
         $wechat= new Jssdk(C('WX_CONFIG')['APPID'], C('WX_CONFIG')['APPSECRET']);
-//        $url = 'http://'.$this->host . $_SERVER['REQUEST_URI'];
-      $url = urlencode('http://myh.meishangyun.com/index.php/Mall/Index/index');
+        $url = 'http://'.$this->host . '/index.php/Mall/Index/index';
+        print_r($url);exit;
+//      $url = urlencode('http://myh.meishangyun.com/index.php/Mall/Index/index');
        // $_SESSION['wx_redirect'] = $url;
         $wechat -> getOauthRedirect($url,"wxbase");
 //        print_r($oauto_url);exit;
