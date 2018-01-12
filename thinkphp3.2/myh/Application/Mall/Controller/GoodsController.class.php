14641f6324633188424ce7714170d9c760d4517a
yangguanbao <yangguanbao@qq.com>
2018-01-12 09:14:09 +0800

    ygb

--- thinkphp3.2/myh/Application/Mall/Controller/GoodsController.class.php
+++ thinkphp3.2/myh/Application/Mall/Controller/GoodsController.class.php
@@ -143,6 +143,14 @@ class GoodsController extends BaseController {
             $this -> aveScore = round($modelComment -> avg('score'),1);//平均分数
             $this -> userCommentNum = $modelComment -> count();//多少用户评价
             //授权获取微信信息
+            if($code){
+                $wxUser = $this -> getOAuthWeiXinUserInfo();
+                if(!$wxUser){
+                    return false;
+                }else{
+                    return $wxUser;
+                }
+            }
             //$wxUser = $this -> wxLogin();
         }
         $this -> display();
--- thinkphp3.2/myh/Application/Mall/Controller/WeiXinController.class.php
+++ thinkphp3.2/myh/Application/Mall/Controller/WeiXinController.class.php
@@ -5,24 +5,23 @@ use web\all\Component\WxpayAPI\Jssdk;
 class WeiXinController extends BaseController {
     //微信登录
     public function wxLogin(){
-        $url = $_GET['url'];
-        if(isWxBrowser()) {//判断是否为微信浏览器
-            $wechat= new Jssdk(C('WX_CONFIG')['APPID'], C('WX_CONFIG')['APPSECRET']);
-            $code = isset($_GET['code'])?$_GET['code']:'';
-            if($code){
-                $wxUser = $this -> getOAuthWeiXinUserInfo();
-                if(!$wxUser){
-                    return false;
-                }else{
-                    return $wxUser;
-                }
-            }else{
-                $wechat -> getOauthRedirect($url,"wxbase");
-            }
-        }
-
+        $wxUser = $this -> getOAuthWeiXinUserInfo();
+//        $url = $_GET['url'];
+//        if(isWxBrowser()) {//判断是否为微信浏览器
+//            $wechat= new Jssdk(C('WX_CONFIG')['APPID'], C('WX_CONFIG')['APPSECRET']);
+//            $code = isset($_GET['code'])?$_GET['code']:'';
+//            if($code){
+//                $wxUser = $this -> getOAuthWeiXinUserInfo();
+//                if(!$wxUser){
+//                    return false;
+//                }else{
+//                    return $wxUser;
+//                }
+//            }else{
+//                $wechat -> getOauthRedirect($url,"wxbase");
+//            }
+//        }
     }
-
 
 }
 
