1e067d0eb18d7d821fb0f418b7653ac8a066084b
yangguanbao <yangguanbao@qq.com>
2018-01-12 11:51:00 +0800

    ygb

--- thinkphp3.2/myh/Application/Mall/Controller/WeiXinController.class.php
+++ thinkphp3.2/myh/Application/Mall/Controller/WeiXinController.class.php
@@ -26,7 +26,7 @@ class WeiXinController extends BaseController {
         $url = $_GET['url'];
         $wxUser = $this -> getOAuthWeiXinUserInfo();
         $backUrl = substr($url,0,strrpos($url,'.html'));
-        $url = $url.'/code';
+        $url = $url.'/code=';
         header("Location: $url");
 
     }
