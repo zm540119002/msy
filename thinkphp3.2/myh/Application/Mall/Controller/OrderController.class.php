a923e17bb0a476ca187a2bed9fb38294d863e43e
yangguanbao <yangguanbao@qq.com>
2018-01-12 16:42:10 +0800

    ygb

--- thinkphp3.2/myh/Application/Mall/Controller/OrderController.class.php
+++ thinkphp3.2/myh/Application/Mall/Controller/OrderController.class.php
@@ -225,7 +225,7 @@ class OrderController extends AuthUserController {
             //判断是否已团购
             $userIdArray = D('GroupBuyDetail')->where($_where)->getField('user_id',true);
             if(in_array($this->user['id'],$userIdArray)){
-                $this->ajaxReturn(errorMsg('已团购',array('url'=>U('Goods/goodsDetail/',array('goodsId'=>$goodsList[0]['foreign_id'])))));
+                $this->ajaxReturn(errorMsg('你有参加此团购，是否重新开团',array('url'=>U('Goods/goodsDetail/',array('goodsId'=>$goodsList[0]['foreign_id'])))));
             }
             D('GroupBuy')->joinGroupBuy($goodsList[0], $this->user['id'],$orderId,$groupBuyId);
         }
