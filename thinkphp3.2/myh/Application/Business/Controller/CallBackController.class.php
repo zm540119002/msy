206df1a110b922a1bf3c02e6f235d96c8a50cd9d
zhangmin <540119002@qq.com>
2018-01-15 15:40:49 +0800

    zm

--- thinkphp3.2/myh/Application/Business/Controller/CallBackController.class.php
+++ thinkphp3.2/myh/Application/Business/Controller/CallBackController.class.php
@@ -274,6 +274,17 @@ class CallBackController extends Controller{
         }
     }
 
+    public function test(){
+        $parameter = array(
+            'out_trade_no' =>'20180115133056462855292312274665',//微信回的商家订单号
+            'total_fee' => 1,//支付金额
+            'transaction_id' => '4200000056201801154355151125',//微信交易订单
+            'time_end' => '20180109172730',//支付时间
+            'attach' => '4',//支付时间
+        );
+        $this->depositHandle($parameter);
+    }
+
     /**席位订金充值回调
      */
     private function depositHandle($data){
@@ -296,7 +307,7 @@ class CallBackController extends Controller{
         $_POST = [];
         $_POST['auth_status'] = 2;
         $where = array(
-            'p.user_id' => $data['attach'],
+            'user_id' => $data['attach'],
         );
         $returnData = $modelPartner->savePartner($where);
         if ($returnData['status'] == 0) {
