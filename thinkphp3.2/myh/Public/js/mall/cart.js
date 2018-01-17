a9646665f7d678c71cfb0917fee75e68e2f398a5
yangguanbao <yangguanbao@qq.com>
2018-01-17 16:24:32 +0800

    ygb

--- thinkphp3.2/myh/Public/js/mall/cart.js
+++ thinkphp3.2/myh/Public/js/mall/cart.js
@@ -14,18 +14,23 @@ $(function () {
         var foreign_ids=[];
         foreign_ids.push(_li.data('id'));
         postData.foreign_ids = foreign_ids;
-        delCart(postData);
+        var type = 'single';
+        delCart(postData,type,$(this));
     });
+    //选择删除购物车
     $('body').on('click','.detele_carts',function(){
         var postData = {};
         var foreign_ids = [];
         $.each($('.purchase_package_list li'),function(){
             var _this=$(this);
-            var foreign_id = _this.data('id');
-            foreign_ids.push(foreign_id);
+            if(_this.find('.sigle_checkbox').is(':checked')){
+                var foreign_id = _this.data('id');
+                foreign_ids.push(foreign_id);
+            }
         });
         postData.foreign_ids = foreign_ids;
-        delCart(postData);
+        var type = 'more';
+        delCart(postData,type,$('.purchase_package_list li'));
     });
 });
 //从购物车里替换单个商品信息
@@ -56,29 +61,52 @@ function replaceOneGoodsToCart(obj) {
         }
     });
 }
-
-function delCart(postData) {
+//选择或当个删除购物车
+function delCart(postData,type,obj) {
     var url = CONTROLLER + '/delCart';
-    $.ajax({
-        url: url,
-        data: postData,
-        type: 'post',
-        beforeSend: function(){
-            $('.loading').show();
-        },
-        error:function(){
-            $('.loading').hide();
-            dialog.error('AJAX错误');
-        },
-        success: function(data){
-            $('.loading').hide();
-            if(data.status==0){
-                dialog.error(data.info);
-            }else {
-                dialog.success(data.info);
-            }
+    layer.open({
+        content:'确定要删除此购物商品？',
+        btn:['确定','取消'],
+        yes:function(index){
+            $.ajax({
+                url: url,
+                data: postData,
+                type: 'post',
+                beforeSend: function(){
+                    $('.loading').show();
+                },
+                error:function(){
+                    $('.loading').hide();
+                    dialog.error('AJAX错误');
+                },
+                success: function(data){
+                    $('.loading').hide();
+                    if(data.status==0){
+                        dialog.error(data.info);
+                    }else {
+                        if(type == 'single'){
+                            obj.parents('li').remove();
+                        }
+                        if(type == 'more'){
+                            $.each(obj,function(){
+                                var _this=$(this);
+                                var foreign_id = _this.data('id');
+                                for(var i=0;i<postData.foreign_ids.length;i++){
+                                    if(foreign_id == postData.foreign_ids[i]){
+                                        _this.remove();
+                                    }
+                                }
+                            });
+                        }
+                        dialog.success(data.info);
+                    }
+                }
+            });
+            layer.close(index);
         }
-    });
+    })
+
+
 
 }
 
