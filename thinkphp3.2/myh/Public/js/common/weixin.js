f1cca7bc3c67794bfa178e1f7d18def8b36ad384
yangguanbao <yangguanbao@qq.com>
2018-01-19 11:55:18 +0800

    ygb

--- thinkphp3.2/myh/Public/js/common/weixin.js
+++ thinkphp3.2/myh/Public/js/common/weixin.js
@@ -1,18 +1,6 @@
 /**
  * Created by Administrator on 2018/1/12.
  */
-// setTimeout(function(){
-//     $(window).load(function() {
-//         alert(1);return;
-//         var url = window.location.href;
-//         var ua = window.navigator.userAgent.toLowerCase();
-//         if(ua.match(/MicroMessenger/i) == 'micromessenger'){//判断微信浏览器
-//             if(url.indexOf("code=") <= 0 ) { //code
-//                 self.location=MODULE + 'checkWxUser';
-//             }
-//         }
-//     });
-// },1000);
 $(window).load(function() {
     var url = window.location.href;
     var ua = window.navigator.userAgent.toLowerCase();
