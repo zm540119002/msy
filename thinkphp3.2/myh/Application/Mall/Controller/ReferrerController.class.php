8fb45bae327fc9df3e4d2201b8dbacc801f9aa02
yangguanbao <yangguanbao@qq.com>
2018-01-25 10:44:20 +0800

    ygb

--- thinkphp3.2/myh/Application/Mall/Controller/ReferrerController.class.php
+++ thinkphp3.2/myh/Application/Mall/Controller/ReferrerController.class.php
@@ -49,30 +49,30 @@ class ReferrerController extends AuthUserController{
         $userId = $this->user['id'];
         $scene_type = 'QR_LIMIT_SCENE';
         $res = $this -> getQRcode($scene_type, $userId);
-        print_r($res);exit;
-//        $where['user_id'] = $userId;
-//        $mode = D('member');
-//        $memberInfo = $mode->where($where)->find();
-//        if(!empty($memberInfo) && $memberInfo['qr_code']){
-//            $this->ajaxReturn(successMsg('成功',array('url'=>$memberInfo['qr_code'])));
-//        }
-//        $avatarPath = $this->user['avatar'];
-//        $url =  $this->host.'index.php/Mall/Index/index/userId/'.$userId;
-//        $newRelativePath = C('USER_LOGO');
-//        $shareQRCodes = createLogoQRcode($url,$avatarPath,$newRelativePath);
-//        $data['qr_code'] = $shareQRCodes;
-//        $data['user_id'] = $this->user['id'];
-//        if(empty($memberInfo)){
-//            $res = $mode -> add($data);
-//        }
-//        if(!empty($memberInfo) && empty($memberInfo['qr_code'])){
-//            $res = $mode -> where($where)->save($data);
-//        }
-//        if($res){
-//            $this->ajaxReturn(successMsg('成功',array('url'=>$shareQRCodes)));
-//        }else{
-//            $this->ajaxReturn(errorMsg('失败'));
-//        }
+        $where['user_id'] = $userId;
+        $mode = D('member');
+        $memberInfo = $mode->where($where)->find();
+        if(!empty($memberInfo) && $memberInfo['qr_code']){
+            $this->ajaxReturn(successMsg('成功',array('url'=>$memberInfo['qr_code'])));
+        }
+        $avatarPath = $this->user['avatar'];
+        $url =  $res['url'];
+        $newRelativePath = C('USER_LOGO');
+        $shareQRCodes = createLogoQRcode($url,$avatarPath,$newRelativePath);
+        print_r($shareQRCodes);exit;
+        $data['qr_code'] = $shareQRCodes;
+        $data['user_id'] = $this->user['id'];
+        if(empty($memberInfo)){
+            $res = $mode -> add($data);
+        }
+        if(!empty($memberInfo) && empty($memberInfo['qr_code'])){
+            $res = $mode -> where($where)->save($data);
+        }
+        if($res){
+            $this->ajaxReturn(successMsg('成功',array('url'=>$shareQRCodes)));
+        }else{
+            $this->ajaxReturn(errorMsg('失败'));
+        }
     }
 
     //我的推客收益
