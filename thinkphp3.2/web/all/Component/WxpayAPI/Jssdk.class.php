cb0279f1fbcd16c08b4d15c80ee5fea70d3cbbca
yangguanbao <yangguanbao@qq.com>
2018-01-25 10:38:44 +0800

    ygb

--- thinkphp3.2/web/all/Component/WxpayAPI/Jssdk.class.php
+++ thinkphp3.2/web/all/Component/WxpayAPI/Jssdk.class.php
@@ -854,7 +854,7 @@ class Jssdk {
         );
         break;
     }
-    // var_dump(json_encode($msg));
+    var_dump(json_encode($msg));
     $url = "https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=".$this->access_token;
     $res = $this->http_request($url, json_encode($msg));
     print_r($res);
