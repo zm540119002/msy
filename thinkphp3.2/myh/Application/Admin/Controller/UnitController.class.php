7319a04360a901b2acfbfe75185bd62f05c5b92f
zhangmin <540119002@qq.com>
2018-01-26 17:29:38 +0800

    zm

--- thinkphp3.2/myh/Application/Admin/Controller/UnitController.class.php
+++ thinkphp3.2/myh/Application/Admin/Controller/UnitController.class.php
@@ -7,11 +7,7 @@ class UnitController extends BaseController {
     /**单位-管理
      */
     public function unitManage(){
-<<<<<<< HEAD
         $modelUnit = D('Mall/Unit');
-=======
-        $modelUnit = D('Business/Unit');
->>>>>>> 918034240c5e655e5d543e81e3d131f7c9e53042
         if(IS_POST){
             $this->unitList = $modelUnit->selectUnit();
             $this->display('unitList');
@@ -24,11 +20,7 @@ class UnitController extends BaseController {
     /**单位-编辑
      */
     public function unitEdit(){
-<<<<<<< HEAD
         $modelUnit = D('Mall/Unit');
-=======
-        $modelUnit = D('Business/Unit');
->>>>>>> 918034240c5e655e5d543e81e3d131f7c9e53042
         if(IS_POST){
             if(isset($_POST['unitId']) && intval($_POST['unitId'])){
                 $res = $modelUnit->saveUnit();
@@ -55,11 +47,7 @@ class UnitController extends BaseController {
             $this->ajaxReturn(errorMsg(C('NOT_GET')));
         }
 
-<<<<<<< HEAD
         $modelUnit = D('Mall/Unit');
-=======
-        $modelUnit = D('Business/Unit');
->>>>>>> 918034240c5e655e5d543e81e3d131f7c9e53042
         $where = array(
             'ut.status' => 0,
         );
@@ -91,11 +79,7 @@ class UnitController extends BaseController {
         if(!IS_POST){
             $this->ajaxReturn(errorMsg(C('NOT_POST')));
         }
-<<<<<<< HEAD
         $modelUnit = D('Mall/Unit');
-=======
-        $modelUnit = D('Business/Unit');
->>>>>>> 918034240c5e655e5d543e81e3d131f7c9e53042
         $res = $modelUnit->delUnit();
         $this->ajaxReturn($res);
     }
