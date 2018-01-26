990d00a4133b1d10012777dc14169a63b6b589bc
zhangmin <540119002@qq.com>
2018-01-26 10:48:47 +0800

    zm

--- thinkphp3.2/myh/Application/Business/Controller/PersonalCenterController.class.php
+++ thinkphp3.2/myh/Application/Business/Controller/PersonalCenterController.class.php
@@ -31,33 +31,4 @@ class PersonalCenterController extends BaseController {
             $this->display();
         }
     }
-
-    //个人中心-升级VIP
-    public function upgrade(){
-        $modeLevel = D('Level');
-        if(IS_POST){
-        }else{
-            $where = array(
-                'l.type' => 1,
-            );
-            $this->levelList = $modeLevel->selectLevel($where);
-            $this->display();
-        }
-    }
-
-    //个人中心-升级VIP-级别详情
-    public function levelDetail(){
-        $modeLevel = D('Level');
-        if(IS_POST){
-        }else{
-            $id = I('get.levelId',0,'int');
-            $where = array(
-                'l.type' => 1,
-                'l.id' => $id,
-            );
-            $levelInfo = $modeLevel->selectLevel($where);
-            $this->levelInfo = $levelInfo[0];
-            $this->display();
-        }
-    }
 }
