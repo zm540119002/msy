485fed7265c8906ef47e9d9f6ac0ba582ca26f9c
yangguanbao <yangguanbao@qq.com>
2018-02-08 15:46:23 +0800

    ygb

--- thinkphp3.2/myh/Application/Mall/Controller/GoodsController.class.php
+++ thinkphp3.2/myh/Application/Mall/Controller/GoodsController.class.php
@@ -140,12 +140,13 @@ class GoodsController extends BaseController {
                 $model = D('GroupBuyDetail');
                 $_where['gbd.group_buy_id'] =  $this -> groupBuyId ;
                 $_where['gbd.pay_status'] = 2;
-                $field=['wxu.id','wxu.openid',   'wxu.headimgurl','gb.overdue_time' ];
+                $field=['wxu.id','wxu.openid','wxu.headimgurl','gb.overdue_time' ];
                 $join=[ 'left join wx_user wxu on wxu.openid = gbd.openid ',
                     'left join group_buy gb on gb.id = gbd.group_buy_id ',
                 ];
                 $groupBuyDetail = $model->selectGroupBuyDetail($_where,$field,$join);
                 $this->groupBuyDetail = $groupBuyDetail;
+                print_r( $this->groupBuyDetail);exit;
                 //判断团购是否已过期
                 if($this->groupBuyDetail[0]['overdue_time'] - time() < 0){
                     $conf = array(20);
