b33ec74e9937e9b0f708d3c46db1bcc86e67dc18
yangguanbao <yangguanbao@qq.com>
2018-02-08 14:15:48 +0800

    ygb

--- thinkphp3.2/myms/Application/Mall/Model/WeiXinModel.class.php
+++ thinkphp3.2/myms/Application/Mall/Model/WeiXinModel.class.php
@@ -100,7 +100,6 @@ class WeiXinModel extends Model {
     public function wxLogin(){
         if(isWxBrowser()) {//判断是否为微信浏览器
             $wechat= new Jssdk(C('WX_CONFIG')['APPID'], C('WX_CONFIG')['APPSECRET']);
-            print_r(C('WX_CONFIG')['APPID']);exit;
             $code = isset($_GET['code'])?$_GET['code']:'';
             if($code){
                 $wxUser =$wechat ->getOauthUserInfo();
