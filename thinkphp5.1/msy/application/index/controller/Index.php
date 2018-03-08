63cce2aa401dbbb3fc41986644bb662eaa3a9ede
zhangmin <540119002@qq.com>
2018-03-08 16:59:57 +0800

    zm

--- thinkphp5.1/msy/application/index/controller/Index.php
+++ thinkphp5.1/msy/application/index/controller/Index.php
@@ -9,14 +9,6 @@ class Index extends Controller
     public function index()
     {
 //        return dump(config());
-<<<<<<< HEAD
-=======
-        return $this->fetch();
-    }
-
-    public function hello()
-    {
->>>>>>> 8b86c73c5e5e133e08364efaefba68fda42f7d0b
         return $this->fetch();
     }
 }
