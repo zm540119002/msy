5870dd8e4b3da7e9c16fbd9680aa239a4a1a0657
webserver <webserver@qq.com>
2018-03-11 15:16:03 +0800

    zm

--- thinkphp5.1/msy/public/static/common/js/loginDialog.js
+++ thinkphp5.1/msy/public/static/common/js/loginDialog.js
@@ -54,3 +54,99 @@ $('body').on('click','.mesg_code',function(){
         }
     });
 });
+
+var userLoginForm=$('#userLoginForm').html();
+//登录触发
+var loginLayer = null;
+function loginDialog(func){
+    loadTrigger=false;
+    loginLayer = layer.open({
+        className:'loginLayer',
+        content:userLoginForm,
+        // shadeClose:false,
+        btn:'登录',
+        success:function(){
+            window.scrollTo(0,0);
+            tab_down('.loginNav li','.loginTab .login_wrap','click');
+            $('.layui-m-layershade').on('touchmove',function(e){
+                event.preventDefault();
+            });
+            isRolling($('.loginLayer'));
+        },
+        end:function(){
+            loadTrigger=true;
+        },
+        yes:function(index){
+            var $layer = $('.loginLayer').find('.active');
+            var _index =$('.loginLayer').find('.login_wrap.active').index();
+            var content='';
+            //验证
+            switch(_index){
+                case 0:
+                    var userPhone=$layer.find('.user_phone').val();
+                    var verifiCode=$layer.find('.tel_code').val();
+                    if(!register.phoneCheck(userPhone)){
+                        content='请输入正确手机号';
+                    }else if(!register.vfyCheck(verifiCode)){
+                        content = "请输入正确的验证码";
+                    }
+                    break;
+                case 1:
+                    var userName=$layer.find('.user_name').val();
+                    var password=$layer.find('.password').val();
+                    if(!checkAccount(userName)){
+                        content='请输入正确手机号';
+                    }else if(!register.pswCheck(password)){
+                        content = "请输入6-16数字或字母的密码";
+                    }
+                    break;
+            }
+            if(content){
+                errorTipc(content);
+                return false;
+            }
+
+            var url = '{:U("/Home/User/login")}';
+            var postData = $('.loginLayer').find('#formLogin').serializeObject();
+            $.ajax({
+                url:url,
+                type:'post',
+                data:postData,
+                error:function(xhr){},
+                success:function(data){
+                    if(data.status==0){
+                        errorTipc(data.info);
+                        return false;
+                    }else if(data.status==1){
+                        if(func && $.isFunction(func)){
+                            func();
+                        }
+                        loadTrigger=true;
+                        layer.close(index);
+                    }
+                }
+            });
+        }
+    });
+};
+
+$(function(){
+    $('.top_menu_list a').tab();
+    //轮播
+    var elem = document.getElementById('slider');
+    swipe(elem);
+    //登录
+    $('body').on('click','.deployed-deployment,.order-management',function(){
+        loginDialog();
+    });
+    //注册
+    $('body').on('click','.register_dialog',function(){
+        platformNotesDialog();
+        //registerDialog();
+    });
+
+    //忘记密码
+    $('body').on('click','.forget_dialog',function(){
+        forgetPasswordDialog();
+    });
+})
