7da1e66bcdf53ce58bf66c91153836e5efb4d402
yangguanbao <yangguanbao@qq.com>
2018-03-11 11:55:24 +0800

    ygb

--- thinkphp5.1/msy/runtime/temp/f23e22ec4aeeb3cb1b6dde06fd5bd4cd.php
+++ thinkphp5.1/msy/runtime/temp/f23e22ec4aeeb3cb1b6dde06fd5bd4cd.php
@@ -1,4 +1,4 @@
-<?php /*a:2:{s:68:"E:\web\thinkphp5.1\msy\application/store_admin/view\login\index.html";i:1520732659;s:68:"E:\web\thinkphp5.1\msy\application/store_admin/view\Public\base.html";i:1520733081;}*/ ?>
+<?php /*a:2:{s:68:"E:\web\thinkphp5.1\msy\application/store_admin/view\login\index.html";i:1520739249;s:68:"E:\web\thinkphp5.1\msy\application/store_admin/view\Public\base.html";i:1520738368;}*/ ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
 <head>
@@ -7,11 +7,8 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<link rel="stylesheet" href="http://msy.dev/static/common/css/common/public.css">
-	<link rel="stylesheet" href="http://msy.dev/static/common/css/common/weui.css">
 	
-
-  <link href="http://msy.dev/static/hadmin/static/h-ui.admin/css/H-ui.login.css" rel="stylesheet" type="text/css" />
-
+<link rel="stylesheet" href="public/static/admin/common/css/user.css" type="text/css"/>
 
 	<script type="text/javascript" src="http://msy.dev/static/common/js/common/jquery-1.9.1.min.js"></script>
 </head>
@@ -19,47 +16,47 @@
 <body>
 <!-- 基础内容模块，子页面可覆盖 -->
 
-  <input type="hidden" id="TenantId" name="TenantId" value="" />
-  <div class="header"></div>
-  <div class="loginWraper">
-    <div id="loginform" class="loginBox">
-      <form class="form form-horizontal" action="<?php echo url('User/login'); ?>" method="post">
-        <div class="row cl">
-          <label class="form-label col-xs-3"><i class="Hui-iconfont">&#xe60d;</i></label>
-          <div class="formControls col-xs-8">
-            <input id="" name="name" type="text" placeholder="账户" class="input-text size-L">
-          </div>
+<article class="msy-bg">
+    <section class="userInfoWrapper">
+        <img src="http://msy.dev/static/common/img/common/logo.png" alt="" class="msy-logo">
+        <h2 class="tc">用户登录</h2>
+        <div class="">
+            <ul class="loginNav">
+                <li class="current">账号密码登录</li>
+                <li>短信验证码登陆</li>
+            </ul>
+            <form class="loginTab" id="formLogin">
+                <div class="userLogin login_wrap active">
+                    <div class="login_item">
+                        <span class="username_txt">手机号</span>
+                        <input class="username user_name" type="text" placeholder="请输入用户名(6-16位数字或者字母、下划线)" name="name">
+                    </div>
+                    <div class="login_item">
+                        <span class="password_txt">密码</span>
+                        <input class="username password" type="password" placeholder="请输入密码(输入6-16数字或字母的密码)" name="password">
+                    </div>
+                </div>
+                <div class="smsLogin login_wrap hide">
+                    <div class="login_item">
+                        <span class="username_txt">手机号</span>
+                        <input class="username user_phone" type="tel" placeholder="请输入手机号" name="mobile_phone">
+                        <a href="javascript:void(0);" class="mesg_code">获取验证码</a>
+                    </div>
+                    <div class="login_item">
+                        <span class="username_txt txt3">验证码</span>
+                        <input type="text" class="tel_code username input_txt3" placeholder="请输入收到的验证码(6位数字)" name="captcha">
+                    </div>
+                </div>
+                <input type="hidden" name="captcha_type" value="login" />
+            </form>
+            <a href="javascript:void(0);" class="loginBtn">登录</a>
+            <div class="friend_tipc">
+                <a href="__CONTROLLER__/register" class="register_dialog">注册</a>
+                <a href="__CONTROLLER__/forget_password" class="forget_dialog">忘记密码？</a>
+            </div>
         </div>
-        <div class="row cl">
-          <label class="form-label col-xs-3"><i class="Hui-iconfont">&#xe60e;</i></label>
-          <div class="formControls col-xs-8">
-            <input id="" name="password" type="password" placeholder="密码" class="input-text size-L">
-          </div>
-        </div>
-        <div class="row cl">
-          <div class="formControls col-xs-8 col-xs-offset-3">
-            <input class="input-text size-L" type="text" placeholder="验证码"  name="captcha" value="" style="width:150px;">
-            <img src="<?php echo url('Admin/User/verify'); ?>" alt="验证码" onclick="reloadcode(this)" title="点击刷新"/>
-          </div>
-        </div>
-
-        <div class="row cl">
-          <div class="formControls col-xs-8 col-xs-offset-3">
-            <label for="online">
-              <input type="checkbox" name="online" id="online" value="">
-              使我保持登录状态</label>
-          </div>
-        </div>
-        <div class="row cl">
-          <div class="formControls col-xs-8 col-xs-offset-3">
-            <input name="" type="submit" class="btn btn-success radius size-L" value="&nbsp;登&nbsp;&nbsp;&nbsp;&nbsp;录&nbsp;">
-            <input name="" type="reset" class="btn btn-default radius size-L" value="&nbsp;取&nbsp;&nbsp;&nbsp;&nbsp;消&nbsp;">
-          </div>
-        </div>
-      </form>
-    </div>
-  </div>
-
+    </section>
+</article>
 
 <!-- 返回顶部-->
 
@@ -82,11 +79,12 @@
 <script type="text/javascript" src="http://msy.dev/static/common/js/common/loginDialog.js"></script>
 
 
-  <script type="text/javascript">
+<script type="text/javascript" src="public/static/admin/common/js/login.js"></script>
+<script type="text/javascript">
     // 刷新验证码
     function reloadcode(obj) {
-      obj.src="<?php echo url('Admin/User/verify'); ?>";
+        obj.src = "<?php echo url('Admin/User/verify'); ?>";
     }
-  </script>
+</script>
 
 </body></html>
