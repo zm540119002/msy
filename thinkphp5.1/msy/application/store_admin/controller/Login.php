7c414c951e737e41ea9cfcb157b01e6afe91c527
yangguanbao <yangguanbao@qq.com>
2018-03-11 09:46:28 +0800

    ygb

---
+++ thinkphp5.1/msy/application/store_admin/controller/Login.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/3/11
+ * Time: 9:36
+ */
+
+namespace app\store_admin\controller;
+use think\Controller;
+use Request;
+
+class Login extends Controller
+{
+    public function index()
+    {
+
+//        return dump(config());
+        // return dump(config('menu_conf.menu_array'));
+        return $this->fetch();
+    }
+}
---
+++ thinkphp5.1/msy/runtime/temp/05d3faf44ee5fd0e0a35898a240ac405.php
@@ -0,0 +1,2 @@
+<?php /*a:1:{s:75:"E:\web\thinkphp5.1\msy\application/store_admin/view\goods\goods_manage.html";i:1520585652;}*/ ?>
+
