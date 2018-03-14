e04031aa25db5a4b05148010c6447957159a4dd3
zhangmin <540119002@qq.com>
2018-03-14 16:37:03 +0800

    zm

--- thinkphp5.1/common/Controller/Base.php
+++ thinkphp5.1/common/Controller/Base.php
@@ -7,12 +7,15 @@ use think\Image;
 /**公共基础控制器
  */
 class Base extends Controller{
+    protected $host;
     public function __construct(){
         parent::__construct();
         //登录验证后跳转回原验证发起页
+        $this->host = isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] :
+            (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '');
         session('backUrl',$_SERVER['REQUEST_URI'] ? $this->host . $_SERVER['REQUEST_URI'] : $this->host . $_SERVER['HTTP_REFERER']);
         //多步跳转后回原发起页
-        session('returnUrl',I('get.returnUrl','','string')?:I('post.returnUrl','','string'));
+//        session('returnUrl',I('get.returnUrl','','string')?:I('post.returnUrl','','string'));
     }
 
     //返回图片临时相对路径
--- thinkphp5.1/msy/application/index/controller/Test.php
+++ thinkphp5.1/msy/application/index/controller/Test.php
@@ -2,11 +2,13 @@
 namespace app\index\controller;
 
 use common\controller\Base;
+use Config;
 
 class Test extends Base
 {
     public function index()
     {
         return $this->fetch();
+        return dump(config());
     }
 }
