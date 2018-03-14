8a98af9a742014150729ace462a69f4e8db68743
zhangmin <540119002@qq.com>
2018-03-14 17:44:02 +0800

    zm

--- thinkphp5.1/msy/application/common.php
+++ thinkphp5.1/msy/application/common.php
@@ -1,6 +1,10 @@
 <?php
 require __DIR__ . '/../../common/function/common.php';
 require __DIR__ . '/../../common/function/page.php';
+
+/**示例函数
+ * @return string
+ */
 function test(){
     return 'test';
 }
