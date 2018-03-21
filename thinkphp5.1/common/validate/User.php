2ecea2e489df74dbdb0f9b3c056ee81367780030
webserver <webserver@qq.com>
2018-03-21 09:15:01 +0800

    zm

--- thinkphp5.1/common/validate/User.php
+++ thinkphp5.1/common/validate/User.php
@@ -5,16 +5,27 @@ use think\Validate;
 
 class User extends Validate
 {
+    //验证规则
     protected $rule = [
-        'mobile_phone' => 'require|mobile',
-        'mobile_phone' => 'unique:user',
+        'mobile_phone'  => [
+            'require','mobile','unique' => 'user',
+        ],
     ];
+    //验证消息
     protected $message  =   [
-        'mobile_phone.require' => '请填写正确的手机号码！',
+        'mobile_phone.require' => '手机号码必须！',
+        'mobile_phone.mobile' => '请填写正确的手机号码！',
         'mobile_phone.unique' => '此号码已被注册！',
     ];
+    //验证场景
     protected $scene = [
-        'add' => ['mobile_phone.require',],
-        'edit' => ['mobile_phone.require','mobile_phone.unique',],
+        'login'  =>  ['mobile_phone'],
+        'register'  =>  ['mobile_phone'],
     ];
+    //login验证场景重定义
+    public function sceneLogin()
+    {
+        return $this->only(['mobile_phone'])
+            ->remove('mobile_phone', 'unique');
+    }
 }
