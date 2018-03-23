5002f51327a073b081184e135292462afc08973f
yangguanbao <yangguanbao@qq.com>
2018-03-23 17:34:25 +0800

    zm

---
+++ thinkphp5.1/msy/application/factory/controller/brand.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\factory\controller;
+use app\factory\model\Brand as M;
+use common\controller\Base;
+class Brand extends Base
+{
+    public function index()
+    {
+        return $this->fetch('template/category_second_template.html');
+    }
+
+    public function add()
+    {
+        if(request()->isPost()){
+            $model = new M();
+            return $model -> add();
+        }
+        return $this->fetch();
+    }
+
+
+
+}
