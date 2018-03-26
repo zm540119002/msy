b1573d5fc90bbededfa54d2e78a51ca92435ba73
webserver <webserver@qq.com>
2018-03-26 10:04:01 +0800

    zm

---
+++ thinkphp5.1/msy/application/factory/model/series .php
@@ -0,0 +1,33 @@
+<?php
+namespace app\factory\model;
+use think\Model;
+use think\Db;
+/**
+ * 基础模型器
+ */
+
+class Series  extends Model {
+	// 设置当前模型对应的完整数据表名称
+	protected $table = 'series ';
+	// 设置当前模型的数据库连接
+//	protected $connection = 'db_factory';
+	/**
+	 * 新增
+	 */
+	public function add(){
+		$data = input('post.');
+		$validate = validate('Series');
+		if(!$result = $validate->scene('add')->check($data)) {
+			return errorMsg($validate->getError());
+		}
+		$data['brand_img'] = moveImgFromTemp(config('upload_dir.factory_brand'),basename($data['brand_img']));
+		$data['certificate'] = moveImgFromTemp(config('upload_dir.factory_brand'),basename($data['certificate']));
+		$data['authorization'] = moveImgFromTemp(config('upload_dir.factory_brand'),basename($data['authorization']));
+		$result = $this->allowField(true)->save($data);
+		if(false !== $result){
+			return successMsg("已提交申请");
+		}else{
+			return errorMsg($this->getError());
+		}
+	}
+}
