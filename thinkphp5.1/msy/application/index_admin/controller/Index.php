d53bcdfdf331da4cc51e4abb06cf3427f4332015
yangguanbao <yangguanbao@qq.com>
2018-04-02 15:09:58 +0800

    zm

--- thinkphp5.1/msy/application/index_admin/controller/Index.php
+++ thinkphp5.1/msy/application/index_admin/controller/Index.php
@@ -8,8 +8,6 @@ class Index extends Controller
 {
     public function index()
     {
-//        return dump(config());
-       // return dump(config('menu_conf.menu_array'));
         return $this->fetch();
     }
 
