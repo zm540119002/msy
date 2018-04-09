5bd4ecdb4b19f42e553602ab4dfe2c7e9d756715
webserver <webserver@qq.com>
2018-04-09 14:45:53 +0800

    zm

--- thinkphp5.1/msy/public/static/common/js/uploadMultiImg.js
+++ thinkphp5.1/msy/public/static/common/js/uploadMultiImg.js
@@ -78,7 +78,88 @@ $(function(){
     var goodsVideoList=$('#goodsVideoList').html();
     $('body').on('click','.uploadGoodsVideo',function(){
         uploadsMultiVideo(goodsVideoList);
-    })
+    });
+     //上传单图片和描述
+    $('body').on('change','.uploadImgDescribe',function () {
+        var img = event.target.files[0];
+        var obj=$(this).parent();
+        var imgContainer = $('.multi-picture-module');
+        // 判断是否图片
+        if(!img){
+            return false;
+        }
+        // 判断图片格式
+        var imgRegExp=/\.(?:jpg|jpeg|png|gif)$/;
+        if(!(img.type.indexOf('image')==0 && img.type && imgRegExp.test(img.name)) ){
+            layer.open({
+                content:'请上传：jpg、jpeg、png、gif格式图片',
+                time:2
+            }) ;
+        }
+        var reader = new FileReader();
+        reader.readAsDataURL(img);
+        reader.onload = function(e){
+            var imgUrl=e.target.result;
+            // $(obj).addClass('active');
+            var oLiLen=imgContainer.find('li').length;
+            var img=  $('<img src="" class="upload_img">');
+            img.attr("src", imgUrl);
+            var imgAdd = $('<li><div class="picture-module active"><input type="file" class="uploadImg uploadSingleEditImg" name=""><a class="delete-picture">X</a></div><a href="javascript:void(0);" class="edit-describe">编辑照片描述</a><textarea name="" id="" cols="30" rows="5" placeholder="请填写描述" class="edit-text"></textarea></li>');
+            imgAdd.find('.picture-module').append(img);
+            imgContainer.append(imgAdd);
+           
+            
+            //提交
+            // $.post("uploadImgToTemp",postData,function(msg){
+            //     if(msg.status == 1){
+            //         $(obj).find('.img').val(msg.info);
+            //         $(obj).find('img').attr('src','/uploads/'+msg.info);
+            //     }else{
+            //         dialog.error(msg.info)
+            //     }
+            // })
+        }
+    });
+    //上传单视频和描述
+    $('body').on('change','.uploadVideoDescribe',function () {
+        var img = event.target.files[0];
+        var obj=$(this).parent();
+        var imgContainer = $('.multi-picture-module');
+        // 判断是否图片
+        // if(!img){
+        //     return false;
+        // }
+        // // 判断图片格式
+        // var imgRegExp=/\.(?:jpg|jpeg|png|gif)$/;
+        // if(!(img.type.indexOf('image')==0 && img.type && imgRegExp.test(img.name)) ){
+        //     layer.open({
+        //         content:'请上传：jpg、jpeg、png、gif格式图片',
+        //         time:2
+        //     }) ;
+        // }
+        var reader = new FileReader();
+        reader.readAsDataURL(img);
+        reader.onload = function(e){
+            var videoUrl=e.target.result;
+            // $(obj).addClass('active');
+            var video=  $('<video src="" class="upload_img" autoplay="autoplay"></video>');
+            video.attr("src", videoUrl);
+            var videoAdd = $('<li><div class="picture-module active"><input type="file" class="uploadImg uploadSingleEditImg" name=""><span class="delete-picture">X</span></div><a href="javascript:void(0);" class="edit-describe">编辑视频描述</a><textarea name="" id="" cols="30" rows="5" placeholder="请填写描述" class="edit-text"></textarea></li>');
+            videoAdd.find('.picture-module').append(video);
+            imgContainer.append(videoAdd);
+           
+            
+            //提交
+            // $.post("uploadImgToTemp",postData,function(msg){
+            //     if(msg.status == 1){
+            //         $(obj).find('.img').val(msg.info);
+            //         $(obj).find('img').attr('src','/uploads/'+msg.info);
+            //     }else{
+            //         dialog.error(msg.info)
+            //     }
+            // })
+        }
+    });
     //编辑商品详情 上传多张图片 确认上传
     var editDetail=$('#editDetail').html();
     $('body').on('click','.editDetail',function(){
@@ -129,6 +210,25 @@ $(function(){
         }
 
     });
+    //上传企业图片
+    var uploadCompanyPic=$('#uploadCompanyPic').html();
+    $('body').on('click','.uploadCompanyPic',function(){
+        var _this=$(this);
+        var storageDataObj=_this.next('input[type="hidden"]');
+        uploadsImgDescribe(uploadCompanyPic,storageDataObj);
+    });
+    //上传企业视频
+    var companyVideoList=$('#companyVideoList').html();
+    $('body').on('click','.companyVideoList',function(){
+        var _this=$(this);
+        var storageDataObj=_this.next('input[type="hidden"]');
+        uploadsVideoDescribe(companyVideoList,storageDataObj);
+    }); 
+    //编辑描述
+    $('body').on('click','.edit-describe',function () {
+        var _this=$(this);
+        _this.next('.edit-text').toggleClass('active');
+    })
 })
 //图片弹窗
 function uploadsMultiImg(content){
@@ -199,6 +299,9 @@ function uploadsMultiVideo(content){
                 if(multiVideoSrc){
                     var multiVideoAttr=multiVideoSrc.split(',');
                     for(var i=0;i<multiVideoAttr.length-1;i++){
+                        if(multiVideoAttr[i].indexOf("uploads") == -1 && multiVideoAttr[i] !=''){
+                            multiVideoAttr[i] = uploads+multiVideoAttr[i];
+                        }
                         $('.editVideoLayer .multi-picture-module').append(html);
                         $('.editVideoLayer video').eq(i).attr('src',multiVideoAttr[i]);
                     }
@@ -253,5 +356,149 @@ function uploadsMultiVideo(content){
         })
 
 
+}
+//图片描述弹窗
+function uploadsImgDescribe(content,obj){
+    layer.open({
+            // title:['商品分类标签','border-bottom:1px solid #d9d9d9'],
+            className:'editCompanyPicLayer',
+            content:content,
+            btn:['确定','取消'],
+            success:function(){
+                //var html=$('#img_list').html(); 模板
+                var html='';
+                    html+='<li>';
+                    html+='<div class="picture-module active">';
+                    html+='<input type="file" class="uploadImg uploadSingleEditImg" name="">';
+                    html+='<a class="delete-picture">X</a>';
+                    html+='<img src="" class="upload_img">';
+                    html+='</div>';
+                    html+='<a href="javascript:void(0);" class="edit-describe">编辑照片描述</a>';
+                    html+='<textarea name="" id="" cols="30" rows="5" placeholder="请填写描述" class="edit-text"></textarea>';
+                    html+='</li>';                  
+                var multiImgAttr=obj.data('src');
+                console.log(multiImgAttr);
+                for(var i=0;i<multiImgAttr.length;i++){
+                    $('.editCompanyPicLayer .multi-picture-module').append(html);
+                    $('.editCompanyPicLayer .upload_img').eq(i).attr('src',multiImgAttr[i].imgSrc);
+                    $('.editCompanyPicLayer .edit-text').eq(i).val(multiImgAttr[i].imgText);
+                }
+            },
+            yes:function(index){
+                var layermultiImgAttr=[];
+                var layerImgInfoData={};
+                $.each($('.editCompanyPicLayer li'),function(i,val){
+                    var _this=$(this);
+                    var imgSrc=_this.find('img').attr('src');
+                    var imgText=_this.find('textarea').val();
+                    layerImgInfoData={
+                        imgSrc:imgSrc,
+                        imgText:imgText
+                    }
+                    layermultiImgAttr.push(layerImgInfoData);
+                })
+               
+                // obj.data('src',layermultiImgAttr);
+                obj.data('src',layermultiImgAttr);
+                // if(layermultiImgAttr.length==0){
+                //     layer.close(index);
+                //     return false;
+                // }
+                // var postDate = {};
+                // postDate.imgs = layermultiImgAttr;
+                // $.post('uploadMultiImgToTemp',postDate,function(info){
+                //    if(info.status == 0){
+                //        dialog.error(info.msg);
+                //        return false;
+                //    }
+                //     var imgArray = [];
+                //     $.each(info.info,function(index,img){
+                //         if(img.indexOf("uploads") == -1 && img !=''){
+                //             img = uploads+img;
+                //         }
+                //         imgArray.push(img);
+                //     });
+
+                //     $('.goods-detail').data('src',imgArray);
+                //     layer.close(index);
+                // })
+                layer.close(index);
+            },
+            no:function(){
+                $('.editCompanyPicLayer li').remove();
+            }
+        })
+}
+//视频描述弹窗
+function uploadsVideoDescribe(content,obj){
+    layer.open({
+            title:['上传企业视频','border-bottom:1px solid #d9d9d9'],
+            className:'editCompanyPicLayer',
+            content:content,
+            btn:['确定','取消'],
+            success:function(){
+                //var html=$('#img_list').html(); 模板
+                var html='';
+                    html+='<li>';
+                    html+='<div class="picture-module active">';
+                    html+='<input type="file" class="uploadImg uploadSingleEditImg" name="">';
+                    html+='<a class="delete-picture">X</a>';
+                    html+='<video src="" class="upload_img"></video>';
+                    html+='</div>';
+                    html+='<a href="javascript:void(0);" class="edit-describe">编辑照片描述</a>';
+                    html+='<textarea name="" id="" cols="30" rows="5" placeholder="请填写描述" class="edit-text"></textarea>';
+                    html+='</li>';                  
+                var multiImgAttr=obj.data('src');
+                console.log(multiImgAttr);
+                for(var i=0;i<multiImgAttr.length;i++){
+                    $('.editCompanyPicLayer .multi-picture-module').append(html);
+                    $('.editCompanyPicLayer .upload_img').eq(i).attr('src',multiImgAttr[i].imgSrc);
+                    $('.editCompanyPicLayer .edit-text').eq(i).val(multiImgAttr[i].imgText);
+                }
+            },
+            yes:function(index){
+                var layermultiImgAttr=[];
+                var layerImgInfoData={};
+                $.each($('.editCompanyPicLayer li'),function(i,val){
+                    var _this=$(this);
+                    var imgSrc=_this.find('video').attr('src');
+                    var imgText=_this.find('textarea').val();
+                    layerImgInfoData={
+                        imgSrc:imgSrc,
+                        imgText:imgText
+                    }
+                    layermultiImgAttr.push(layerImgInfoData);
+                })
+               
+                // obj.data('src',layermultiImgAttr);
+                obj.data('src',layermultiImgAttr);
+                // if(layermultiImgAttr.length==0){
+                //     layer.close(index);
+                //     return false;
+                // }
+                // var postDate = {};
+                // postDate.imgs = layermultiImgAttr;
+                // $.post('uploadMultiImgToTemp',postDate,function(info){
+                //    if(info.status == 0){
+                //        dialog.error(info.msg);
+                //        return false;
+                //    }
+                //     var imgArray = [];
+                //     $.each(info.info,function(index,img){
+                //         if(img.indexOf("uploads") == -1 && img !=''){
+                //             img = uploads+img;
+                //         }
+                //         imgArray.push(img);
+                //     });
+
+                //     $('.goods-detail').data('src',imgArray);
+                //     layer.close(index);
+                // })
+                layer.close(index);
+            },
+            no:function(){
+                $('.editCompanyPicLayer li').remove();
+            }
+        })
 }
 
