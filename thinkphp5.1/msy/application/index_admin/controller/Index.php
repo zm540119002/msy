c4924b2356016659d35c4c0439b2137596075a0a
zhangmin <540119002@qq.com>
2018-04-20 14:47:16 +0800

    zm

--- thinkphp5.1/msy/application/index_admin/controller/Index.php
+++ thinkphp5.1/msy/application/index_admin/controller/Index.php
@@ -6,13 +6,6 @@ class Index extends \common\controller\UserBase
     //首页
     public function index()
     {
-        $menu = new \common\lib\Menu();
-        if($this->user['type']==0){//超级管理员
-            $menuList = $menu->getAllDisplayMenu();
-        }else{
-            $menuList = $menu->getOwnDisplayMenu();
-        }
-        $this->assign('menu',$menuList);
         return $this->fetch();
     }
     //欢迎页
