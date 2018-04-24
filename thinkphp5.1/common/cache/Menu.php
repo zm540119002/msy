8c3cdfbe2db398153fa61c58eb6187a158bf28ba
zhangmin <540119002@qq.com>
2018-04-24 09:35:23 +0800

    zm

--- thinkphp5.1/common/cache/Menu.php
+++ thinkphp5.1/common/cache/Menu.php
@@ -2,54 +2,50 @@
 namespace common\cache;
 
 class Menu{
-    private static $_cache_key = 'cache_menu_';
+    private static $_cache_key = 'all_display_menu_';
+    private static $_cache_key_2 = 'all_menu_';
 
     /**从缓存中获取信息
      */
-    public static function get($user_id){
-        $menu = S(self::$_cache_key.$user_id);
-        if(!$menu){
-            $where = array(
-                'user_id' => $user_id,
-                'status' => 0,
-                'auth_status' => 1,
-            );
-            $modelMenu = D('Business/Menu');
-            $menu = $modelMenu->selectMenu($where);
-            $menu = $menu[0];
-            S(self::$_cache_key.$user_id, $menu, array('type'=>'file', 'expire'=>C('DEFAULT_EXPIRE')));
+    public static function getAllDisplayMenu($user){
+        $allDisplayMenu = cache(self::$_cache_key.$user['id']);
+        if(!$allDisplayMenu){
+            $menu = new \common\lib\Menu();
+            if($user['type']==0){//超级管理员
+                $allDisplayMenu = $menu->getAllDisplayMenu();
+            }else{
+                $allDisplayMenu = $menu->getOwnDisplayMenu();
+            }
+            cache(self::$_cache_key.$user['id'], $allDisplayMenu);
         }
-        return $menu;
-    }
-
-    /**从缓存中获取信息
-     */
-    public static function getByMobilePhone($mobilePhone){
-        $menu = S(self::$_cache_key.$mobilePhone);
-        if(!$menu){
-            $where = array(
-                'mobile_phone' => $mobilePhone,
-                'status' => 0,
-                'auth_status' => 1,
-            );
-            $modelMenu = D('Menu');
-            $menu = $modelMenu->selectMenu($where);
-            $menu = $menu[0];
-            S(self::$_cache_key.$mobilePhone, $menu, array('type'=>'file', 'expire'=>C('DEFAULT_EXPIRE')));
-        }
-        return $menu;
+        return $allDisplayMenu;
     }
 
     /**删除缓存信息
      */
-    public static function remove($id){
-        S(self::$_cache_key.$id, null);
+    public static function removeAllDisplayMenu($userId){
+        cache(self::$_cache_key.$userId, null);
+    }
+
+    /**从缓存中获取信息
+     */
+    public static function getAllMenu($user){
+        $allMenu = cache(self::$_cache_key_2.$user['id']);
+        if(!$allMenu){
+            $menu = new \common\lib\Menu();
+            if($user['type']==0){//超级管理员
+                $allMenu = $menu->getAllMenu();
+            }else{
+                $allMenu = $menu->getOwnMenu();
+            }
+            cache(self::$_cache_key_2.$user['id'], $allMenu);
+        }
+        return $allMenu;
     }
 
     /**删除缓存信息
      */
-    public static function removeByMobilePhone($mobilePhone){
-        S(self::$_cache_key.$mobilePhone, null);
+    public static function removeAllMenu($userId){
+        cache(self::$_cache_key_2.$userId, null);
     }
 }
-
