ba22f7dec1ae3908086d479a0adcd1e1b8be1fcc
yangguanbao <yangguanbao@qq.com>
2018-05-02 09:44:19 +0800

    ygb

--- thinkphp5.1/msy/application/factory/controller/Goods.php
+++ thinkphp5.1/msy/application/factory/controller/Goods.php
@@ -8,19 +8,20 @@ use GuzzleHttp\Psr7\Request;
 
 class Goods extends FactoryBase
 {
-    public function index()
-    {
-        $seriesModel = new seriesModel();
-        $where = [
-            ['factory_id','=',$this->factory['id']],
-        ];
-        $seriesList = $seriesModel -> selectSeries($where,[],['sort'=>'desc','id'=>'desc',]);
-        $this -> assign('seriesList',$seriesList);
-        $model = new M();
-        $goodsList = $model -> selectGoods();
-        $this -> assign('goodsList',$goodsList);
-        return $this->fetch();
-    }
+//    //
+//    public function index()
+//    {
+//        $seriesModel = new seriesModel();
+//        $where = [
+//            ['factory_id','=',$this->factory['id']],
+//        ];
+//        $seriesList = $seriesModel -> selectSeries($where,[],['sort'=>'desc','id'=>'desc',]);
+//        $this -> assign('seriesList',$seriesList);
+//        $model = new M();
+//        $goodsList = $model -> selectGoods();
+//        $this -> assign('goodsList',$goodsList);
+//        return $this->fetch();
+//    }
 
     /**
      * @return array|mixed
