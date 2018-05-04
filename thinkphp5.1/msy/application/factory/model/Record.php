eed8e8ad0dab8034b16f7c545b4ff6042da6fd25
yangguanbao <yangguanbao@qq.com>
2018-05-04 16:20:02 +0800

    ygb

--- thinkphp5.1/msy/application/factory/model/Record.php
+++ thinkphp5.1/msy/application/factory/model/Record.php
@@ -15,56 +15,15 @@ class Record extends Model {
     protected $connection = 'db_config_factory';
 
 	/**
-	 * 新增
+	 * 编辑厂商档案 新增和修改
+	 * @param string $factoryId
+	 * @return array
 	 */
-	public function add($factoryId=''){
+	public function edit($factoryId=''){
 		$data = input('post.');
 		$data['factory_id'] = $factoryId;
 		//$validate = validate('Record');
 //		if(!$result = $validate->scene('add')->check($data)) {
-//			return errorMsg($validate->getError());
-//		}
-		if(!empty($data['company_img'])){
-			$data['company_img'] = moveImgFromTemp(config('upload_dir.factory_record'),basename($data['company_img']));
-		}
-		if(!empty($data['logo_img'])){
-			$data['logo_img'] = moveImgFromTemp(config('upload_dir.factory_record'),basename($data['logo_img']));
-		}
-		if(!empty($data['rb_img'])){
-			$data['rb_img'] = moveImgsWithDecFromTemp(config('upload_dir.factory_record'),$data['rb_img']);
-		}
-		if(!empty($data['factory_video'])){
-			$data['factory_video'] = moveImgsWithDecFromTemp(config('upload_dir.factory_record'),$data['factory_video']);
-		}
-		if(!empty($data['license'])){
-			$data['license'] = moveImgsWithDecFromTemp(config('upload_dir.factory_record'),$data['license']);
-		}
-		if(!empty($data['glory_img'])){
-			$data['glory_img'] = moveImgsWithDecFromTemp(config('upload_dir.factory_record'),$data['glory_img']);
-		}
-		$data['create_time'] = time();
-		$result = $this->allowField(true)->save($data);
-		if(false !== $result){
-			return successMsg("已成功添加");
-		}else{
-			return errorMsg($this->getError());
-		}
-	}
-
-	/**
-	 * 修改
-	 */
-	public function edit($factoryId){
-		$data = input('post.');
-		$where['id'] = $data['record_id'];
-		$file = array(
-			'logo_img','company_img','rb_img','factory_video','license','glory_img'
-		);
-
-		$oldRecordInfo = $this -> getRecord($where,$file);
-		//return $oldRecordInfo;
-//		$validate = validate('Record');
-//		if(!$result = $validate->scene('edit')->check($data)) {
 //			return errorMsg($validate->getError());
 //		}
 		if(!empty($data['company_img'])){
@@ -96,50 +55,64 @@ class Record extends Model {
 			$data['glory_img'] = $rse['imgsWithDecNew'];
 			$newGloryImg = $rse['imgsArray'];
 		}
-		$data['update_time'] = time();
 
-		$result = $this->allowField(true)->save($data,['id' => $data['record_id'],'factory_id'=>$factoryId]);
-		if(false !== $result){
-			delImgFromPaths($oldRecordInfo['company_img'],$data['company_img']);
-			delImgFromPaths($oldRecordInfo['logo_img'],$data['logo_img']);
-			if(!empty($oldRecordInfo['rb_img']) && !empty($data['rb_img'])){
-				$rbImgWithDec = json_decode($oldRecordInfo['rb_img'],true);
-				$oldRbImg = [];
-				foreach ($rbImgWithDec as $item){
-					$oldRbImg[] = $item['imgSrc'];
-				}
-				delImgFromPaths($oldRbImg,$newRbImg);
-			}
-			if(!empty($oldRecordInfo['factory_video']) && !empty($data['factory_video'])) {
-				$rbImgWithDec = json_decode($oldRecordInfo['factory_video'],true);
-				$oldFactoryVideo = [];
-				foreach ($rbImgWithDec as $item){
-					$oldFactoryVideo[] = $item['imgSrc'];
-				}
-				delImgFromPaths($oldFactoryVideo,$newFactoryVideo);
-			}
-			if(!empty($oldRecordInfo['license']) && !empty($data['license'])){
-				$rbImgWithDec = json_decode($oldRecordInfo['license'],true);
-				$oldLicense = [];
-				foreach ($rbImgWithDec as $item){
-					$oldLicense[] = $item['imgSrc'];
-				}
-				delImgFromPaths($oldLicense,$newLicense);
-			}
-			if(!empty($oldRecordInfo['glory_img']) && !empty($data['glory_img'])){
-				$rbImgWithDec = json_decode($oldRecordInfo['glory_img'],true);
-				$oldGloryImg = [];
-				foreach ($rbImgWithDec as $item){
-					$oldGloryImg[] = $item['imgSrc'];
-				}
-				delImgFromPaths($oldGloryImg,$newGloryImg);
-			}
-
-			return successMsg("已修改");
+		if(input('?post.record_id') && !input('?post.record_id') == ''){
+			$where['id'] = $data['record_id'];
+			$file = array(
+				'logo_img','company_img','rb_img','factory_video','license','glory_img'
+			);
+			$oldRecordInfo = $this -> getRecord($where,$file);
+			$data['update_time'] = time();
+			$result = $this->allowField(true)->save($data,['id' => $data['record_id'],'factory_id'=>$factoryId]);
 		}else{
-			return errorMsg($this->getError());
+			$data['create_time'] = time();
+			$result = $this->allowField(true)->save($data);
+		}
+		if(false !== $result){
+			//删除旧图片
+			if(input('?post.record_id') && !input('?post.record_id') == ''){
+				delImgFromPaths($oldRecordInfo['company_img'],$data['company_img']);
+				delImgFromPaths($oldRecordInfo['logo_img'],$data['logo_img']);
+				if(!empty($oldRecordInfo['rb_img']) && !empty($data['rb_img'])){
+					$rbImgWithDec = json_decode($oldRecordInfo['rb_img'],true);
+					$oldRbImg = [];
+					foreach ($rbImgWithDec as $item){
+						$oldRbImg[] = $item['imgSrc'];
+					}
+					delImgFromPaths($oldRbImg,$newRbImg);
+				}
+				if(!empty($oldRecordInfo['factory_video']) && !empty($data['factory_video'])) {
+					$rbImgWithDec = json_decode($oldRecordInfo['factory_video'],true);
+					$oldFactoryVideo = [];
+					foreach ($rbImgWithDec as $item){
+						$oldFactoryVideo[] = $item['imgSrc'];
+					}
+					delImgFromPaths($oldFactoryVideo,$newFactoryVideo);
+				}
+				if(!empty($oldRecordInfo['license']) && !empty($data['license'])){
+					$rbImgWithDec = json_decode($oldRecordInfo['license'],true);
+					$oldLicense = [];
+					foreach ($rbImgWithDec as $item){
+						$oldLicense[] = $item['imgSrc'];
+					}
+					delImgFromPaths($oldLicense,$newLicense);
+				}
+				if(!empty($oldRecordInfo['glory_img']) && !empty($data['glory_img'])){
+					$rbImgWithDec = json_decode($oldRecordInfo['glory_img'],true);
+					$oldGloryImg = [];
+					foreach ($rbImgWithDec as $item){
+						$oldGloryImg[] = $item['imgSrc'];
+					}
+					delImgFromPaths($oldGloryImg,$newGloryImg);
+				}
+			}
+			return successMsg("成功");
+		}else{
+			return errorMsg("失败");
 		}
 	}
+
+
 	/**
 	 * @param array $where
 	 * @param array $field
@@ -149,25 +122,35 @@ class Record extends Model {
 	 * @return array|\PDOStatement|string|\think\Collection
 	 * 查询多条数据
 	 */
-	public function selectRecord($where=[],$field=[],$order=[],$limit=''){
+	public function selectRecord($where=[],$field=[],$join=[],$order=[],$limit=''){
 		$_where = array(
 			'r.status' => 0,
 		);
-
+		$_join = array(
+		);
 		$where = array_merge($_where, $where);
+		$_order = array(
+			'r.id'=>'desc',
+		);
+		$order = array_merge($_order, $order);
 		if($field){
 			$list = $this->alias('r')
 				->where($where)
 				->field($field)
+				->join(array_merge($_join,$join))
 				->order($order)
 				->limit($limit)
 				->select();
 		}else{
 			$list = $this->alias('r')
 				->where($where)
+				->join(array_merge($_join,$join))
 				->order($order)
 				->limit($limit)
 				->select();
+		}
+		if(!empty($list)){
+			$list = $list->toArray();
 		}
 		return $list;
 	}
@@ -179,20 +162,27 @@ class Record extends Model {
 	 * @return array|null|\PDOStatement|string|Model
 	 * 查找一条数据
 	 */
-	public function getRecord($where=[],$field=[]){
+	public function getRecord($where=[],$field=[],$join=[]){
 		$_where = array(
 			'r.status' => 0,
 		);
 		$where = array_merge($_where, $where);
+		$_join = array(
+		);
 		if($field){
 			$info = $this->alias('r')
 				->field($field)
+				->join(array_merge($_join,$join))
 				->where($where)
 				->find();
 		}else{
 			$info = $this->alias('r')
 				->where($where)
+				->join(array_merge($_join,$join))
 				->find();
+		}
+		if(!empty($info)){
+			$info = $info->toArray();
 		}
 		return $info;
 	}
