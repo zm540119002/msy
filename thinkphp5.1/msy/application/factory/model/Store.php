133c836ca0114424b0023c18e3f35cf6a845a5c9
webserver <webserver@qq.com>
2018-05-08 14:25:27 +0800

    zm

--- thinkphp5.1/msy/application/factory/model/Store.php
+++ thinkphp5.1/msy/application/factory/model/Store.php
@@ -6,39 +6,45 @@ use think\Db;
  * 基础模型器
  */
 
-class Brand extends Model {
+class Store extends Model {
 	// 设置当前模型对应的完整数据表名称
-	protected $table = 'brand';
-//	// 设置当前模型的数据库连接
-	protected $connection = 'db_config_factory';
+	protected $table = 'store';
+	// 设置当前模型的数据库连接
+    protected $connection = 'db_config_factory';
+//	protected $readonly = ['name'];
 
 	/**
-	 * 编辑 新增和修改
+	 * 编辑
 	 */
-	public function edit($factory_id = ''){
+	public function edit($factoryId=''){
 		$data = input('post.');
-		$validate = validate('Brand');
-		if(!$result = $validate->scene('edit')->check($data)) {
+		$data['factory_id'] = $factoryId;
+		$validate = validate('Store');
+		if(!$result = $validate->check($data)) {
 			return errorMsg($validate->getError());
 		}
-		$data['brand_img'] = moveImgFromTemp(config('upload_dir.factory_brand'),basename($data['brand_img']));
-		$data['certificate'] = moveImgFromTemp(config('upload_dir.factory_brand'),basename($data['certificate']));
-		$data['authorization'] = moveImgFromTemp(config('upload_dir.factory_brand'),basename($data['authorization']));
-		if(input('?post.brand_id')){//修改
-			$data['factory_id'] = $factory_id;
+		if(input('?post.store_id')){
 			$data['update_time'] = time();
-			$data['auth_status'] = 0;
-			$result = $this->allowField(true)->save($data, ['id' => $data['brand_id']]);
+			$result = $this->allowField(true)->save($data,['id' => $data['store_id']]);
+			if(false !== $result){
+				return successMsg("成功");
+			}else{
+				return errorMsg('失败');
+			}
 		}else{
 			$data['create_time'] = time();
 			$result = $this->allowField(true)->save($data);
-		}
-		if(false !== $result){
-			return successMsg("成功");
-		}else{
-			return errorMsg('失败');
+			if(!$result){
+				$this ->rollback();
+				return errorMsg('失败');
+			}
+			$this ->commit();
+			return successMsg('提交申请成功');
 		}
 	}
+
+
+
 
 	/**
 	 * @param array $where
@@ -49,15 +55,15 @@ class Brand extends Model {
 	 * @return array|\PDOStatement|string|\think\Collection
 	 * 查询多条数据
 	 */
-	public function selectBrand($where=[],$field=[],$order=[],$join=[],$limit=''){
+	public function selectStore($where=[],$field=[],$order=[],$join=[],$limit=''){
 		$_where = array(
-			'b.status' => 0,
+			's.status' => 0,
 		);
 		$_join = array(
 		);
 		$where = array_merge($_where, $where);
 		if($field){
-			$list = $this->alias('b')
+			$list = $this->alias('s')
 				->where($where)
 				->field($field)
 				->join(array_merge($_join,$join))
@@ -65,12 +71,15 @@ class Brand extends Model {
 				->limit($limit)
 				->select();
 		}else{
-			$list = $this->alias('b')
+			$list = $this->alias('s')
 				->where($where)
 				->join(array_merge($_join,$join))
 				->order($order)
 				->limit($limit)
 				->select();
+		}
+		if(!empty($list)){
+			$list = $list ->toArray();
 		}
 		return $list;
 	}
@@ -82,24 +91,27 @@ class Brand extends Model {
 	 * @return array|null|\PDOStatement|string|Model
 	 * 查找一条数据
 	 */
-	public function getBrand($where=[],$field=[],$join=[]){
+	public function getStore($where=[],$field=[],$join=[]){
 		$_where = array(
-			'b.status' => 0,
-		);
-		$_join = array(
+			'status' => 0,
 		);
 		$where = array_merge($_where, $where);
+		$_join = array(
+		);
 		if($field){
-			$info = $this->alias('b')
+			$info = $this->alias('s')
 				->field($field)
-				->where($where)
 				->join(array_merge($_join,$join))
+				->where($where)
 				->find();
 		}else{
-			$info = $this->alias('b')
+			$info = $this->alias('s')
 				->where($where)
 				->join(array_merge($_join,$join))
 				->find();
+		}
+		if(!empty($info)){
+			$info = $info ->toArray();
 		}
 		return $info;
 	}
