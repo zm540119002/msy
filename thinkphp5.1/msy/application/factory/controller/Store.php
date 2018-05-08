190da4e2b68dc21e24a00dd104e8b436b46b2332
yangguanbao <yangguanbao@qq.com>
2018-05-08 15:38:35 +0800

    ygb

--- thinkphp5.1/msy/application/factory/controller/Store.php
+++ thinkphp5.1/msy/application/factory/controller/Store.php
@@ -3,7 +3,7 @@ namespace app\factory\controller;
 
 use common\controller\UserBase;
 
-class Store extends UserBase
+class Store extends FactoryBase
 {
     /**
      * 店铺管理
@@ -11,11 +11,7 @@ class Store extends UserBase
     public function manage()
     {
         $model = new \app\factory\model\Store();
-        if(request()->isAjax()){
-            return $model -> edit($this -> user['id']);
-        }else{
-            return $this->fetch();
-        }
+        return $this->fetch();
     }
 
     /**
@@ -25,14 +21,41 @@ class Store extends UserBase
     {
         $model = new \app\factory\model\Store();
         if(request()->isAjax()){
-            return $model -> edit($this -> user['id']);
+            return $model -> edit($this -> factory['factory_id']);
         }else{
+            // 企业旗舰店
+            $modelFactory = new \app\factory\model\Factory();
+            $where = [['f.id','=',$this->factory['factory_id']]];
+            $file = ['f.id,f.name,r.logo_img as img'];
+            $join =[
+                ['record r','f.id = r.factory_id'],
+            ];
+            $factoryStore =  $modelFactory -> getFactory($where,$file,$join);
+            $this -> assign('factoryStore',$factoryStore);
+            //企业品牌旗舰店名
+            $modelFactory = new \app\factory\model\Brand();
+            $where = [['b.factory_id','=',$this->factory['factory_id']]];
+            $file = ['b.id,b.name,b.brand_img as img'];
+            $brandStores =  $modelFactory -> selectBrand($where,$file);
+            $this -> assign('brandStores',$brandStores);
             return $this->fetch();
         }
     }
 
+    public function getStoreName(){
+        $model = new \app\factory\model\Factory();
+        if(request()->isAjax()){
+            $where = [];
+            $storeNameList =  $model -> selectFactory($where);
+            $this -> assign('storeNameList',$storeNameList);
+            return $this->fetch('');
+        }
+    }
+
+
+
     //设置默认产商
-    public function setDefaultFactory(){
+    public function setDefaultStore(){
         if(request()->isAjax()){
             $model = new \app\factory\model\Store();
             return $model->setDefaultFactory($this->user['id']);
