db1d17df4c3516ba718d75380a0912ec8c9f3052
zhangmin <540119002@qq.com>
2018-05-12 16:29:43 +0800

    zm

--- thinkphp5.1/msy/application/index/controller/Index.php
+++ thinkphp5.1/msy/application/index/controller/Index.php
@@ -2,23 +2,18 @@
 namespace app\index\controller;
 
 use think\Controller;
-use think\Request;
+
 class Index extends Controller
 {
+    //首页
     public function index()
     {
-//        return dump(config());
 //        return $this->request->param('aa');
         return $this->fetch();
     }
+
     public function test()
     {
-        echo __FILE__;
-        echo  realpath(dirname(__FILE__) . '/../application');
-        exit;
-        $image = \common\component\image\Image::open('./image.png');
-        print_r($image);
-        exit;
         return $this->fetch();
     }
 }
