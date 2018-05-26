9017c93fdd2b0da10872474bea7807651e9cc7f2
lxr <lxr@qq.com>
2018-05-26 15:30:52 +0800

    zcw

--- thinkphp5.1/msy/application/store/controller/cart.php
+++ thinkphp5.1/msy/application/store/controller/cart.php
@@ -7,9 +7,13 @@
  * 购物车控制器
  */
 namespace app\store\controller;
+
 use common\controller\UserBase;
 use think\facade\Session;
 use app\store\model\Cart as CartModel;
+use app\store\model\Address;
+use app\store\model\Goods;
+use app\store\model\store;
 
 /**
  * 购物车控制器
@@ -23,7 +27,7 @@ class Cart extends UserBase
     {
         parent::__construct();
         if(!is_object($this->cart)){
-            $this->cart = new CartModel();
+            $this->cart = new CartModel;
         }
         if(!$this->user_id){
             $this->user_id = Session::get('user.id');
@@ -33,32 +37,32 @@ class Cart extends UserBase
     //购物车页面列表
     public function index()
     {
-        $this->assign( 'cartList', $this->cart->getCartList($this->user_id) );
-        $address =new \app\store\model\Address();
+        $address =new Address;
         $ret = $address->getAddress($this->user_id, true);
-        if($ret){
-            if($ret['status']==1){
-                $this->assign( 'address',  $ret['data']);
-            }else{
-                $this->assign( 'address',  []);
-            }
+        if($ret['status']==1){
+            $this->assign( 'address',  $ret['data']);
         }else{
             $this->assign( 'address',  []);
         }
+        $this->assign( 'cartList', $this->cart->getCartList($this->user_id) );
         return $this->fetch();
     }
 
     /**
      * 商品加入购物车
-     *
-     * @param int $store_id 店铺ID
-     * @param int $goods_id 商品ID
-     * @param int $number 购买数量
-     * @return boolean
+     * @param $store_id
+     * @param $goods_id
+     * @param $number
+     * @return array|bool
      */
     public function addGoods($store_id, $goods_id, $number)
-    {
-        return $this->cart->addGoods(Session::get('user.id'), $store_id, $goods_id, $number);
+    {   //判断商品是否可加入购物车
+        $goods = new Goods;
+        $ret = $goods->hasGoods($store_id, $goods_id, $number);
+        if($ret['status']!==1){
+            return $ret;
+        }
+        return $this->cart->addGoods($this->user_id, $store_id, $goods_id, $number);
     }
 
 }
--- thinkphp5.1/msy/application/store/model/address.php
+++ thinkphp5.1/msy/application/store/model/address.php
@@ -9,18 +9,13 @@
 namespace app\store\model;
 
 use think\Model;
-use think\Db;
 
 class Address extends Model
 {
     protected $table = 'address';
     protected $connection = 'db_config_factory';
     protected $pk = 'address_id';
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    protected $autoWriteTimestamp = true; //自动时间戳
 
     /**
      * 返回用户地址列表
@@ -29,9 +24,8 @@ class Address extends Model
      * @return array
      */
     public function getAddress($user_id, $is_default=false)
-    {
-        $address = Db::table('msy_factory.address')
-            ->where(['user_id'=>$user_id])->order('address_id asc')->select();
+    {   
+        $address = $this->where(['user_id'=>$user_id])->order('address_id asc')->select()->toArray();
         if(!$address||!is_array($address)||count($address)<=0){
             return errorMsg('请添加地址');
         }
@@ -46,19 +40,72 @@ class Address extends Model
         return successMsg('返回第一条为默认地址', [ 'data'=>[0=>$address[0]] ]);
     }
 
-    public function addAddress($user_id)
+    /**
+     * 判断地址是否存在
+     * @param $user_id
+     * @param $address_id
+     * @return array
+     */
+    public function isAddress($user_id, $address_id)
     {
-        //添加地址
+        $ret = $this->where(['user_id'=>$user_id, 'address_id'=>$address_id])->count();
+        if($ret>0){
+            return successMsg('地址可用');
+        }
+        return errorMsg('地址不存在');
     }
 
-    public function setDefault($user_id)
+    /**
+     * 新增地址
+     * @param $user_id
+     * @param array $data
+     * @return array
+     */
+    public function addAddress($user_id, array $data)
     {
-        //设置默认地址
+        $count = $this->where(['user_id'=>$user_id])->count();
+        if($count>=5){
+            return errorMsg('最多只能5个地址');
+        }
+        $ret = static::create($data, ['user_id', 'consignee', 'phone', 'detail', 'is_default']);
+        if($ret){
+            return successMsg('新增地址成功');
+        }
+        return errorMsg('新增地址失败');
     }
 
-    public function updateAddress($user_id)
+    /**
+     * 设置默认地址
+     * @param $user_id
+     * @param $address_id
+     * @return array
+     */
+    public function setDefault($user_id, $address_id)
+    {
+        $ret = $this->allowField('is_default')
+            ->save(['is_default'=>1], ['address_id'=>$address_id, 'user_id'=>$user_id]);
+        if($ret){
+            return successMsg('设置默认地址成功');
+        }
+        return errorMsg('设置默认地址失败');
+    }
+
+    /**
+     * 修改地址
+     * @param $user_id
+     * @param $address_id
+     * @param array $data
+     * @return array
+     */
+    public function updateAddress($user_id, $address_id, array $data)
     {
         //修改地址
+        $ret = $this->allowField(['consignee', 'phone', 'detail', 'is_default'])
+            ->save($data, ['address_id'=>$address_id, 'user_id'=>$user_id]);
+        if($ret){
+            return successMsg('修改地址成功');
+        }
+        return errorMsg('修改地址失败');
     }
 
 }
