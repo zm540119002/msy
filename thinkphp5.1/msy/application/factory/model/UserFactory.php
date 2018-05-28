32764bd1e2f48cfa2463d1d82dfa27d3f4b6e21b
yangguanbao <yangguanbao@qq.com>
2018-05-28 12:01:20 +0800

    ygb

--- thinkphp5.1/msy/application/factory/model/UserFactory.php
+++ thinkphp5.1/msy/application/factory/model/UserFactory.php
@@ -4,6 +4,8 @@ namespace app\factory\model;
 class UserFactory extends \think\Model {
 	// 设置当前模型对应的完整数据表名称
 	protected $table = 'user_factory';
+	// 设置主键
+	protected $pk = 'id';
 	// 设置当前模型的数据库连接
     protected $connection = 'db_config_factory';
 
