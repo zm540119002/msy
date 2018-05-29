80af58de03bc035c0e076968f401a19703800a4f
zhangmin <540119002@qq.com>
2018-05-29 15:45:27 +0800

    zm

--- thinkphp5.1/common/cache/Factory.php
+++ thinkphp5.1/common/cache/Factory.php
@@ -3,45 +3,28 @@ namespace common\cache;
 
 class Factory{
     private static $_cache_key = 'cache_factory_';
-
     /**从缓存中获取信息
      */
-    public static function getByUserId($userId){
-        $factory = cache(self::$_cache_key.$userId);
+    public static function get($factoryId){
+        $factory = cache(self::$_cache_key.$factoryId);
         if(!$factory){
-            $model = new \app\factory\model\UserFactory();
-            $file = [
-                'u.id,u.factory_id,u.is_default,f.name'
+            $modelFactory = new \app\factory\model\Factory();
+            $where = [
+                'status' => 0,
+                'id' => $factoryId,
             ];
-            $join =[
-                ['factory f','f.id = u.factory_id'],
+            $field = [
+                'id','name',
             ];
-            if($factoryCount > 1){
-                $_where = [
-                    ['u.user_id','=',$userId],
-                    ['u.is_default','=',1],
-                ];
-
-                if(!$factoryInfo){
-                    $this->success('你有多家厂商入住，请选择一家', 'Index/index');
-                }
-            }elseif ($factoryCount == 1){
-                $_where = [
-                    ['u.user_id','=',$userId],
-                ];
-                $factoryInfo = $model -> getInfo($where_new,$file,$join);
-            }elseif (!$factoryCount){
-                $this->success('没有产商入住，请入住', 'Deploy/register');
-            }
-            $factory = $model-> getInfo($_where,$file,$join);
-            cache(self::$_cache_key.$userId, $factory,config('custom.factory_cache_time'));
+            $factory = $modelFactory->getInfo($where,$field);
+            cache(self::$_cache_key.$factoryId, $factory,config('custom.factory_cache_time'));
         }
         return $factory;
     }
 
     /**删除缓存信息
      */
-    public static function removeByUserId($userId){
-        cache(self::$_cache_key.$userId, null);
+    public static function remove($factoryId){
+        cache(self::$_cache_key.$factoryId, null);
     }
 }
--- thinkphp5.1/msy/application/factory/controller/FactoryBase.php
+++ thinkphp5.1/msy/application/factory/controller/FactoryBase.php
@@ -9,55 +9,18 @@ class FactoryBase extends UserBase{
     
     public function __construct(){
         parent::__construct();
-        $model = new \app\factory\model\UserFactory();
+        $modelUserFactory = new \app\factory\model\UserFactory();
         $where = [
+            ['status','=',0],
+            ['is_default','=',1],
             ['user_id','=',$this->user['id']],
         ];
-        $factoryCount = $model->where($where)->count('id');
-        if($factoryCount > 1){
-            $this->success('你有多家厂商入住，请选择一家', 'Index/index');
-        }elseif ($factoryCount == 1){
-            $factoryInfo = $model->getInfo($where,$file,$join);
-        }elseif (!$factoryCount){
-            $this->success('没有产商入住，请入住', 'Deploy/register');
+        $info = $modelUserFactory->where($where)->field('factory_id')->find()->toArray();
+        $factoryId = $info['factory_id'];
+        if (!$factoryId){
+            $this->error('没有产商入住，请入住', 'Deploy/register');
         }
-        if(empty($factoryInfo)){
-            $factoryInfo = $this ->_getFactory();
-        }
-        $this -> factory =  $factoryInfo;
-    }
-
-    private function _getFactory(){
-        $model = new \app\factory\model\UserFactory();
-        $uid = $this -> user['id'];
-        $where = [
-            ['user_id','=',$uid],
-        ];
-        $factoryCount = $model -> where($where)->count('id');
-        $file = [
-            'u.id,u.factory_id,u.is_default,f.name'
-        ];
-        $join =[
-            ['factory f','f.id = u.factory_id'],
-        ];
-        if($factoryCount > 1){
-            $_where = [
-                ['u.user_id','=',$uid],
-                ['u.is_default','=',1],
-            ];
-            $factoryInfo = $model -> getInfo($_where,$file,$join);
-            if(!$factoryInfo){
-                $this->success('你有多家厂商入住，请选择一家', 'Index/index');
-            }
-        }elseif ($factoryCount == 1){
-            $where_new = [
-                ['u.user_id','=',$uid],
-            ];
-            $factoryInfo = $model -> getInfo($where_new,$file,$join);
-        }elseif (!$factoryCount){
-            $this->success('没有产商入住，请入住', 'Deploy/register');
-        }
-        Session::set('factory',$factoryInfo);
-        return  Session::get('factory');
+        \common\cache\Factory::remove($factoryId);
+        $this->factory = \common\cache\Factory::get($factoryId);
     }
 }
