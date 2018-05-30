0a31df4decec425cbb7c9d593f5ac08241903806
yangguanbao <yangguanbao@qq.com>
2018-05-30 14:57:53 +0800

    ygb

--- thinkphp5.1/msy/application/factory/controller/StoreBase.php
+++ thinkphp5.1/msy/application/factory/controller/StoreBase.php
@@ -19,8 +19,7 @@ class StoreBase extends FactoryBase
         $where = [
             ['factory_id', '=', $this->factory['id']],
         ];
-<<<<<<< HEAD
-        $list = $model->getList($where);
+        $list = $model -> getList($where);
         $count = count($list);
         if ($count > 1) {
             //多家店判断是否有默认店铺
@@ -28,29 +27,14 @@ class StoreBase extends FactoryBase
             $list = $model -> getList($where);
             $countDefault = count($list);
             if (!$countDefault) {
-
-                $this->success('你有多家店，请选择一家', 'Store/operaManageIndex');;
-=======
-        $storeCount = $model -> where($where)->count('id');
-        $file = [
-            's.id,s.factory_id,s.is_default,s.store_type,run_type,auth_status'
-        ];
-        if($storeCount > 1){
-            $_where = [
-                ['s.factory_id','=',$this->factory['factory_id']],
-                ['s.is_default','=',1],
-            ];
-            $storeInfo = $model -> getInfo($_where,$file);
-            if(!$storeInfo){
-                $this -> assign('notDefaultStore',1);
-                $this->success('你有多家店，请选择一家', 'Store/operaManageIndex');
->>>>>>> 46f893db6dd9beb061cfce55dab45953de4df793
+                $this -> assign('notDefaultStore', 1);
+                $this -> success('你有多家店，请选择一家', 'Store/operaManageIndex');;
             }
         } elseif (!$count) {
-            $this->success('没有店铺，请申请', 'Store/deployIndex');
+            $this -> success('没有店铺，请申请', 'Store/deployIndex');
         }
         $info = $list[0];
         \common\cache\Store::remove($info['id']);
-        $this->store = \common\cache\Store::get($info['id']);
+        $this -> store = \common\cache\Store::get($info['id']);
     }
 }
