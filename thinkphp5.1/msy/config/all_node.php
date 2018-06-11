bbb2ab7f2e3ff52319b8b853361d6a13aef1ef0d
webserver <webserver@qq.com>
2018-06-11 11:58:53 +0800

    zm

--- thinkphp5.1/msy/config/all_node.php
+++ thinkphp5.1/msy/config/all_node.php
@@ -10,11 +10,5 @@ return [
                 ['id'=>2,'name'=>'账号管理','display'=>1,'controller'=>'User','action'=>'info',],
             ],
         ],
-        'system'=>[
-            'id'=>10,'name'=>'系统管理','type'=>1,
-            'sub_menu' => [
-                ['id'=>11,'name'=>'角色管理','display'=>1,'controller'=>'Role','action'=>'manage',],
-            ],
-        ],
     ],
 ];
