a241abd1feee7839756f4236c4617423f3721545
yangguanbao <yangguanbao@qq.com>
2018-06-11 11:47:00 +0800

    ygb

--- thinkphp5.1/msy/application/factory/controller/Record.php
+++ thinkphp5.1/msy/application/factory/controller/Record.php
@@ -27,13 +27,12 @@ class Record extends FactoryBase
         $where = [
             ['r.factory_id','=',$this->factory['id']],
         ];
-        $file = ['r.id,r.shop_name,r.introduction,r.factory_video,r.logo_img,r.license,r.glory_img,r.provinces,r.detail_address,
+        $file = ['r.id,r.shop_name,r.introduction,r.factory_video,r.logo_img,r.rb_img,r.license,r.glory_img,r.provinces,r.detail_address,
         r.company_img,r.create_time,r.update_time,f.name'];
         $join = [
             ['factory f','f.id = r.factory_id'],
         ];
         $recordInfo = $model -> getInfo($where,$file,$join);
-//        $recordInfo = $model::hasWhere('factory',['id'=>$this->factory['id']])->field('factory.name')->find();
         if(!empty($recordInfo)){
             $recordInfo['factory_video'] = json_decode($recordInfo['factory_video'],true);
             $recordInfo['rb_img'] = json_decode($recordInfo['rb_img'],true);
