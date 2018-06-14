f1214a39b5ae0a1c94db1ea38ae7888d63641a8c
yangguanbao <yangguanbao@qq.com>
2018-06-14 11:14:23 +0800

    ygb

--- thinkphp5.1/msy/application/factory/validate/Record.php
+++ thinkphp5.1/msy/application/factory/validate/Record.php
@@ -1,32 +1,29 @@
 <?php
 namespace app\factory\validate;
 
-use think\Validate;
 
-class Record extends Validate
+class Record extends \think\Validate
 {
-
     protected $rule = [
-        'name'  =>  'require|max:25',
-        'agent' =>  'require|max:50',
-        'business_license' =>  'require|max:50',
-        'auth_letter' =>  'require|max:150',
+        'company_img'  =>  'require',
+        'provinces' =>  'require',
+        'detail_address' =>  'require',
+        'logo_img' =>  'require',
+        'introduction' =>  'require',
+        'factory_video' =>  'require',
+        'rb_img' =>  'require',
+        'license' =>  'require',
+        'glory_img' =>  'require',
     ];
     protected $message  =   [
-        'name.require' => '产商全称必须填写',
-        'name.max'     => '产商全称最多不能超过25个字符',
-        'agent.require'   => '代理人名字必须填写',
-        'agent.max'   => '代理人名字最多不能超过25个字符',
-        'business_license.require'   => '营业执照必须上传',
-        'business_license.max'   => '营业执照存在地址过长',
-        'auth_letter.require'   => '授权信必须上传',
-        'auth_letter.max'   => '授权信存在地址最多不能超过25个字符',
+        'company_img.require' => '请上传企业形象图片',
+        'provinces.require'   => '请选择所在的省市区',
+        'detail_address.require'   => '请填写详细地址',
+        'logo_img.require'   => '请上传企业标志图',
+        'introduction.require'   => '请编辑企业简介',
+        'factory_video.require'   => '请上传企业视频',
+        'rb_img.require'   => '请上传企业研发生产图片',
+        'license.require'   => '请上传执照资质图片',
+        'glory_img.require'   => '请上传专利荣誉图片',
     ];
-    
-    protected $scene = [
-        'add'   =>  ['name','agent','business_license','auth_letter'],
-        'edit'  =>  ['name','agent','business_license','auth_letter'],
-    ];
-
-
 }
