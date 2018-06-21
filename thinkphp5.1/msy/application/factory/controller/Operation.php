9c0405f7d12822ebf529b540c8728a412a7679be
yangguanbao <yangguanbao@qq.com>
2018-06-21 16:03:26 +0800

    ygb

--- thinkphp5.1/msy/application/factory/controller/Operation.php
+++ thinkphp5.1/msy/application/factory/controller/Operation.php
@@ -9,6 +9,22 @@ class Operation extends StoreBase
     }
     //店铺管理设置页
     public function set(){
+        if(request()->isPost()){
+            $logoImg = input('post.logo_img');
+            if(empty($logoImg)){
+                return errorMsg('参数错误');
+            }
+            $model = new \app\factory\model\Store();
+            $where = [
+                ['id','=', $this->store['id']]
+            ];
+            $result = $model->where($where)->setField('logo_img',$logoImg);
+            if(false == $result){
+                return errorMsg('优化失败');
+            }else{
+                return successMsg('成功');
+            }
+        }
         return $this->fetch();
     }
 
