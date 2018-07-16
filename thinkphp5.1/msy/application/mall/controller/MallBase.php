d38967de50ace6b4b42abc4b139ad63a824eff69
zhangmin <540119002@qq.com>
2018-07-16 16:14:52 +0800

    zm

--- thinkphp5.1/msy/application/mall/controller/MallBase.php
+++ thinkphp5.1/msy/application/mall/controller/MallBase.php
@@ -2,7 +2,6 @@
 namespace app\mall\controller;
 
 class MallBase extends \common\controller\UserBase{
-    protected $twitter = null;
     public function __construct(){
         parent::__construct();
     }
