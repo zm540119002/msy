9164ef1d400ac4154d464df4019823dc61979dec
zhangmin <540119002@qq.com>
2018-07-16 16:48:12 +0800

    zm

--- thinkphp5.1/msy/application/mall/controller/Index.php
+++ thinkphp5.1/msy/application/mall/controller/Index.php
@@ -7,4 +7,16 @@ class Index extends MallBase{
     public function index(){
         return $this->fetch();
     }
+
+    /**供应商零售店
+     */
+    public function factoryRetailStore(){
+        return $this->fetch();
+    }
+
+    /**美容机构门店
+     */
+    public function saleStore(){
+        return $this->fetch();
+    }
 }
