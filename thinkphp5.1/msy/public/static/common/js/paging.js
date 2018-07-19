ef658bba9fd7beeeed053062f350b3d63e186e9e
zhangmin <540119002@qq.com>
2018-07-19 14:27:44 +0800

    zm

--- thinkphp5.1/msy/public/static/common/js/paging.js
+++ thinkphp5.1/msy/public/static/common/js/paging.js
@@ -1,25 +1,33 @@
 //下拉获取分页列表-公共回调函数
-function pullDownListCallBack(){
+function getPagingListCallBack(obj,data){
+    obj.html(data);
 }
 
 //下拉获取分页列表
 var currentPage = 1;//记录当前页
 var requestEnd = false;//请求结束标记
-function pullDownList(config,postData) {
-    var _postData = $.extend({},postData);
-    _postData.page = currentPage ? currentPage : 1;
-    _postData.pageSize = _postData.pageSize?_postData.pageSize:4;
+function getPagingList(config,postData) {
+    //容器
+    config.container = config.container?config.container:$("#list");
+    //提交路径
+    config.url = config.url?config.url:action;
+    //type为true时为下拉分页,默认为普通分页
+    config.type = config.type?config.type:false;
+    //回调函数名
+    config.callBack = config.callBack?config.callBack:getPagingListCallBack;
+    //要提交的数据
+    postData = postData?postData:$('#form1').serializeObject();
+    postData.page = postData.currentPage ? postData.currentPage : 1;
+    postData.pageSize = postData.pageSize?postData.pageSize:4;
     //请求结束标志
-    if(requestEnd){
+    if(config.type && requestEnd){
         dialog.error('没有更多啦');
         loadTrigger = true;
         return false;
     }
-    config.url = config.url?config.url:action;
-    config.callBack = config.callBack?config.callBack:pullDownListCallBack;
     $.ajax({
-        url: url,
-        data: _postData,
+        url: config.url,
+        data: postData,
         type: 'get',
         dataType:'json',
         beforeSend: function(){
@@ -30,8 +38,7 @@ function pullDownList(config,postData) {
             dialog.error('AJAX错误');
         },
         success: function(data){
-            config.callBack(data);
+            config.callBack(config.container,data);
         }
     });
 }
-
