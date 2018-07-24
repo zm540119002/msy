622af226faf125d3f029b029b38449944ffd32b9
zhangmin <540119002@qq.com>
2018-07-24 16:44:07 +0800

    zm

--- thinkphp5.1/msy/application/mall/controller/RetailStore.php
+++ thinkphp5.1/msy/application/mall/controller/RetailStore.php
@@ -28,7 +28,7 @@ class RetailStore extends MallBase{
                 ],
             ];
             $list = $modelGoods->pageQuery($config);
-            return $list->listRows();
+            $this->assign('listRows',$list->listRows());
             return view('list_tpl',['list'=>$list]);
         }else{
             return $this->fetch();
