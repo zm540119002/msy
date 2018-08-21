648cb1f54e0c126c6dbd6c3c52904c3e965c300f
zhuguangfu <zhuguangfu@qq.com>
2018-08-21 14:58:38 +0800

    zgf

--- thinkphp5.1/msy/public/static/common/js/uploadMultiImg.js
+++ thinkphp5.1/msy/public/static/common/js/uploadMultiImg.js
@@ -401,6 +401,7 @@ function uploadsImgDescribe(content,obj){
             btn:['确定','取消'],
             success:function(){
                 var winHeight=$(window).height();
+                document.documentElement.scrollTop = document.body.scrollTop = 0
                 $('html,body').addClass('scrollStyle');
                 $('.editCompanyPicLayer .layui-m-layercont').css('height',winHeight-112+'px');
                 $('.layui-m-layer .layui-m-layermain').addClass('arrow-bottom');
