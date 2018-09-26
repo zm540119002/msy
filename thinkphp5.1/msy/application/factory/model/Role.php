84dd092ccc7eb84d02f7ad7bd33bb81fe035142b
zhangmin <540119002@qq.com>
2018-09-26 10:55:49 +0800

    zm

--- thinkphp5.1/msy/application/factory/model/Role.php
+++ thinkphp5.1/msy/application/factory/model/Role.php
@@ -52,7 +52,7 @@ class Role extends \common\model\Base {
 			'id'=>'desc',
 		];
 		$list = $this->where($where)->field($field)->order($order)->select();
-		return json($list);
+		return count($list)!=0?$list:[];
 	}
 
 	//删除
