ab02f0bf0be80eec6d682af3ac46b47ea2fa2053
yangguanbao <yangguanbao@qq.com>
2018-09-30 09:44:54 +0800

    ygb

--- thinkphp5.1/msy/application/mall/controller/Index.php
+++ thinkphp5.1/msy/application/mall/controller/Index.php
@@ -21,7 +21,7 @@ class Index extends MallBase{
     public function pay(){
         $payInfo = array(
             'sn'=>generateSN(10),
-            'actually_amount'=>1,
+            'actually_amount'=>0.01,
             'cancel_back' => url('payCancel'),
             'fail_back' => url('payFail'),
             'success_back' => url('payComplete'),
