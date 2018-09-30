8f7370201a10c3ec4bf7c8bdd6ef2269e22ef9ed
yangguanbao <yangguanbao@qq.com>
2018-09-30 09:16:53 +0800

    ygb

--- thinkphp5.1/msy/application/mall/controller/Index.php
+++ thinkphp5.1/msy/application/mall/controller/Index.php
@@ -27,7 +27,7 @@ class Index extends MallBase{
             'success_back' => url('payComplete'),
             'notify_url'=>config('wx_config.call_back_url'),
         );
-        print_r($payInfo);exit;
+        print_r(config('wx_config'));exit;
         Pay::wxPay($payInfo);
     }
 
