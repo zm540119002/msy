f5e38acd55f64c8baed9f4722335de27ffcedf69
yangguanbao <yangguanbao@qq.com>
2018-09-30 10:03:38 +0800

    ygb

--- thinkphp5.1/common/lib/Pay.php
+++ thinkphp5.1/common/lib/Pay.php
@@ -148,6 +148,7 @@ EOF;
         $input->SetTrade_type("MWEB");				//支付类型
         $order2 = \WxPayApi::unifiedOrder($input);	//统一下单
         $url = $order2['mweb_url'];
+        print_r($order2);exit;
         $html = <<<EOF
             <head>
                <script type="text/javascript" src="/static/common/js/jquery/jquery-1.9.1.min.js"></script>
