e16cae9bddb1b140e03dca510880f9d278c9e710
yangguanbao <yangguanbao@qq.com>
2018-10-08 15:52:36 +0800

    ygb

--- thinkphp5.1/common/component/payment/weixin/WxPay.JsApiPay.php
+++ thinkphp5.1/common/component/payment/weixin/WxPay.JsApiPay.php
@@ -103,8 +103,8 @@ class JsApiPay
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,FALSE);
 		curl_setopt($ch, CURLOPT_HEADER, FALSE);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-		if(C('WX_CONFIG')['CURL_PROXY_HOST'] != "0.0.0.0"
-			&& C('WX_CONFIG')['CURL_PROXY_PORT'] != 0){
+		if(config('wx_config.curl_proxy_host') != "0.0.0.0"
+			&&config('wx_config.curl_proxy_port')  != 0){
 			curl_setopt($ch,CURLOPT_PROXY, config('wx_config.curl_proxy_host'));
 			curl_setopt($ch,CURLOPT_PROXYPORT, config('wx_config.curl_proxy_port'));
 		}
