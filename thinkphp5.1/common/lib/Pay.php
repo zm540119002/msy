73053d61bb46614504515f632432933eb7604c64
yangguanbao <yangguanbao@qq.com>
2018-10-08 16:09:51 +0800

    ygb

--- thinkphp5.1/common/lib/Pay.php
+++ thinkphp5.1/common/lib/Pay.php
@@ -118,8 +118,7 @@ EOF;
     public static function payQRcode($url){
         //生成二维码图片
         $object = new \common\component\code\Qrcode();
-        $qrcodePath = WEB_URL.'public/images/qrcode/';//保存文件路径
-        print_r($qrcodePath);exit;
+        $qrcodePath = config('uploads');//保存文件路径
         $fileName = time().'.png';//保存文件名
         $outFile = $qrcodePath.$fileName;
         $level = 'L'; //容错级别
