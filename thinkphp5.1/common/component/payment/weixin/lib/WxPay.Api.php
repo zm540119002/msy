8f7c85164a721fdd0997258ab8b0cd336888c1eb
yangguanbao <yangguanbao@qq.com>
2018-10-08 15:39:57 +0800

    ygb

--- thinkphp5.1/common/component/payment/weixin/lib/WxPay.Api.php
+++ thinkphp5.1/common/component/payment/weixin/lib/WxPay.Api.php
@@ -58,7 +58,6 @@ class WxPayApi
 		$xml = $inputObj->ToXml();
 		$startTimeStamp = self::getMillisecond();//请求开始时间
 		$response = self::postXmlCurl($xml, $url, false, $timeOut);
-		print_r($response);exit;
 		$result = WxPayResults::Init($response);
 		self::reportCostTime($url, $startTimeStamp, $result);//上报请求花费时间
 		return $result;
