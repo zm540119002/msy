301a03a52e6eb5bcd3cd9c480abf807e71a43a12
zhangmin <540119002@qq.com>
2018-10-22 15:03:09 +0800

    zm

--- thinkphp5.1/msy/application/common.php
+++ thinkphp5.1/msy/application/common.php
@@ -41,8 +41,6 @@ function getStoreType($num){
 function getRunType($num){
     return $num?config('custom.run_type')[$num]:'保留';
 }
-//获取店铺经营类型（自营还是联营）
-
 //获取店铺合作类型
 function getOperationalModel($num){
     return $num?config('custom.operational_model')[$num]:'保留';
