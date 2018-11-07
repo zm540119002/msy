cc5a642484501664cb69a91a4970110c466eb1d5
zhangmin <540119002@qq.com>
2018-11-07 10:06:36 +0800

    zm

--- thinkphp5.1/msy/application/mall/controller/CaiHui.php
+++ thinkphp5.1/msy/application/mall/controller/CaiHui.php
@@ -2,7 +2,18 @@
 namespace app\mall\controller;
 
 class CaiHui extends \think\Controller{
+    //商城首页
     public function index(){
+        return $this->fetch();
+    }
+
+    //商城商品列表页
+    public function goods(){
+        return $this->fetch();
+    }
+
+    //商品详情页
+    public function detail(){
         return $this->fetch();
     }
 }
