672ce0dd72e5dfc0f9ff26759717abf04229ae70
zhangmin <540119002@qq.com>
2018-11-12 14:15:08 +0800

    zm

--- thinkphp5.1/msy/application/store/model/Manager.php
+++ thinkphp5.1/msy/application/store/model/Manager.php
@@ -40,47 +40,34 @@ class Manager extends \common\model\Base {
 			}
 			$managerId = $this->getAttr('id');
 		}
-		if(isset($postData['id']) && intval($postData['id'])
-			&& isset($postData['userFactoryId']) && intval($postData['userFactoryId'])){//修改
+		if(isset($postData['userFactoryId']) && intval($postData['userFactoryId'])){//修改
 			$where = [
-				'id' => $postData['id'],
+				['factory_id','=',$factoryId],
+				['id','=',$postData['userFactoryId']],
+				['status','=',0],
 			];
 			$saveData = [
-				'update_time' => time(),
+				'user_id' => $managerId,
+				'user_name' => $postData['name'],
 			];
-			$res = $this->isUpdate(true)->save($saveData,$where);
+			$modelUserFactory = new \common\model\UserFactory();
+			$res = $modelUserFactory->isUpdate(true)->save($saveData,$where);
 			if($res===false){
 				$this->rollback();//事务回滚
-				return errorMsg('失败',$this->getError());
+				return errorMsg('失败',$modelUserFactory->getError());
 			}
-			//判断是否修改管理员
-			$res = $this->_checkManagerChange($factoryId,$postData['id'],$postData['userFactoryId'],$postData['mobile_phone']);
-			if($res){//修改管理员
-				$where = [
-					['factory_id','=',$factoryId],
-					['id','=',$postData['userFactoryId']],
-					['status','=',0],
-				];
-				$saveData = [
-					'user_id' => $managerId,
-				];
-				$modelUserFactory = new \common\model\UserFactory();
-				$res = $modelUserFactory->isUpdate(true)->save($saveData,$where);
-				if($res===false){
-					$this->rollback();//事务回滚
-					return errorMsg('失败',$modelUserFactory->getError());
-				}
-			}
+			$postData['user_factory_id'] = $postData['userFactoryId'];
 		}else{//新增
 			//验证用户是否已经是管理员
-			$userFactoryId = $this->_checkManager($managerId,$factoryId);
+			$userFactoryId = $this->_checkIsManager($managerId,$factoryId);
 			if($userFactoryId){//已经是为管理员
 				$this->rollback();//事务回滚
-				return errorMsg('此号码已经是管理员，请更换手机号码！');
+				return errorMsg('此号码已经是本店家管理员，请更换手机号码！');
 			}
 			$saveData = [
 				'type' => 2,
 				'user_id' => $managerId,
+				'user_name' => $postData['name'],
 				'factory_id' => $factoryId,
 				'factory_type' => $factoryType,
 			];
@@ -91,7 +78,6 @@ class Manager extends \common\model\Base {
 				return errorMsg('失败',$this->getError());
 			}
 			$userFactoryId = $modelUserFactory->getAttr('id');
-			$postData['id'] = $managerId;
 			$postData['user_factory_id'] = $userFactoryId;
 		}
 		$this->commit();//事务提交
@@ -108,8 +94,8 @@ class Manager extends \common\model\Base {
 			['u.status','=',0],
 		];
 		$field = [
-			'u.id','u.name','u.mobile_phone',
-			'uf.id user_factory_id',
+			'u.id','u.mobile_phone',
+			'uf.id user_factory_id','uf.user_name name',
 		];
 		$join = [
 			['user u','u.id = uf.user_id','left'],
@@ -147,31 +133,9 @@ class Manager extends \common\model\Base {
 		return successMsg('成功');
 	}
 
-	/**判断是否修改管理员
+	/**检查是否是本店家管理员
 	 */
-	private function _checkManagerChange($factoryId,$userId,$userFactoryId,$mobilePhone){
-		$modelUserFactory = new \common\model\UserFactory();
-		$config = [
-			'field' => [
-				'u.mobile_phone',
-			],'join' => [
-				['user u','u.id = uf.user_id','left'],
-			],'where' => [
-				['uf.factory_id','=',$factoryId],
-				['uf.id','=',$userFactoryId],
-				['uf.user_id','=',$userId],
-				['uf.status','=',0],
-				['uf.type','=',2],
-				['u.status','=',0],
-			],
-		];
-		$res = $modelUserFactory->getInfo($config);
-		return ($res['mobile_phone']==$mobilePhone)?false:true;
-	}
-
-	/**检查管理员账号
-	 */
-	private function _checkManager($userId,$factoryId){
+	private function _checkIsManager($userId,$factoryId){
 		$modelUserFactory = new \common\model\UserFactory();
 		$where = [
 			['user_id','=',$userId],
