e4dd4947982a8906cf9c3e5c7e9dbaf672e4378b
zhangmin <540119002@qq.com>
2018-11-15 17:48:32 +0800

    zm

--- thinkphp5.1/msy/application/store/model/ManagerManage.php
+++ thinkphp5.1/msy/application/store/model/ManagerManage.php
@@ -39,12 +39,13 @@ class ManagerManage extends \common\model\Base {
 			}
 			$userId = $this->getAttr('id');
 		}
+		if($postData['id'] != $userId){
+			return errorMsg('不能修改手机号码！');
+		}
 		$this->startTrans();//事务开启
 		if($postData['id'] && intval($postData['id']) && $postData['userStoreId'] && intval($postData['userStoreId'])){//修改
-			$userId = $postData['id'];
-			$postData['id'] = $postData['userStoreId'];
 			$saveData = [
-				'user_id' => $userId,
+//				'user_id' => $userId,//备注：不能修改手机号
 				'user_name' => $postData['name'],
 				'post' => $postData['post'],
 				'duty' => $postData['duty'],
