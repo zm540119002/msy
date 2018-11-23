54573095d71c1582d89a4135c129cdef97ba7406
zhangmin <540119002@qq.com>
2018-11-23 15:29:12 +0800

    zm

--- thinkphp5.1/msy/public/static/common/js/public.js
+++ thinkphp5.1/msy/public/static/common/js/public.js
@@ -456,6 +456,20 @@ function isRolling(container){
     });
 }
 
+// 移除数组中的第二项
+//array.remove(1);
+// 移除数组中的倒数第二项
+//array.remove(-2);
+// 移除数组中的第二项和第三项（从第二项开始，删除2个元素）
+//array.remove(1,2);
+// 移除数组中的最后一项和倒数第二项（数组中的最后两项）
+//array.remove(-2,-1);
+Array.prototype.remove = function(from, to) {
+    var rest = this.slice((to || from) + 1 || this.length);
+    this.length = from < 0 ? this.length + from : from;
+    return this.push.apply(this, rest);
+};
+
 //获取列表
 function getList(config) {
     $.ajax({
