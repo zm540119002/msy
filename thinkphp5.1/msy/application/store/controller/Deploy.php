d23de69695dcfc169d99637af441428f00afb9cc
yangguanbao <yangguanbao@qq.com>
2018-12-03 10:47:47 +0800

    ygb

--- thinkphp5.1/msy/application/store/controller/Deploy.php
+++ thinkphp5.1/msy/application/store/controller/Deploy.php
@@ -8,10 +8,6 @@ class Deploy extends UserBase
      */
     public function register()
     {
-
-        ini_set("display_errors","On");
-        error_reporting(E_ALL);
-
         $model = new \common\model\Factory();
         if(request()->isAjax()){
             return $model -> edit($this -> user['id'],config('custom.type'));
