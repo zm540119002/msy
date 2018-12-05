eb3b0d484ae32afd04336266387f2b3c1b3d01f9
zhangmin <540119002@qq.com>
2018-12-05 14:54:22 +0800

    zm

--- thinkphp5.1/msy/application/store/controller/CustomerService.php
+++ thinkphp5.1/msy/application/store/controller/CustomerService.php
@@ -2,16 +2,6 @@
 namespace app\store\controller;
 
 class CustomerService extends \common\controller\UserBase{
-    /**首页
-     */
-    public function index(){
-        if(request()->isAjax()){
-            return successMsg(123);
-        }else{
-            return $this->fetch();
-        }
-    }
-
     /**售前
      */
     public function beforeSale(){
