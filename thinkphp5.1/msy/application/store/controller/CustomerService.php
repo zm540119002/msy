ce870e9576897188ea6d8e55860f285b970f6548
zhangmin <540119002@qq.com>
2018-12-08 15:11:18 +0800

    zm

--- thinkphp5.1/msy/application/store/controller/CustomerService.php
+++ thinkphp5.1/msy/application/store/controller/CustomerService.php
@@ -1,6 +1,9 @@
 <?php
 namespace app\store\controller;
 
+require_once '/your/path/GatewayClient/Gateway.php';
+use \GatewayWorker\Lib\Gateway;
+
 class CustomerService extends \common\controller\UserBase{
     /**售前
      */
@@ -16,6 +19,19 @@ class CustomerService extends \common\controller\UserBase{
      */
     public function afterSale(){
         if(request()->isAjax()){
+            return successMsg(123);
+        }else{
+            return $this->fetch();
+        }
+    }
+
+    /**售后
+     */
+    public function bindUid(){
+        if(request()->isAjax()){
+            $postData = input('.post');
+            // client_id与uid绑定
+            Gateway::bindUid($postData['client_id'], $this->user['id']);
             return successMsg(123);
         }else{
             return $this->fetch();
