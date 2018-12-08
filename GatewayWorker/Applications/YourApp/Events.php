fecd292b67875656a203af57b279762b9066e9b3
zhangmin <540119002@qq.com>
2018-12-08 14:02:39 +0800

    zm

--- GatewayWorker/Applications/YourApp/Events.php
+++ GatewayWorker/Applications/YourApp/Events.php
@@ -36,10 +36,10 @@ class Events
      */
     public static function onConnect($client_id)
     {
-        // 向当前client_id发送数据
-        Gateway::sendToClient($client_id, "Hello: $client_id\r\n");
-        // 向所有人发送
-        Gateway::sendToAll("$client_id login\r\n");
+        Gateway::sendToClient($client_id, json_encode(array(
+            'type'      => 'init',
+            'client_id' => $client_id
+        )));
     }
     
    /**
