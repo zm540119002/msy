75f968b2568302ff36e5e3101faabc9301b8d019
yangguanbao <yangguanbao@qq.com>
2018-12-10 15:03:32 +0800

    admin

--- thinkphp5.1/msy/public/static/admin_pc/common/js/paging.js
+++ thinkphp5.1/msy/public/static/admin_pc/common/js/paging.js
@@ -13,6 +13,7 @@ function commonCallBack(config,data){
  * @param getData 提交数据
  */
 function getPagingList(config,getData) {
+  
     //容器
     config.container = config.container?config.container:$("#list");
     //提交路径
