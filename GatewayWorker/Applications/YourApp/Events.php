4ca4db6401a606269c370339a5d4ccc8e8729790
zhangmin <540119002@qq.com>
2018-12-12 14:03:29 +0800

    zm

--- GatewayWorker/Applications/YourApp/Events.php
+++ GatewayWorker/Applications/YourApp/Events.php
@@ -50,7 +50,7 @@ class Events
    public static function onMessage($client_id, $message)
    {
         // 向所有人发送
-//        Gateway::sendToAll("$client_id said: $message\r\n");
+        Gateway::sendToAll("$client_id said: $message\r\n");
    }
    
    /**
