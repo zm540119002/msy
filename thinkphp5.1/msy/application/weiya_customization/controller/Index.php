514b491619fbd6274cc345c1d8af113495dc9288
yangguanbao <yangguanbao@qq.com>
2018-12-13 17:28:27 +0800

    admin

--- thinkphp5.1/msy/application/weiya_customization/controller/Index.php
+++ thinkphp5.1/msy/application/weiya_customization/controller/Index.php
@@ -5,6 +5,35 @@ class Index extends \common\controller\Base{
     /**首页
      */
     public function index(){
+        //获取商品的分类
+        $modelGoodsCategory = new \app\weiya_customization\model\GoodsCategory();
+        $config =[
+            'where' => [
+                ['gc.status', '=', 0],
+                ['gc.level','=',1]
+            ], 'order'=>[
+                'sort'=>'desc',
+                'id'=>'desc'
+            ],  'limit'=>'5'
+
+        ];
+        $categoryList  = $modelGoodsCategory->getList($config);
+        $this ->assign('categoryList',$categoryList);
+        //获取精选的6个项目
+        $modelProject = new \app\weiya_customization\model\Project();
+        $config =[
+            'where' => [
+                ['p.status', '=', 0],
+                ['p.is_selection', '=', 1],
+                ['p.shelf_status','=',3]
+            ], 'order'=>[
+                'sort'=>'desc',
+                'id'=>'desc'
+            ],  'limit'=>'6'
+
+        ];
+        $projectList  = $modelProject->getList($config);
+        $this ->assign('projectList',$projectList);
         return $this->fetch();
     }
 }
