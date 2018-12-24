0f52e1406d98a7045027a124a2aaf49c32b9e4e6
zhangmin <540119002@qq.com>
2018-12-24 17:06:56 +0800

    zm

--- thinkphp5.1/msy/application/store/controller/CustomerClient.php
+++ thinkphp5.1/msy/application/store/controller/CustomerClient.php
@@ -23,35 +23,4 @@ class CustomerClient extends \common\controller\UserBase{
             return $this->fetch();
         }
     }
-
-    /**绑定用户ID
-     */
-    public function bindUid(){
-        if(request()->isAjax()){
-            $postData = input('post.');
-            // client_id与uid绑定
-            Gateway::bindUid($postData['client_id'], $this->user['id']);
-            if(!Gateway::isUidOnline($this->user['id'])){
-                return successMsg('绑定失败！');
-            }
-            return successMsg('绑定成功！',['user_id'=>$this->user['id'],'avatar'=>$this->user['avatar'],]);
-        }
-    }
-
-    /**发送消息
-     */
-    public function sendMessage(){
-        if(request()->isAjax()){
-            $postData = input('post.');
-            if(!Gateway::isUidOnline($this->user['id'])){
-                return successMsg('对方未在线！');
-            }
-            $msg = [
-                'type' => 'msg',
-                'msg' => $postData['msg'],
-            ];
-            Gateway::sendToUid($postData['to_user_id'],json_encode($msg));
-            return successMsg('发送成功！');
-        }
-    }
 }
