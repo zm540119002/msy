bd101c7504ca221b3b72939ff230459034cf0e4e
zm <540119002@qq.com>
2018-12-24 17:04:39 +0800

    zm

--- thinkphp5.1/msy/application/index/controller/CustomerService.php
+++ thinkphp5.1/msy/application/index/controller/CustomerService.php
@@ -4,25 +4,6 @@ namespace app\index\controller;
 use common\component\GatewayClient\Gateway;
 
 class CustomerService extends \common\controller\UserBase{
-    /**售前
-     */
-    public function beforeSale(){
-        if(request()->isAjax()){
-            return successMsg(123);
-        }else{
-            return $this->fetch();
-        }
-    }
-
-    /**售后
-     */
-    public function afterSale(){
-        if(request()->isAjax()){
-            return successMsg(123);
-        }else{
-            return $this->fetch();
-        }
-    }
 
     /**绑定用户ID
      */
