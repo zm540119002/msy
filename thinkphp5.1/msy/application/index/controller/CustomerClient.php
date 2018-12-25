be33fd6870f186992a47598ff7940ad10e6f20dd
zhangmin <540119002@qq.com>
2018-12-25 09:16:14 +0800

    zm

--- thinkphp5.1/msy/application/index/controller/CustomerClient.php
+++ thinkphp5.1/msy/application/index/controller/CustomerClient.php
@@ -4,6 +4,10 @@ namespace app\index\controller;
 class CustomerClient extends \common\controller\UserBase{
     //首页
     public function index(){
-        return $this->fetch();
+        if(request()->isAjax()){
+            $this->assign('user_id',$this->user['id']);
+        }else{
+            return $this->fetch();
+        }
     }
 }
