4abcfb907e7c2fa6909d22d2bc4f57b40d06d443
zhangmin <540119002@qq.com>
2018-12-25 16:51:24 +0800

    zm

--- thinkphp5.1/msy/application/store/controller/CustomerClient.php
+++ thinkphp5.1/msy/application/store/controller/CustomerClient.php
@@ -8,6 +8,27 @@ class CustomerClient extends \common\controller\UserBase{
         if(request()->isAjax()){
             return successMsg('成功');
         }else{
+            $modelChatMessage = new \common\model\ChatMessage();
+            $config = [
+                'field' => [
+                    'cm.from_id','cm.to_id','cm.content',
+                    'u.mobile_phone',
+                    'us.id user_shop_id','us.type','us.user_name name',
+                ],'join' => [
+                    ['user_shop us','s.id = us.shop_id','left'],
+                    ['common.user u','u.id = us.user_id','left'],
+                ],'where' => [
+                    ['s.status','=',0],
+                    ['us.status','=',0],
+                    ['s.user_id','=',$this->user['id']],
+                    ['s.factory_id','=',$this->factory['id']],
+                    ['s.store_id','=',$this->store['id']],
+                    ['us.type','=',3],
+                ],
+            ];
+            $list = $modelChatMessage->getList($config);
+            $this->assign('list',$list);
+            return view('list_tpl');
             return $this->fetch();
         }
     }
