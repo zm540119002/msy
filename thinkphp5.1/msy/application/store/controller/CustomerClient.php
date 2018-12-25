4b47da00085ebe859ce1c148b7c3222d44b1cb2c
zhangmin <540119002@qq.com>
2018-12-25 17:15:52 +0800

    zm

--- thinkphp5.1/msy/application/store/controller/CustomerClient.php
+++ thinkphp5.1/msy/application/store/controller/CustomerClient.php
@@ -12,18 +12,14 @@ class CustomerClient extends \common\controller\UserBase{
             $config = [
                 'field' => [
                     'cm.from_id','cm.to_id','cm.content',
-                    'u.mobile_phone',
-                    'us.id user_shop_id','us.type','us.user_name name',
+                    'u.name',
                 ],'join' => [
-                    ['user_shop us','s.id = us.shop_id','left'],
-                    ['common.user u','u.id = us.user_id','left'],
+                    ['common.user u','u.id = cm.from_id','left'],
                 ],'where' => [
-                    ['s.status','=',0],
-                    ['us.status','=',0],
-                    ['s.user_id','=',$this->user['id']],
-                    ['s.factory_id','=',$this->factory['id']],
-                    ['s.store_id','=',$this->store['id']],
-                    ['us.type','=',3],
+                    ['u.status','=',0],
+                    ['cm.status','=',0],
+                    ['cm.to_id','=',$this->user['id']],
+                    ['cm.type','=',1],
                 ],
             ];
             $list = $modelChatMessage->getList($config);
