20217a76d5178253a77ec637209eb029e32b584c
zhangmin <540119002@qq.com>
2018-12-27 14:31:57 +0800

    zm

--- thinkphp5.1/msy/application/store/controller/CustomerClient.php
+++ thinkphp5.1/msy/application/store/controller/CustomerClient.php
@@ -18,12 +18,10 @@ class CustomerClient extends \common\controller\UserBase{
                     ['cm.status','=',0],
                     ['cm.type','=',1],
                     ['cm.to_read','=',0],
-                    ['cm.from_id','=',$this->user['id']],
                     [
 //                        ['cm.from_id','=',$this->user['id']],
 //                        ['cm.to_id','=',$this->user['id']],
-                        ['cm.from_id','=',$this->user['id']],['cm.to_id','=',$this->user['id']],
-//                        'cm.from_id|cm.to_id', ['=',$this->user['id']], ['=',$this->user['id']], 'or'
+                        'cm.from_id|cm.to_id', ['=',$this->user['id']], ['=',$this->user['id']],
                     ],
                 ],'whereOr' => [
 //                    [
