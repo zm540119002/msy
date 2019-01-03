a8dc30f67cb39e45e7334611ca54cc2ab6a2701d
zm <540119002@qq.com>
2019-01-03 16:33:32 +0800

    zm

--- thinkphp5.1/msy/application/index/controller/CustomerService.php
+++ thinkphp5.1/msy/application/index/controller/CustomerService.php
@@ -30,6 +30,7 @@ class CustomerService extends \common\controller\UserBase{
                 $msg = [
                     'type' => 'msg',
                     'content' => $postData['content'],
+                    'to_user_id' => $postData['to_user_id'],
                 ];
                 Gateway::sendToUid($postData['to_user_id'],json_encode($msg));
                 $saveData['send_sign'] = 1;
