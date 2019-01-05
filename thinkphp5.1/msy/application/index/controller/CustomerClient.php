8bd3168edf668a2cca4a7ab20be2565448625e53
zhangmin <540119002@qq.com>
2019-01-05 16:03:15 +0800

    zm

--- thinkphp5.1/msy/application/index/controller/CustomerClient.php
+++ thinkphp5.1/msy/application/index/controller/CustomerClient.php
@@ -32,6 +32,17 @@ class CustomerClient extends \common\controller\UserBase{
             $this->assign('list',$messages);
             return view('list_tpl');
         }else{
+            $modelChatMessage = new \common\model\ChatMessage();
+            $config = [
+                'field' => [
+                    'count(id)',
+                ],'where' =>
+                    'status = 0 and type = 1 and to_read = 0 ' .
+                    'and (from_id = 17 and cm.to_id = ' . $this->user['id'] . ') '
+                ,
+            ];
+            $unreadCount = $modelChatMessage->getList($config);
+            $this->assign('unreadCount',$unreadCount);
             return $this->fetch();
         }
     }
