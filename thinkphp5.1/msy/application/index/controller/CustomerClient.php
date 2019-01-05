e2030018c86acd8e8c4d38c8a4463c1721e130cd
zhangmin <540119002@qq.com>
2019-01-05 16:25:34 +0800

    zm

--- thinkphp5.1/msy/application/index/controller/CustomerClient.php
+++ thinkphp5.1/msy/application/index/controller/CustomerClient.php
@@ -42,8 +42,7 @@ class CustomerClient extends \common\controller\UserBase{
                 ,
             ];
             $unreadCount = $modelChatMessage->getList($config);
-            print_r($unreadCount);exit;
-            $this->assign('unreadCount',$unreadCount['num']);
+            $this->assign('unreadCount',$unreadCount[0]['num']);
             return $this->fetch();
         }
     }
