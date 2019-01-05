ffff0d9d62b95a2e43e9576e78f2fd90e9c6bc3d
zhangmin <540119002@qq.com>
2019-01-05 17:05:05 +0800

    zm

--- thinkphp5.1/msy/application/index/controller/CustomerService.php
+++ thinkphp5.1/msy/application/index/controller/CustomerService.php
@@ -90,7 +90,7 @@ class CustomerService extends \common\controller\UserBase{
             $postData = input('post.');
             $modelChatMessage = new \common\model\ChatMessage();
             $where =
-                'status = 0 and id in (' . implode (",",$postData['messageIds']) .
+                '`status` = 0 and id in (' . implode (",",$postData['messageIds']) .
                 ') and ((from_id = ' . $postData['from_id'] . ' and to_id = ' . $this->user['id'] .') ' .
                 'or from_id = ' . $this->user['id'] . ' and to_id = ' . $postData['from_id'];
             $res = $modelChatMessage->where($where)->setField('status',2);
