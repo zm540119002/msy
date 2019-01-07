c0780b0ebc2a5ccf10ab7a8a075b11bd3ffd1c85
zhangmin <540119002@qq.com>
2019-01-07 10:11:50 +0800

    zm

--- thinkphp5.1/msy/application/store/controller/CustomerClient.php
+++ thinkphp5.1/msy/application/store/controller/CustomerClient.php
@@ -22,7 +22,7 @@ class CustomerClient extends \common\controller\UserBase{
                 ,
             ];
             $fromUserList = $modelChatMessage->getList($config);
-            print_r($fromUserList);exit;
+            print_r($modelChatMessage->getLastSql());exit;
             foreach ($fromUserList as &$fromUser){
                 $config = [
                     'field' => [
