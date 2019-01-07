fe0f7d9dc9b1623bbf02125a960a03f1c3597d5e
zhangmin <540119002@qq.com>
2019-01-07 11:25:39 +0800

    zm

--- thinkphp5.1/msy/application/index/controller/CustomerService.php
+++ thinkphp5.1/msy/application/index/controller/CustomerService.php
@@ -76,7 +76,7 @@ class CustomerService extends \common\controller\UserBase{
             $modelChatMessage = new \common\model\ChatMessage();
             $where =
                 '`status` = 0 and `read` = 0 and id in (' . implode (",",$postData['messageIds']) .
-                ') and from_id = 17 and to_id = ' . $postData['from_id'];
+                ') and from_id = ' . $this->user['id'] . ' and to_id = ' . $postData['from_id'];
             $res = $modelChatMessage->where($where)->setField('read',1);
             if($res==false){
                 return errorMsg('设置已读出错',$modelChatMessage->getError());
