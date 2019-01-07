ea7a020c02fe18963a47749667ded8c091375b17
zhangmin <540119002@qq.com>
2019-01-07 16:14:49 +0800

    zm

--- thinkphp5.1/msy/application/index/controller/CustomerService.php
+++ thinkphp5.1/msy/application/index/controller/CustomerService.php
@@ -20,7 +20,7 @@ class CustomerService extends \common\controller\UserBase{
         if(request()->isAjax()){
             $postData = input('post.');
             if($this->user['id']==$postData['to_user_id']){
-                return errorMsg('不能发给同一个账号的不同设备！');
+                return errorMsg('不能发给自己！');
             }
             $modelChatMessage = new \common\model\ChatMessage();
             $msgCreateTime = time();
