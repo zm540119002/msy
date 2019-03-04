c72c8bdd5d54594313de6b9ff3e0387d97df16e6
yangguanbao <yangguanbao@qq.com>
2019-03-04 14:12:54 +0800

    ygb

--- thinkphp5.1/mcs/application/index/controller/CenterStore.php
+++ thinkphp5.1/mcs/application/index/controller/CenterStore.php
@@ -1,6 +1,6 @@
 <?php
 namespace app\index\controller;
-class Index extends \common\controller\Base{
+class CenterStore extends \common\controller\Base{
     /**首页
      */
     public function index(){
