b8ed0bc6487eb9166cad26526fad6a5bf104ae5f
zm <540119002@qq.com>
2019-03-09 09:52:01 +0800

    zm

--- thinkphp5.1/mcs/public/static/common/js/common.js
+++ thinkphp5.1/mcs/public/static/common/js/common.js
@@ -332,26 +332,24 @@ $.fn.serializeObject = function() {
     });
     //解决checkbox未选中时，没有序列化到对象中的代码
     var checkboxes = $(this).find('input[type=checkbox]');
-    if(checkboxes.length){
-        $.each(checkboxes, function () {
-            if (o[this.name]) {
-                if (!o[this.name].push) {
-                    o[this.name] = [o[this.name]];
-                }
-                if($(this).prop('checked')) {
-                    o[this.name].push('1');
-                }else{
-                    o[this.name].push('0');
-                }
-            }else{
-                if($(this).prop('checked')){
-                    o[this.name] = '1';
-                }else{
-                    o[this.name] = '0';
-                }
+    $.each(checkboxes, function () {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
             }
-        });
-    }
+            if($(this).prop('checked')) {
+                o[this.name].push('1');
+            }else{
+                o[this.name].push('0');
+            }
+        }else{
+            if($(this).prop('checked')){
+                o[this.name] = '1';
+            }else{
+                o[this.name] = '0';
+            }
+        }
+    });
     return o;
 };
 
