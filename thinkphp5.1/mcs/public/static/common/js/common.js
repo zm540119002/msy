c52c811476c9ffafd54bb7e9aad8f35f48a0e582
zhangmin <540119002@qq.com>
2019-03-09 09:33:15 +0800

    zm

--- thinkphp5.1/mcs/public/static/common/js/common.js
+++ thinkphp5.1/mcs/public/static/common/js/common.js
@@ -322,27 +322,27 @@ $.fn.serializeObject = function() {
         .serializeArray();
     //解决checkbox未选中时，没有序列化到对象中的代码
     var checkboxes = $(this).find('input[type=checkbox]');
-    if(checkboxes.length){
-        $.each(checkboxes, function (i,obj) {
-            console.log(i);
-            if (o[this.name]) {
-                if (!o[this.name].push) {
-                    o[this.name] = [o[this.name]];
-                }
-                if($(this).prop('checked')) {
-                    o[this.name].push('1');
-                }else{
-                    o[this.name].push('0');
-                }
-            }else{
-                if($(this).prop('checked')){
-                    o[this.name] = '1';
-                }else{
-                    o[this.name] = '0';
-                }
-            }
-        });
-    }
+    // if(checkboxes.length){
+    //     $.each(checkboxes, function (i,obj) {
+    //         console.log(i);
+    //         if (o[this.name]) {
+    //             if (!o[this.name].push) {
+    //                 o[this.name] = [o[this.name]];
+    //             }
+    //             if($(this).prop('checked')) {
+    //                 o[this.name].push('1');
+    //             }else{
+    //                 o[this.name].push('0');
+    //             }
+    //         }else{
+    //             if($(this).prop('checked')){
+    //                 o[this.name] = '1';
+    //             }else{
+    //                 o[this.name] = '0';
+    //             }
+    //         }
+    //     });
+    // }
     $.each(a, function(index,val) {
         if (o[this.name]) {
             if (!o[this.name].push) {
