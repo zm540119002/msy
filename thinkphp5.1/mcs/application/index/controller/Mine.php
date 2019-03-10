15096133e2efae6ae7794afa6f714305ed2dfb89
zhangmin <540119002@qq.com>
2019-03-10 14:37:47 +0800

    zm

--- thinkphp5.1/mcs/application/index/controller/Mine.php
+++ thinkphp5.1/mcs/application/index/controller/Mine.php
@@ -4,7 +4,6 @@ namespace app\index\controller;
 class Mine extends \common\controller\Base{
     //我的首页
     public function index(){
-        
         $this->assign('user',session('user'));
         return $this->fetch();
     }
