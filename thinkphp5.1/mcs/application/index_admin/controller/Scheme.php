8e1a6bfa4c0d9dd52a666cc8962035922e7d88e7
jing <2517187814@qq.com>
2019-03-15 14:49:35 +0800

    后台完成方案设置

--- thinkphp5.1/mcs/application/index_admin/controller/Scheme.php
+++ thinkphp5.1/mcs/application/index_admin/controller/Scheme.php
@@ -10,7 +10,7 @@ class Scheme extends Base {
      *审核首页
      */
     public function manage(){
-        return $this->fetch('manage');
+        return $this->fetch();
     }
 
     /**
@@ -65,12 +65,12 @@ class Scheme extends Base {
 
             $result = $model -> allowField(true) -> save($data,$where);
 
-            if($result){
-                return successMsg('成功');
-
-            }else{
+            if($result===false){
                 $model ->rollback();
                 return errorMsg('失败');
+
+            }else{
+                return successMsg('成功');
             }
         }
     }
@@ -81,7 +81,7 @@ class Scheme extends Base {
     public function getList(){
 
         $modelProject = new \app\index_admin\model\Scheme();
-        $where = [];
+        //$where = [];
         //$where[] = ['audit','=',0];
 /*        if(isset($_GET['category_id_1']) && intval($_GET['category_id_1'])){
             $where[] = ['p.category_id_1','=',input('get.category_id_1',0,'int')];
@@ -103,11 +103,12 @@ class Scheme extends Base {
             ],
             'order'=>[
                 'sort'=>'desc',
+                'id'=>'asc',
             ],
         ];
         $list = $modelProject ->pageQuery($config);
         $this->assign('list',$list);
-        //return $list;
+
         return view('list_tpl');
 
     }
@@ -125,9 +126,6 @@ class Scheme extends Base {
 
         $config = array();
         if($id=input('post.id/d')){
-            $condition = [
-                ['id','=',$id]
-            ];
             $config = [
                 'where' => [
                     ['id','=',$id]
@@ -145,9 +143,6 @@ class Scheme extends Base {
                 ],'field' => [
                     'thumb_img'
                 ]
-            ];
-            $condition = [
-                ['id','in',$ids]
             ];
         }
 
@@ -167,45 +162,7 @@ class Scheme extends Base {
     }
 
     /**
-     * 上下架
-     */
-    public function setShelfStatus(){
-        if(!request()->isPost()){
-            return config('custom.not_post');
-        }
-        $model = new \app\index_admin\model\Project();
-        $id = input('post.id/d');
-        if(!input('?post.id') && !$id){
-            return errorMsg('失败');
-        }
-        $rse = $model->where(['id'=>input('post.id/d')])->setField(['shelf_status'=>input('post.shelf_status/d')]);
-        if(!$rse){
-            return errorMsg('失败');
-        }
-        return successMsg('成功');
-    }
-
-    /**
-     * 设置精选
-     */
-    public function setSelection(){
-        if(!request()->isPost()){
-            return config('custom.not_post');
-        }
-        $model = new \app\index_admin\model\Project();
-        $id = input('post.id/d');
-        if(!input('?post.id') && !$id){
-            return errorMsg('失败');
-        }
-        $rse = $model->where(['id'=>input('post.id/d')])->setField(['is_selection'=>input('post.is_selection/d')]);
-        if(!$rse){
-            return errorMsg('失败');
-        }
-        return successMsg('成功');
-    }
-
-    /**
-     * 单值设置
+     * 单字段设置
      */
     public function setInfo(){
         if(!request()->isPost()){
@@ -234,50 +191,5 @@ class Scheme extends Base {
         return successMsg('成功');
     }
 
-    /**
-     * 添加项目相关商品
-     * @return array|mixed
-     * @throws \Exception
-     */
-    public function addProjectGoods(){
-        if(request()->isPost()){
-            $model = new \app\index_admin\model\ProjectGoods();
-            $data = input('post.selectedIds/a');
-            $condition = [
-                ['project_id','=',$data[0]['project_id']]
-            ];
-            $model->startTrans();
-            $rse = $model -> del($condition,$tag=false);
-            if(false === $rse){
-                $model->rollback();
-                return errorMsg('失败');
-            }
-            $res = $model->allowField(true)->saveAll($data)->toArray();
-            if (!count($res)) {
-                $model->rollback();
-                return errorMsg('失败');
-            }
-            $model -> commit();
-            return successMsg('成功');
-            
-        }else{
-            if(!input('?id') || !input('id/d')){
-                $this ->error('参数有误',url('manage'));
-            }
-            // 所有商品分类
-            $model = new \app\index_admin\model\GoodsCategory();
-            $config = [
-                'where'=>[
-                    'status'=>0
-                ]
-            ];
-            $allCategoryList = $model->getList($config);
-            $this->assign('allCategoryList',$allCategoryList);
-
-            $id = input('id/d');
-            $this->assign('id',$id);
-            return $this->fetch();
-        }
-    }
 
 }
