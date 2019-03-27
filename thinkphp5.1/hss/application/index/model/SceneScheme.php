01234ca11c4e11b48ca2fd891285b9e6114e335a
jing <2517187814@qq.com>
2019-03-27 11:49:44 +0800

    1

--- thinkphp5.1/hss/application/index/model/SceneScheme.php
+++ thinkphp5.1/hss/application/index/model/SceneScheme.php
@@ -7,7 +7,7 @@ class SceneScheme extends \common\model\Base {
 	// 设置主键
 	protected $pk = 'id';
 	// 设置当前模型的数据库连接
-    protected $connection = 'db_config_hss';
+    protected $connection = 'db_config_1';
 	// 别名
 	protected $alias = 'ss';
 }
