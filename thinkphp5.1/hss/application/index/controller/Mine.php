b1c6da8fde2d417f9030494e22324e2c45ee118c
jing <2517187814@qq.com>
2019-03-30 11:55:04 +0800

    1

--- thinkphp5.1/hss/application/index/controller/Mine.php
+++ thinkphp5.1/hss/application/index/controller/Mine.php
@@ -27,20 +27,7 @@ class Mine extends \common\controller\Base{
 
     //修改头像
     public function editAvatar(){
-
-        $backtrace = debug_backtrace();
-        //p($backtrace);
-        $backtrace = array_shift($backtrace);
-        p($backtrace);
-        exit;
-        $request= request();
-        $module_name=$request->module();
-        $controller_name=$request->controller();
-        $action=$request->action();
-        p($request);
-
-        exit;
-
+        
         if(!request()->isPost()){
             return errorMsg('请求方式错误');
         }
