31eb40554230f45d84c2e722fb8556dba26f9721
jing <2517187814@qq.com>
2019-03-30 13:55:24 +0800

    1

--- thinkphp5.1/hss/application/index/controller/Mine.php
+++ thinkphp5.1/hss/application/index/controller/Mine.php
@@ -20,6 +20,7 @@ class Mine extends \common\controller\Base{
         }
         $this->assign('wallet',$wallet);
 
+        p(__CLASS__);
         $res = str_replace('/','\\',__CLASS__);
         $controller = basename($res);
         //$res = substr(__CLASS__,strrpos(__CLASS__,'\\'));
