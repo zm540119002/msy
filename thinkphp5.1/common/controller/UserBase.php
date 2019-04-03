ab5e70161a34b01fb34a6bdecf35c302754adbf5
zhangmin <540119002@qq.com>
2019-04-03 16:15:04 +0800

    zm

--- thinkphp5.1/common/controller/UserBase.php
+++ thinkphp5.1/common/controller/UserBase.php
@@ -12,11 +12,12 @@ class UserBase extends Base{
         parent::__construct();
         //判断是否登录
         $this->user = checkLogin();
+        $this->loginUrl = str_replace('/index.php','',url($this->loginUrl));
         if (!$this->user) {
             if (request()->isAjax()) {
                 $this->success('您还未登录平台，请先登录！',url($this->indexUrl),'no_login',0);
             }else{
-                $this->error('您还未登录平台，请先登录！',str_replace('/index.php','',$this->loginUrl));
+                $this->error('您还未登录平台，请先登录！',$this->loginUrl);
             }
         }
     }
