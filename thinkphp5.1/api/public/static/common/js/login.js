27d5f593c45c7d2e02e475c2ca36aa47276c652f
zm <540119002@qq.com>
2019-04-08 17:57:48 +0800

    zm

--- thinkphp5.1/api/public/static/common/js/login.js
+++ thinkphp5.1/api/public/static/common/js/login.js
@@ -53,7 +53,9 @@ function logoutDialog(){
 }
 var loginBackFunctionParameter = {};
 var loginBackFunction = function(parameter){
-    location.href = parameter.jump_url;
+    if(parameter.jump_url){
+        location.href = parameter.jump_url;
+    }
 };
 $(function(){
     //登录-弹窗事件
@@ -107,6 +109,38 @@ $(function(){
             });
         }
     });
+
+    //异步登录验证
+    $('body').on('click','.async_login',function () {
+        var jump_url = $(this).data('jump_url');
+        loginBackFunctionParameter.jump_url = jump_url;
+        var postData = {};
+        $.ajax({
+            url: jump_url,
+            data: postData,
+            type: 'post',
+            beforeSend: function(xhr){
+                $('.loading').show();
+            },
+            error:function(xhr){
+                $('.loading').hide();
+                dialog.error('AJAX错误');
+            },
+            success: function(data){
+                $('.loading').hide();
+                if(data.status==0){
+                    dialog.error(data.info);
+                }else if(data.code==1){
+                    if(data.data == 'no_login'){
+                        loginDialog();
+                    }
+                }else{
+                    loginBackFunction();
+                }
+            }
+        });
+    });
+
     //显示隐藏密码
     $('body').on('click','.view-password',function(){
         var _this=$(this);
@@ -184,37 +218,6 @@ $(function(){
             success:function(){
             },
             btn:['确定']
-        });
-    });
-
-    $('body').on('click','.my_cart,.address_manage,.recharge,.order_manage,.my_brand,.my_message,.my_collection,.my_bottom_cart',function () {
-        var jump_url = $(this).data('jump_url');
-        loginBackFunctionParameter.jump_url = jump_url;
-        var url = module+'Brand/index';
-        var postData = {};
-        $.ajax({
-            url: url,
-            data: postData,
-            type: 'post',
-            beforeSend: function(xhr){
-                $('.loading').show();
-            },
-            error:function(xhr){
-                $('.loading').hide();
-                dialog.error('AJAX错误');
-            },
-            success: function(data){
-                $('.loading').hide();
-                if(data.status==0){
-                    dialog.error(data.info);
-                }else if(data.code==1){
-                    if(data.data == 'no_login'){
-                        loginDialog();
-                    }
-                }else{
-                    location.href = jump_url;
-                }
-            }
         });
     });
 });
