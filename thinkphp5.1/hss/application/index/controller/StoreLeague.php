97e3845ace9f4c4128d4548347cbe1a5fb95120c
jing <2517187814@qq.com>
2019-04-09 10:15:11 +0800

    1

--- thinkphp5.1/hss/application/index/controller/StoreLeague.php
+++ thinkphp5.1/hss/application/index/controller/StoreLeague.php
@@ -11,6 +11,9 @@ class StoreLeague extends \common\controller\Base{
     public function index(){
         if(request()->isAjax()){
         }else{
+            // 底部菜单，见配置文件custom.footer_menu
+            $this->assign('currentPage',request()->controller().'/'.request()->action());
+            
             return $this->fetch();
         }
     }
