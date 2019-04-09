7e284b3f92302a63dead8416fd48ae6f946b4b0c
jing <2517187814@qq.com>
2019-04-09 10:11:43 +0800

    1

--- thinkphp5.1/hss/application/index/config/custom.php
+++ thinkphp5.1/hss/application/index/config/custom.php
@@ -40,7 +40,7 @@ return [
         ],
         'Cart/manage' => [
             'name' => '采购车',
-            'a'    => 'href=javascript:void(0) class=my_bottom_cart async_login data-jump_url='.url('Cart/manage'),
+            'a'    => "href=javascript:void(0) class='my_bottom_cart async_login' data-jump_url=".url('Cart/manage'),
             'icon' => 'cart',
         ],
         'Mine/index' => [
