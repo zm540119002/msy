487e44ab3c440d51f8cf6b0aa3d9085b346973d3
jing <2517187814@qq.com>
2019-04-12 10:38:28 +0800

    1

--- thinkphp5.1/common/component/payment/weixin/WxPay.JsApiPay.php
+++ thinkphp5.1/common/component/payment/weixin/WxPay.JsApiPay.php
@@ -66,13 +66,14 @@ class JsApiPay
 	 */
 	public function GetJsApiParameters($UnifiedOrderResult)
 	{
+	    p(new WxPayException);
+        exit;
 		if(!array_key_exists("appid", $UnifiedOrderResult)
 		|| !array_key_exists("prepay_id", $UnifiedOrderResult)
 		|| $UnifiedOrderResult['prepay_id'] == "")
 		{
 			//throw new WxPayException("参数错误");
-			//throw new WxPayException($UnifiedOrderResult['err_code_des']);
-			return new WxPayException($UnifiedOrderResult['err_code_des']);
+			throw new WxPayException($UnifiedOrderResult['err_code_des']);
 		}
 
 		$jsapi = new WxPayJsApiPay();
