8cec610e0a7e04d854f2545f1775418a2eff248b
zhangmin <540119002@qq.com>
2019-04-13 10:34:57 +0800

    zm

--- thinkphp5.1/msy/application/index/controller/Payment.php
+++ thinkphp5.1/msy/application/index/controller/Payment.php
@@ -59,6 +59,7 @@ class Payment extends \common\controller\Base {
 
     // 支付处理
     public function orderPayment(){
+        return 123;
 //        if( empty(input('order_sn')) || empty(input('?pay_code'))){
 //            $this -> error('参数错误');
 //        }
