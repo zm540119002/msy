3c4b949c9fc2a403e02c8ae8c245536844ddc8bc
jing <2517187814@qq.com>
2019-04-17 16:34:50 +0800

    1

--- thinkphp5.1/hss/application/index/controller/Order.php
+++ thinkphp5.1/hss/application/index/controller/Order.php
@@ -363,8 +363,8 @@ class Order extends \common\controller\UserBase
                 // system_id,order_sn
                 $curl = new \common\component\curl\Curl();
                 $res = $curl->get('https://msy.meishangyun.com/index/Order/refundOrder',array('system_id'=>3,'order_sn'=>$orderInfo['sn']));
-                p($res);
-                exit;
+/*                p($res);
+                exit;*/
                 $res = json_decode($res,true);
                 if(!$res['status']){
                     $type = false;
