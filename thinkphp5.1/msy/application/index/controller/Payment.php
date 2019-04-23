ff7fd61242dabdcbaa9427312dba115e8ffb836c
yangguanbao <yangguanbao@qq.com>
2019-04-23 16:39:33 +0800

    55

--- thinkphp5.1/msy/application/index/controller/Payment.php
+++ thinkphp5.1/msy/application/index/controller/Payment.php
@@ -121,6 +121,9 @@ class Payment extends \common\controller\Base {
                 $attach = json_encode($attach);
                 $jump_url =config('custom.system_id')[$systemId]['jump_url'];
                 $return_url = config('wx_config.return_url');
+                $tools = new \common\component\payment\weixin\Jssdk(config('wx_config.appid'), config('wx_config.appsecret'));
+                print_r($tools);exit;
+                $payOpenId  = $tools->getOpenid();
                 $payInfo = [
                     'sn'=>generateSN(),
                     'product'=>5,
