ecf68636b965b24c7fb7eb9bf7d23fbe51da3730
zhangmin <540119002@qq.com>
2019-04-25 16:59:57 +0800

    zm

--- thinkphp5.1/msy/application/index/controller/Payment.php
+++ thinkphp5.1/msy/application/index/controller/Payment.php
@@ -165,6 +165,7 @@ class Payment extends \common\controller\Base {
                 $wxPay = new \common\component\payment\weixin\weixinpay;
                 $jsApiParameters   = $wxPay::wxPay($payInfo);
                 $this -> assign('jsApiParameters',$jsApiParameters);
+                print_r(array($payInfo));
                 $this->assign('payInfo',json_encode(array($payInfo)));
 //                $this->assign('success_url',$payInfo['success_url']);
 //                $this->assign('fail_url',$payInfo['fail_url']);
