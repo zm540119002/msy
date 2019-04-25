4b4d430ceeb27b9adf90a252967819869099167c
yangguanbao <yangguanbao@qq.com>
2019-04-25 11:19:02 +0800

    55

---
+++ thinkphp5.1/msy/application/index/model/WalletDetail.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\index\model;
+
+class WalletDetail extends \common\model\Base
+{
+    // 设置当前模型对应的完整数据表名称
+    protected $table = 'wallet_detail';
+    // 设置主键
+    protected $pk = 'id';
+    // 设置当前模型的数据库连接
+    protected $connection = 'db_config_weiya';
+    //表的别名
+    protected $alias = 'wd';
+
+}
