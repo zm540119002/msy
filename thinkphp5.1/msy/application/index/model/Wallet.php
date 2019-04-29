96b064e2edef8f08496fa39b8b806c54858138e1
jing <2517187814@qq.com>
2019-04-29 15:24:25 +0800

    1

--- thinkphp5.1/msy/application/index/model/Wallet.php
+++ thinkphp5.1/msy/application/index/model/Wallet.php
@@ -7,7 +7,7 @@ class Wallet extends \common\model\Base {
 	// 设置主键
 	protected $pk = 'id';
 	// 设置当前模型的数据库连接
-    public $connection = 'db_config_weiya';
+    protected $connection = 'db_config_weiya';
 	//表的别名
 	protected $alias = 'w';
 
