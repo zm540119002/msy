1ceb2e1f9154579cd534060ee5ae4e6deaff5191
jing <2517187814@qq.com>
2019-04-29 14:59:54 +0800

    1

--- thinkphp5.1/msy/application/index/controller/Payment.php
+++ thinkphp5.1/msy/application/index/controller/Payment.php
@@ -272,10 +272,8 @@ class Payment extends \common\controller\Base {
     private function setRechargePayStatus($info){
         $modelWalletDetail= new \app\index\model\WalletDetail();
         $modelWalletDetail ->connection = config('custom.system_id')[$info['system_id']]['db'];
-        p(config('custom.system_id')[$info['system_id']]['db']);
         $modelWallet = new \app\index\model\Wallet();
         $modelWallet ->connection = config('custom.system_id')[$info['system_id']]['db'];
-        p(config('custom.system_id')[$info['system_id']]['db']);
         $condition = [
             'where' => [
                 ['status', '=', 0],
--- thinkphp5.1/msy/application/index/model/WalletDetail.php
+++ thinkphp5.1/msy/application/index/model/WalletDetail.php
@@ -8,7 +8,7 @@ class WalletDetail extends \common\model\Base
     // 设置主键
     protected $pk = 'id';
     // 设置当前模型的数据库连接
-    protected $connection = 'db_config_hss';
+    protected $connection = 'db_config_weiya';
     //表的别名
     protected $alias = 'wd';
 
