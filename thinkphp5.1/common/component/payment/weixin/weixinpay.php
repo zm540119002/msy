a17ece6bc036b859d3bf9a751b8d5f501d467112
jing <2517187814@qq.com>
2019-04-29 17:14:04 +0800

    1

--- thinkphp5.1/common/component/payment/weixin/weixinpay.php
+++ thinkphp5.1/common/component/payment/weixin/weixinpay.php
@@ -233,7 +233,7 @@ EOF;
             exit;*/
 
             //$xml = json_decode(file_get_contents('./array2.json'),true);
-            $xml = json_decode(file_get_contents('./array2.json'),true);
+            $xml = json_decode(file_get_contents('./array.json'),true);
             echo 11111;
             //exit;
             $data = \WxPayResults::Init($xml);
