dac89eba9ea52d7c914181db173aa43abed1d6a5
jing <2517187814@qq.com>
2019-05-05 10:00:14 +0800

    1

--- thinkphp5.1/common/component/payment/weixin/weixinpay.php
+++ thinkphp5.1/common/component/payment/weixin/weixinpay.php
@@ -282,8 +282,7 @@ EOF;
 
     // 确定退款
     public function refundOrder($data){
-        p($data);
-        exit;
+
         try {
             $this->getWxOpenid();
             $input = new \WxPayRefund();
