b920a4521d5fe45641a3304947b82a8c88b12eaf
zhuguangfu <zhuguangfu@qq.com>
2019-05-08 10:48:59 +0800

    zgf

--- thinkphp5.1/hss/public/static/common/js/register.js
+++ thinkphp5.1/hss/public/static/common/js/register.js
@@ -33,55 +33,7 @@ $(function(){
             $('.apply-module:eq(1)').show();
         }
     });
-    //验证与 提交申请
-    $('body').on('click','.two-step',function(){
-        name=trim($('.name').val(),'g');
-        applicant=trim($('.applicant').val(),'g');
-        id = $('.id').val();
-        var content='';
-       var content='';
-        if(!postData.name){
-            content='请填写店家名称';
-        }else if(!postData.applicant){
-            content='请填写申请人姓名';
-        }else if(!register.phoneCheck(postData.mobile)){
-            content='请填写手机号码';
-        }else if(!area_address){
-            content='请选择地区';
-        }else if(!postData.detail_address){
-            content='请填写详细地址';
-        }
-        applicantData={
-            id : id,
-            name:name,
-            applicant:applicant,
-            mobile:mobile,
-            area_address:area_address,
-            detail_address:detail_address
-        };
-        if(content){
-            dialog.error(content);
-            return false;
-        }
-        var _this = $(this);
-        _this.addClass("nodisabled");
-        console.log(postData);
-        $.post(controller + 'register',postData,function(msg){
-            _this.removeClass("nodisabled");
-            if(msg.status == 0){
-                dialog.error(msg.info);
-            }
-            if(msg.status == 1){
-                $('.weui-flex-item:eq(1)').removeClass('current');
-                $('.weui-flex-item:eq(2)').addClass('current');
-                $('.apply-module:eq(1)').hide();
-                $('.apply-module:eq(2)').show();
-                $('.weui-flex-item:eq(0)').addClass('disabled');
-                $('.weui-flex-item:eq(1)').addClass('disabled');
-                dialog.success(msg.info,module+'Store/index');
-            }
-        });
-    });
+    
     //确定通过入驻
     $('body').on('click','.three-step',function(){
     });
@@ -109,8 +61,6 @@ $(function(){
 
         var postData = {};
         postData.applicantData=applicantData; 
-        postData.order_id = $('.order_id').val();
-        postData.order_sn = $('.order_sn').val();
         postData.pay_code = $('.pay_code').val();
         _this = $(this);
         console.log(postData);
