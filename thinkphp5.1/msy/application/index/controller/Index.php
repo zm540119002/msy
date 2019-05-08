c3edb3387d633c4ee50d3c551d41264125af967f
jing <2517187814@qq.com>
2019-05-08 18:10:14 +0800

    1

--- thinkphp5.1/msy/application/index/controller/Index.php
+++ thinkphp5.1/msy/application/index/controller/Index.php
@@ -4,9 +4,6 @@ namespace app\index\controller;
 class Index extends \common\controller\Base{
     //首页
     public function index(){
-        echo 1111;
-        exit;
-
         return $this->fetch();
     }
 }
