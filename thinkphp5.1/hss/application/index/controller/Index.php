9e6827f506048fe2923e2637f0032a909f5bed37
zhangmin <540119002@qq.com>
2019-05-08 17:56:26 +0800

    zm

--- thinkphp5.1/hss/application/index/controller/Index.php
+++ thinkphp5.1/hss/application/index/controller/Index.php
@@ -7,7 +7,6 @@ class Index extends \common\controller\Base{
      * 促销列表，场景列表，商品列表 -ajax
      */
     public function index(){
-        echo 123;exit;
         // 促销列表 7个
         $modelPromotion = new \app\index\model\Promotion();
         $condition =[
