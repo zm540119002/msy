3c36a9425448f1c5f24e9b4462acab1f4532f031
zhangmin <540119002@qq.com>
2019-05-09 09:26:03 +0800

    zm

--- thinkphp5.1/hss/public/static/index/js/pay.js
+++ thinkphp5.1/hss/public/static/index/js/pay.js
@@ -14,7 +14,7 @@ $(function () {
 
     // 弹出支付方式
     $('body').on('click','.confirm_order',function(){
-        alert(44444);
+        alert(555);
         var postData = {};
         postData = addAddress(postData);
         postData.order_id = $('.order_id').val();
