2c70726f3ecbafbba5ac68b3b74db6e4891e98f7
zhangmin <540119002@qq.com>
2019-05-09 11:27:32 +0800

    zm

--- thinkphp5.1/hss/application/index/controller/Scene.php
+++ thinkphp5.1/hss/application/index/controller/Scene.php
@@ -59,10 +59,12 @@ class Scene extends \common\controller\Base{
     public function detail(){
         if(request()->isAjax()){
         }else{
-            $unlockingFooterCart = unlockingFooterCartConfigTest([0,2,1]);
+            $unlockingFooterCart = unlockingFooterCartConfigTest([0,2,1,3,4]);
             array_push($unlockingFooterCart['menu'][0]['class'],'group_btn30');
             array_push($unlockingFooterCart['menu'][1]['class'],'group_btn30');
             array_push($unlockingFooterCart['menu'][2]['class'],'group_btn30');
+            array_push($unlockingFooterCart['menu'][3]['class'],'group_btn30');
+            array_push($unlockingFooterCart['menu'][4]['class'],'group_btn30');
             $this->assign('unlockingFooterCart',json_encode($unlockingFooterCart));
 
             $id = intval(input('id'));
