75b531b9a50e4a2fc0dce2b9eb2d326671ca4904
yangguanbao <yangguanbao@qq.com>
2019-05-09 13:53:56 +0800

    ygb

--- thinkphp5.1/msy/application/index/controller/Payment.php
+++ thinkphp5.1/msy/application/index/controller/Payment.php
@@ -360,6 +360,7 @@ class Payment extends \common\controller\Base {
             //$systemId = $attach['system_id'];
             $systemId = 3;
             $payInfo = $this->getPayInfo($systemId,$data['out_trade_no']);
+            print_r($payInfo);exit;
             if(empty($payInfo)){
                 return $this->writeLog("数据库没有此订单",$payInfo);
             }
