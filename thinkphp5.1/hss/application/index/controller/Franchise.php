7526f8a15c05e9ab04754203ece57a6290d27492
yangguanbao <yangguanbao@qq.com>
2019-05-09 15:28:37 +0800

    ygb

--- thinkphp5.1/hss/application/index/controller/Franchise.php
+++ thinkphp5.1/hss/application/index/controller/Franchise.php
@@ -33,11 +33,14 @@ class Franchise extends \common\controller\UserBase {
             $postData['sn'] = $sn;
             $postData['user_id'] = $this->user['id'];
             $postData['franchise_fee'] = config('custom.franchise_fee');
+            $postData['create_time'] = time();
             $result  = $modelFranchise->isUpdate(false)->save($postData);
             if(!$result){
                 $modelFranchise ->rollback();
                 return errorMsg('失败');
             }
+
+            //生成支付表数据
             $modelPay = new \app\index\model\Pay();
             $data = [
                 'sn' => $sn,
@@ -45,6 +48,7 @@ class Franchise extends \common\controller\UserBase {
                 'user_id' => $this->user['id'],
                 'pay_code' => $postData['pay_code'],
                 'type' => config('custom.pay_type')['franchisePay']['code'],
+                'create_time' => time(),
             ];
             $result  = $modelPay->isUpdate(false)->save($data);
             if(!$result){
