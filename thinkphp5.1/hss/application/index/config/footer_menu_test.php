3430cd9e6219558ea4420aabe94e3c1e33c0f1ae
jing <2517187814@qq.com>
2019-05-10 14:51:05 +0800

    1

--- thinkphp5.1/hss/application/index/config/footer_menu_test.php
+++ thinkphp5.1/hss/application/index/config/footer_menu_test.php
@@ -35,6 +35,7 @@ return [
                 'bottom_item',
                 'add_cart_icon',
             ],
+            'action'=>url('Cart/manage'),
         ],[//3
             'name'   => '立即购买',
             'class'=>[
