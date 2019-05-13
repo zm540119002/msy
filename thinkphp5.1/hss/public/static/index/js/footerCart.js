865911138d80d87b1af8be4d4ac809dd558962e6
yangguanbao <yangguanbao@qq.com>
2019-05-13 10:47:49 +0800

    ygb

--- thinkphp5.1/hss/public/static/index/js/footerCart.js
+++ thinkphp5.1/hss/public/static/index/js/footerCart.js
@@ -76,6 +76,7 @@ cart = {
                 var find = false;
                 $.each(goodsList,function(j,goods){
                     if(addGoods.goods_id == goods.goods_id && addGoods.buy_type == goods.buy_type){
+                        console.log(12);
                         //找到修改数量
                         find = true;
                         cartListOld[j].num = parseInt(cartListOld[j].num) + parseInt(goods.num);
@@ -90,9 +91,9 @@ cart = {
                     }
                 });
             });
-            //保存购物车
-            localStorage.setItem('cartList',goodsList);
         }
+        //保存购物车
+        localStorage.setItem('cartList',JSON.stringify(addGoodsList));
         return false;
     }
 };
