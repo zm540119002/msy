ea3ca1534fb87dfa6c38b4463541f55803bba47a
jing <2517187814@qq.com>
2019-05-13 14:13:05 +0800

    1

--- thinkphp5.1/hss/application/index_admin/controller/Project.php
+++ thinkphp5.1/hss/application/index_admin/controller/Project.php
@@ -52,7 +52,7 @@ class Project extends Base {
 
             $data['update_time'] = time();
             $data['audit'] = 1; // 暂时没有审核，先固定
-    
+
             if(isset($data['id']) && $id=input('post.id/d')){//修改
                 // 编辑
                 $condition = ['where' => ['id' => $id,]];
@@ -84,8 +84,7 @@ class Project extends Base {
                     delImgFromPaths($oldImgArr,$newImgArr);
                 }
 
-
-
+                
             } else{
                 //新增
                 $data['create_time'] = time();
