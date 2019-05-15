c67464ee21f58a6b864bca4cee00d5d271343194
yangguanbao <yangguanbao@qq.com>
2019-05-15 14:40:22 +0800

    ygb

--- thinkphp5.1/hss/public/static/index/js/footerCart.js
+++ thinkphp5.1/hss/public/static/index/js/footerCart.js
@@ -120,7 +120,7 @@ $(function () {
     //初始化购物车数量
     var total_num = 0;
     if(user_id){
-         total_num = '{$total_num}';
+         total_num = "{$total_num}";
     }else{
          total_num = cart.getGoodsTotal();
 
