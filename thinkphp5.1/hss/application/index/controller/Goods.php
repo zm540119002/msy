a46be327a2066a062cb284648de77873f22a9a81
yangguanbao <yangguanbao@qq.com>
2019-05-15 15:05:42 +0800

    ygb

--- thinkphp5.1/hss/application/index/controller/Goods.php
+++ thinkphp5.1/hss/application/index/controller/Goods.php
@@ -248,14 +248,12 @@ class Goods extends \common\controller\Base{
             }
 
             //统计购物车商品数量
-            $modelCart = new \app\index\model\Cart();
             $user = checkLogin();
             if($user){
+                $modelCart = new \app\index\model\Cart();
                 $totalNum = $modelCart->where(['status'=>0,'user_id'=>$user['id']])->sum('num');
                 $this -> assign('total_num',$totalNum);
             }
-
-
             $unlockingFooterCart = unlockingFooterCartConfig([0,2,1]);
             $this->assign('unlockingFooterCart', $unlockingFooterCart);
             return $this->fetch();
