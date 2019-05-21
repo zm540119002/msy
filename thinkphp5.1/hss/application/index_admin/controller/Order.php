b45c7bc71c7d815f355c37f9492e22887625e3c1
jing <2517187814@qq.com>
2019-05-21 13:53:32 +0800

    1

--- thinkphp5.1/hss/application/index_admin/controller/Order.php
+++ thinkphp5.1/hss/application/index_admin/controller/Order.php
@@ -77,4 +77,38 @@ class Order extends Base{
         $this->assign('list',$list);
         return view('list_tpl');
     }
+
+
+    /**
+     * 增加快递信息
+     */
+    public function addExpress(){
+        $id        = input('id/d');
+        $express_id= input('express_id/d');
+        $express_sn= input('express_sn/s');
+
+       if( !$id OR !$express_id OR !$express_sn  ){
+           return $this->errorMsg('参数不能为空');
+       }
+
+       $data = [
+           'express_id'  => $express_id,
+           'express_sn'  => $express_sn,
+           'order_status'=> 2,
+       ];
+
+        $condition = [
+            ['id','=',$id],
+            ['status','=',0],
+        ];
+        $model = new \app\index_admin\model\Order();
+        $res = $model->edit($data,$condition);
+
+        if($res===false){
+            return $this->errorMsg('更新失败');
+
+        }else{
+            return $this->successMsg('更新成功');
+        }
+    }
 }
