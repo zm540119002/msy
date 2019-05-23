5624926f6e9aa531b2d930e0f25c31ff1de91629
zhangmin <540119002@qq.com>
2019-05-23 15:15:26 +0800

    zm

--- thinkphp5.1/common/model/UserCenter.php
+++ thinkphp5.1/common/model/UserCenter.php
@@ -81,6 +81,7 @@ class UserCenter extends Base {
 			if(!$validateUser->scene('resetPassword')->check($data)){
 				return errorMsg($validateUser->getError());
 			}
+			return errorMsg('重置密码失败');
 			if(!$this->_resetPassword($saveData['mobile_phone'],$saveData['password'])){
 				return errorMsg('重置密码失败');
 			}
