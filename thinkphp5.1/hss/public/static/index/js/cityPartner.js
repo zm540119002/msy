28fb3d9d7e27928f47595d62e9cbc5e889ec2098
zhuguangfu <zhuguangfu@qq.com>
2019-05-28 11:48:53 +0800

    zg

--- thinkphp5.1/hss/public/static/index/js/cityPartner.js
+++ thinkphp5.1/hss/public/static/index/js/cityPartner.js
@@ -15,8 +15,6 @@ $(function(){
 		}
         var cityData=[];
         cityData.push(parseInt(area_address[0]),parseInt(area_address[1]));
-        console.log(cityData);
-        console.log(cityArr);
         var provinces=arrayHasElement(cityArr,cityData);
         if(!provinces){
             layer.open({
