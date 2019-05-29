b94b4b27661c000d5ea8bdfc7f4b90fa610878f1
zhuguangfu <zhuguangfu@qq.com>
2019-05-29 14:07:30 +0800

    zgf

--- thinkphp5.1/hss/public/static/index/js/cityPartner.js
+++ thinkphp5.1/hss/public/static/index/js/cityPartner.js
@@ -19,7 +19,7 @@ $(function(){
         $('.applicant').val(apply[0].applicant);
         $('.mobile').val(apply[0].mobile);
         console.log($('ul li:eq(1)'));
-        $('ul li:eq(1)').click();
+        $('ul li:eq(1)').trigger("click");
     }
     return false;
     $('body').on('click','.apply-data-nav .switch-item',function(){
