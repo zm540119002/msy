ed89a102a84d796dc5d5fd8c777f3ff0909ce120
yangguanbao <yangguanbao@qq.com>
2019-05-29 17:31:21 +0800

    'ygb'

--- thinkphp5.1/hss/application/index/controller/CityPartner.php
+++ thinkphp5.1/hss/application/index/controller/CityPartner.php
@@ -101,7 +101,7 @@ class CityPartner extends \common\controller\UserBase {
                     }
                 }else{
                     $result  = $modelCityPartner->isUpdate(false)->save($postData);
-                    $id = $result;
+                    $id = $modelCityPartner->getAttr('id');
                     if(!$result){
                         $modelCityPartner ->rollback();
                         return errorMsg('失败');
@@ -128,7 +128,7 @@ class CityPartner extends \common\controller\UserBase {
                     }
                 }else{
                     $result  = $modelCityPartner->isUpdate(false)->save($postData);
-                    $id = $result;
+                    $id = $modelCityPartner->getAttr('id');
                     if(!$result){
                         $modelCityPartner ->rollback();
                         return errorMsg('失败');
@@ -146,6 +146,7 @@ class CityPartner extends \common\controller\UserBase {
                     'create_time' => time(),
                 ];
                 $result  = $modelPay->isUpdate(false)->save($data);
+                $id = $modelCityPartner->getAttr('id');
                 if(!$result){
                     $modelPay ->rollback();
                     return errorMsg('失败');
