1e6c6cb410f2fc88ecac8e403ad6f348ba496488
yangguanbao <yangguanbao@qq.com>
2019-05-29 16:39:40 +0800

    'ygb'

--- thinkphp5.1/hss/application/index/controller/CityPartner.php
+++ thinkphp5.1/hss/application/index/controller/CityPartner.php
@@ -151,30 +151,4 @@ class CityPartner extends \common\controller\UserBase {
         return successMsg('成功',['url'=>config('custom.pay_gateway').$sn]);
     }
 
-    public function getSigningInfo(){
-        $province_id = 1;
-        $city_id = 1;
-        if(!$province_id OR !$city_id){
-            return errorMsg('请求数据不能为空');
-
-        }else{
-            // 到时看下需不需要按补齐尾款的时间来判断
-            $model = new \app\index\model\CityPartner;
-
-            $condition = [
-                'where'=>[
-                    ['status',['=', 2], ['=', 3],'OR'],
-                ],
-                'field'=>['id'],
-            ];
-
-            $res = $model->getInfo($condition);
-
-            p($model->getLastSql());
-            exit;
-
-            return true;
-        }
-
-    }
 }
