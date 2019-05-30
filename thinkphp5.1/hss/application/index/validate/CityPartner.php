87463df22dfe8a4b1eaaa0e0e23941bab2605401
yangguanbao <yangguanbao@qq.com>
2019-05-30 14:26:11 +0800

    'gg'

--- thinkphp5.1/hss/application/index/validate/CityPartner.php
+++ thinkphp5.1/hss/application/index/validate/CityPartner.php
@@ -5,6 +5,12 @@ class CityPartner extends \think\Validate
 {
     //验证规则
     protected $rule = [
+        'province'  => [
+            'require', 'max' => 5,'number','mobile'
+        ],
+        'city'  => [
+            'require', 'max' => 5,'number'
+        ],
         'company_name'  => [
             'require','max' => 200,
         ],
@@ -14,27 +20,24 @@ class CityPartner extends \think\Validate
         'mobile'  => [
             'require', 'max' => 12,'mobile',
         ],
-        'province'  => [
-            'require', 'max' => 5,'number'
-        ],
-        'city'  => [
-            'require', 'max' => 5,'number'
-        ],
         'pay_code'  => [
             'require', 'max' => 1,'integer'
         ],
-
     ];
 
     //验证消息
     protected $message  =   [
-        'company_name.require' => '加盟店名称必须！',
-        'company_name.max' => '加盟店名称过长！',
-        'mobile.mobile' => '请填写正确的手机号码！',
         'province.require' => '省份必须！',
+        'province.mobile' => '111！',
         'province.max' => '提交省份数据不符！',
         'city.require' => '城市必须！',
         'city.max' => '提交城市数据不符！',
+        'company_name.require' => '加盟店名称必须！',
+        'company_name.max' => '加盟店名称过长！',
+        'applicant.require' => '申请人名称必须！',
+        'applicant.max' => '申请人名称过长！',
+        'mobile.require' => '手机号码为空！',
+        'mobile.mobile' => '请填写正确的手机号码！',
         'pay_code.require' => '支付方式必须！',
         'pay_code.max' => '提交支付方式不符！',
     ];
@@ -47,16 +50,18 @@ class CityPartner extends \think\Validate
             'city',
         ],
         'step2'  =>  [
-            'company_name',
-            'mobile',
             'province',
             'city',
+            'company_name',
+            'applicant',
+            'mobile',
         ],
         'step3'  =>  [
-            'company_name',
-            'mobile',
             'province',
             'city',
+            'company_name',
+            'applicant',
+            'mobile',
             'pay_code',
         ],
     ];
