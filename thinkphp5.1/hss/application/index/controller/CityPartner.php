e3b188cc3b4222a5ba70f30f10a6169cb3fd2170
yangguanbao <yangguanbao@qq.com>
2019-05-30 13:48:52 +0800

    'gg'

--- thinkphp5.1/hss/application/index/controller/CityPartner.php
+++ thinkphp5.1/hss/application/index/controller/CityPartner.php
@@ -70,17 +70,14 @@ class CityPartner extends \common\controller\UserBase {
      */
     public function submitApplicant()
     {
-
         if(!request()->isAjax()){
             $this->errorMsg('请求方式错误');
         }
         $modelCityPartner = new \app\index\model\CityPartner();
         $postData = input('post.');
-        print_r($postData);exit;
         $validate = new \app\index\validate\CityPartner();
         $modelCityPartner -> startTrans();
         $postData['apply_status'] = $postData['step'];
-
         switch ($postData['step']){
             case 1:
                 if(!$validate->scene('step1')->check($postData)) {
