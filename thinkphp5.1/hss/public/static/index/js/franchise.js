a757d53f67f1a59be7edfa86ca787a414f1040ac
yangguanbao <yangguanbao@qq.com>
2019-06-03 15:40:04 +0800

    'ygb'

--- thinkphp5.1/hss/public/static/index/js/franchise.js
+++ thinkphp5.1/hss/public/static/index/js/franchise.js
@@ -4,8 +4,10 @@ $(function(){
         applicantData={};
     //填写基本资料
     $('body').on('click','.one-step',function(){
+        var _this = $(this);
         applicantData=$('.applicant_form').serializeObject();
         area_address =$('.area-address-name').getArea();
+
         var content='';
         if(!applicantData.name){
             content='请填写店家名称';
@@ -21,10 +23,12 @@ $(function(){
         if(content){
             dialog.error(content);
         }else{
-            $('.weui-flex-item:eq(0)').removeClass('current');
-            $('.weui-flex-item:eq(1)').addClass('current');
-            $('.apply-module:eq(0)').hide();
-            $('.apply-module:eq(1)').show();
+            applicantData.step = 1;
+            submitApplicant(_this,applicantData);
+            // $('.weui-flex-item:eq(0)').removeClass('current');
+            // $('.weui-flex-item:eq(1)').addClass('current');
+            // $('.apply-module:eq(0)').hide();
+            // $('.apply-module:eq(1)').show();
         }
     });
     
@@ -80,15 +84,37 @@ function submitApplicant(_this,postData){
             $('.loading').hide();
             dialog.error('AJAX错误');
         },
+        // success: function(data){
+        //     _this.removeClass("nodisabled");//删除防止重复提交
+        //     $('.loading').hide();
+        //     if(data.status){
+        //         location.href = data.url;
+        //
+        //     }else{
+        //         dialog.success(data.info);
+        //         //dialog.error('结算提交失败!');
+        //     }
+        // }
         success: function(data){
             _this.removeClass("nodisabled");//删除防止重复提交
             $('.loading').hide();
             if(data.status){
-                location.href = data.url;
-
+                applicantData.id = data.data.id;
+                if(postData.step==1){
+                    $('.weui-flex-item:eq(0)').removeClass('current');
+                    $('.weui-flex-item:eq(1)').addClass('current');
+                    $('.apply-module:eq(0)').hide();
+                    $('.apply-module:eq(1)').show();
+                }else if(postData.step==2){
+                    $('.weui-flex-item:eq(0),.weui-flex-item:eq(1)').removeClass('current');
+                    $('.weui-flex-item:eq(2)').addClass('current');
+                    $('.apply-module:eq(1)').hide();
+                    $('.apply-module:eq(2)').show();
+                }else if(postData.step==3){
+                    location.href = data.data.url;
+                }
             }else{
                 dialog.success(data.info);
-                //dialog.error('结算提交失败!');
             }
         }
     });
