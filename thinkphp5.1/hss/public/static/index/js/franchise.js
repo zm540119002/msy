a9ca4b4f233d126ac6735a1823b20f0da472c542
yangguanbao <yangguanbao@qq.com>
2019-06-04 12:04:18 +0800

    'ygb'

--- thinkphp5.1/hss/public/static/index/js/franchise.js
+++ thinkphp5.1/hss/public/static/index/js/franchise.js
@@ -37,7 +37,6 @@ $(function(){
     }
     //填写基本资料
     $('body').on('click','.one-step',function(){
-        alert(11);
         var _this = $(this);
         applicantData=$('.applicant_form').serializeObject();
         area_address =$('.area-address-name').getArea();
