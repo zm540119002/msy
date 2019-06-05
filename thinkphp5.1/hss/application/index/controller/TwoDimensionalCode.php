2e50b571a55655b5257fe14b62528ac3a2727ae6
yangguanbao <yangguanbao@qq.com>
2019-06-05 14:37:49 +0800

    'ygb'

--- thinkphp5.1/hss/application/index/controller/TwoDimensionalCode.php
+++ thinkphp5.1/hss/application/index/controller/TwoDimensionalCode.php
@@ -28,7 +28,7 @@ class TwoDimensionalCode extends \common\controller\UserBase {
             'qrcode'=>$uploadPath.$shareQRCode, // 120*120
             'font'=>'./static/font/simhei.ttf',   //字体
         ];
-        return $init;
+        print_r($init);exit;
 //        $init = [
 //            'save_path'=>$newRelativePath,   //保存目录  ./uploads/compose/goods....
 //            'name'=> '李白', //用户名
