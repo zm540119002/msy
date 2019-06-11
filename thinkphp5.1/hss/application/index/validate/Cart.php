2bee27f33cfe44215ebb43994700382c79c17fa0
zhangmin <540119002@qq.com>
2019-06-11 10:39:49 +0800

    zm

--- thinkphp5.1/hss/application/index/validate/Cart.php
+++ thinkphp5.1/hss/application/index/validate/Cart.php
@@ -6,10 +6,16 @@ class Cart extends \think\Validate
     //验证规则
     protected $rule = [
         'goods_id'  => [
-            'require','number','gt'=>0
+            'require','number','gt'=>0,
         ],
         'num'  => [
-            'require','number','neq'=>0
+            'require','number',
         ],
+    ];
+    protected $message  =   [
+        'goods_id.require' => '商品ID必须',
+        'goods_id.number'     => '商品ID错误',
+        'num.require'     => '商品数量必须',
+        'num.number'   => '商品数量错误',
     ];
 }
