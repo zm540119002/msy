841f5e41b0414b2c0f952b52adc70d3207145cba
jing <2517187814@qq.com>
2019-06-12 14:53:40 +0800

    1

--- thinkphp5.1/hss/application/index_admin/controller/Goods.php
+++ thinkphp5.1/hss/application/index_admin/controller/Goods.php
@@ -573,9 +573,10 @@ class Goods extends Base {
             }
 
         }
-/*        if(count($list) == 1){
-            return successMsg($newQRCodes);
-        }*/
+        if(count($list) == 1){
+            return successMsg('成功',['img'=>$newQRCodes]);
+            //return successMsg($newQRCodes);
+        }
         return successMsg('成功');
     }
 
