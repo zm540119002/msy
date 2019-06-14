dd7756d97a578624af4026b7eb0f1533c15e6825
zhangmin <540119002@qq.com>
2019-06-14 15:59:28 +0800

    zm

--- thinkphp5.1/hss/application/index/controller/Order.php
+++ thinkphp5.1/hss/application/index/controller/Order.php
@@ -75,7 +75,7 @@ class Order extends \common\controller\UserBase
             $goodsIds = array_column($goodsList,'goods_id');
 
             if(!($promotion['belong_to_member_buy']&$res['type'])){
-                $this->errorMsg('非会员 !');
+                $this->errorMsg('仅限会员 !');
             }
 
 
