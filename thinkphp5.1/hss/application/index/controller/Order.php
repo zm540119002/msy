1c850ad74b06055d46e3ffbab37ae8f676c5bd23
zhangmin <540119002@qq.com>
2019-06-14 17:02:16 +0800

    zm

--- thinkphp5.1/hss/application/index/controller/Order.php
+++ thinkphp5.1/hss/application/index/controller/Order.php
@@ -15,7 +15,7 @@ class Order extends \common\controller\UserBase
                 ['update_time'=>time()],
             ];
             $var = $memberModel->edit($data);
-            p($memberModel->id);
+            p($memberModel->getAttr('user_id'));
             exit;
         }
     }
