007c4b43c283c31509f10aa7240e578bc67cf9d5
yangguanbao <yangguanbao@qq.com>
2019-06-15 11:42:13 +0800

    'ygb'

--- thinkphp5.1/hss/application/index/controller/CityPartner.php
+++ thinkphp5.1/hss/application/index/controller/CityPartner.php
@@ -64,6 +64,7 @@ class CityPartner extends \common\controller\UserBase {
                 }
             }
             p($apply);
+            $this->assign('apply1',$apply);
             $this->assign('apply',json_encode($apply));
             $this->assign('applied',json_encode($applied));
             $unlockingFooterCart = unlockingFooterCartConfig([10, 0, 9]);
