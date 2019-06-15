3f99cd442213866f61200bd9d0f123d6fd72649e
yangguanbao <yangguanbao@qq.com>
2019-06-15 10:46:40 +0800

    'ygb'

--- thinkphp5.1/msy/application/index/controller/Payment.php
+++ thinkphp5.1/msy/application/index/controller/Payment.php
@@ -475,6 +475,7 @@ class Payment extends \common\controller\Base {
         }
         unset($data['id']);
         $result = $modelCityPartner  -> allowField(true)-> save($data,$condition);
+        p($modelCityPartner->getLastSql());exit;
         if(false === $result){
             $modelCityPartner ->rollback();
             $info['mysql_error'] = $modelCityPartner->getError();
