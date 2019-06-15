d68dcf67235255896fa4ba316dd0f53b7f71a912
yangguanbao <yangguanbao@qq.com>
2019-06-15 16:21:54 +0800

    'ygb'

--- thinkphp5.1/hss/application/index/controller/Index.php
+++ thinkphp5.1/hss/application/index/controller/Index.php
@@ -119,6 +119,7 @@ class Index extends \common\controller\Base{
                         }
                     }
                 }
+                p($apply);exit;
                 $this->assign('apply',$apply);
                 $this->assign('applied',$applied);
             }
