85277b28c277a3503835a3bebbc045beff39fed1
jing <2517187814@qq.com>
2019-06-15 16:16:11 +0800

    1

--- thinkphp5.1/msy/application/index/model/Member.php
+++ thinkphp5.1/msy/application/index/model/Member.php
@@ -5,10 +5,25 @@ class Member extends \common\model\Base {
 	// 设置当前模型对应的完整数据表名称
 	protected $table = 'member';
 	// 设置主键
-	protected $pk = 'id';
+    protected $pk = 'id';
 	// 设置当前模型的数据库连接
-    public $connection = 'db_config_hss';
+    protected $connection = 'db_config_1';
 	//表的别名
 	protected $alias = 'm';
+
+    /**
+     * 获取账号信息
+     */
+    public function getMemberInfo($uid){
+        $config = [
+
+            'where' => [
+                ['user_id','=',$uid],
+            ]
+        ];
+
+        return $this->getInfo($config);
+    }
+
 
 }
