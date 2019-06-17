ebb5dba1bcf4d1660ef2e3581ffd49aecd221c23
yangguanbao <yangguanbao@qq.com>
2019-06-17 10:11:07 +0800

    'ygb'

--- thinkphp5.1/hss/public/static/index/js/cityPartner.js
+++ thinkphp5.1/hss/public/static/index/js/cityPartner.js
@@ -4,9 +4,6 @@ var area_address,
 $(function(){
     //补款倒计时
     var start_pay_time = apply[0]['payment_time']*1000;
-
-    console.log(start_pay_time*1000);
-    console.log(24*60*60*1000-1)
     var date=new Date(start_pay_time+(24*60*60*1000-1));
     countDown(date,$('#countDownBox'));
     //nav切换
