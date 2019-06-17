3658bb62a81355ec3aaccc19c62d7dd2f0339c2d
jing <2517187814@qq.com>
2019-06-17 10:07:39 +0800

    1

--- thinkphp5.1/hss/application/index/controller/Order.php
+++ thinkphp5.1/hss/application/index/controller/Order.php
@@ -526,7 +526,7 @@ class Order extends \common\controller\UserBase
             'field'=>[
                 'o.id','o.pay_sn','o.sn','o.order_status','o.pay_code','o.amount','o.actually_amount','o.remark',
                 'o.consignee','o.mobile','o.province','o.city','o.area','o.detail_address','o.create_time','o.payment_time',
-                'o.finished_time',
+                'o.finished_time','o.complete_address'
             ],'order' => [
                 'o.create_time'=>'desc',
             ],
