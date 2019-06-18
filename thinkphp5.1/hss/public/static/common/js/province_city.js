972c231d286063a577f587f248640b8ea2800dac
zhangmin <540119002@qq.com>
2019-06-18 10:18:09 +0800

    zm

---
+++ thinkphp5.1/hss/public/static/common/js/province_city.js
@@ -0,0 +1,33 @@
+var provinces = [
+	{
+		name: '请选择',
+		cities: [{name:'请选择'}]
+	},{
+		name: '北京',code:'100',
+		cities:[
+			{name: '北京',code:'100',level: 1}
+		]
+	},{
+		name: '四川省',code:'101',
+		cities:[
+			{name: '成都市',code:'100',level: 1},
+			{name: '遂宁市',code:'101',level: 2},
+			{name: '自贡市',code:'102',level: 3},
+			{name: '泸州市',code:'103',level: 3}
+		]
+	},{
+		name: '广东省',code:'102',
+		cities:[
+			{code:'100',name: '广州市',level: 1},
+			{code:'101',name: '深圳市',level: 1},
+			{code:'102',name: '珠海市',level: 2},
+			{code:'103',name: '汕头市',level: 3},
+			{code:'104',name: '佛山市',level: 3}
+		]
+	},{
+		name: '上海市',code:'103',
+		cities:[
+			{code:'100',name: '上海市',level: 1}
+		]
+	}
+];
