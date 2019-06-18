78fec110d4036e631f9ea94af84fdb619e74b3cb
yangguanbao <yangguanbao@qq.com>
2019-06-18 09:36:33 +0800

    'ygb'

--- thinkphp5.1/common/controller/Base.php
+++ thinkphp5.1/common/controller/Base.php
@@ -22,6 +22,7 @@ class Base extends \think\Controller{
         if(isWxBrowser() && !request()->isAjax()) {//判断是否为微信浏览器
             $mineTools = new \common\component\payment\weixin\Jssdk(config('wx_config.appid'), config('wx_config.appsecret'));
             $this->weixin_user = $mineTools->getOauthUserInfo();
+            p($this->weixin_user);exit;
             $user = checkLogin();
             if($user){
                 if(!$user['hss_openid']){
@@ -44,8 +45,8 @@ class Base extends \think\Controller{
                     $userModel = new \common\model\User();
                     $result = $userModel->isUpdate(true)->save($data);
 //                    if( false === $result){
-//                        return $this->errorMsg('添加微信信息失败');
-//                    }
+////                        return $this->errorMsg('添加微信信息失败');
+////                    }
                 }
             }
 
