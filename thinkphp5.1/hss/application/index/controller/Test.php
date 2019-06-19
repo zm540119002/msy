0b467838a9791a7df18cd34353781f887d901674
yangguanbao <yangguanbao@qq.com>
2019-06-19 16:33:15 +0800

    'ygb'

--- thinkphp5.1/hss/application/index/controller/Test.php
+++ thinkphp5.1/hss/application/index/controller/Test.php
@@ -55,7 +55,6 @@ class Test extends \common\controller\Base{
     public function weixin()
     {
         $mineTools = new \common\component\payment\weixin\Jssdk(config('wx_config.appid'), config('wx_config.appsecret'));
-
         $weiXinUserInfo1 = $mineTools->getOauthUserInfo();
         $weiXinUserInfo2= $mineTools->get_user_info($weiXinUserInfo1['openid']);
         P($weiXinUserInfo1);
