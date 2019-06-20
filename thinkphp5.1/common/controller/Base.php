5e291988d922fe650cb9127d84d8ba202bb90feb
yangguanbao <yangguanbao@qq.com>
2019-06-20 09:28:11 +0800

    'ygb'

--- thinkphp5.1/common/controller/Base.php
+++ thinkphp5.1/common/controller/Base.php
@@ -28,7 +28,13 @@ class Base extends \think\Controller{
 //                session('weiXinUserInfo',$weiXinUserInfo);
 //            }
             $mineTools = new \common\component\payment\weixin\Jssdk(config('wx_config.appid'), config('wx_config.appsecret'));
-            $weiXinUserInfo = $mineTools->getOauthUserInfo();
+            //判断是否关注平台
+            $openId = $mineTools ->getOpenid();
+            $weiXinUserInfo= $mineTools->get_user_info($openId);
+            if(isset($weiXinUserInfo['subscribe']) && !$weiXinUserInfo['subscribe']){
+                $this -> assign('subscribe',1);
+                $weiXinUserInfo = $mineTools->getOauthUserInfo();
+            }
             $user = checkLogin();
             if((!$user['name'] || !$user['avatar']) && $user && isset($weiXinUserInfo['openid'])){
                 //临时相对路径
@@ -54,14 +60,6 @@ class Base extends \think\Controller{
                 $result = $userModel->isUpdate(true)->save($data);
                 setSession($user);
             }
-
-            //判断是否关注平台
-            $weiXinUserInfo2= $mineTools->get_user_info($weiXinUserInfo['openid']);
-            //P($weiXinUserInfo2);
-            if(isset($weiXinUserInfo2['subscribe'])){
-                $this -> assign('subscribe',$weiXinUserInfo2['subscribe']);
-            }
-
         }
 
         if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
