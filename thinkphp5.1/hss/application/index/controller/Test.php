aca26269531712033695aa3e0e31fc9e825e91d6
zhangmin <540119002@qq.com>
2019-06-21 14:23:58 +0800

    zm

--- thinkphp5.1/hss/application/index/controller/Test.php
+++ thinkphp5.1/hss/application/index/controller/Test.php
@@ -53,6 +53,14 @@ class Test extends \common\controller\Base{
             return $this->fetch();
         }
     }
+    /**测试-布局2
+     */
+    public function layout2(){
+        if(request()->isAjax()){
+        }else{
+            return $this->fetch();
+        }
+    }
 
     public function jin(){
 
