434144dba1e004e16349b3fd1c8e771fd187bdc0
yangguanbao <yangguanbao@qq.com>
2019-06-21 11:33:19 +0800

    'ygb'

--- thinkphp5.1/hss/application/index/controller/WechatManage.php
+++ thinkphp5.1/hss/application/index/controller/WechatManage.php
@@ -205,55 +205,55 @@ class WechatManage extends \common\controller\Base {
         $keyword = trim($object->Content);
         $openid = strval($object->FromUserName);
         $content = "";
-        $content = "这是个文本消息\n".$keyword;
+
         //多客服人工回复模式
-//        if (strstr($keyword, "在线客服_") || strstr($keyword, "你好_")){
-//            $result = $this->transmitService($object);
-//        }
-//        //自动回复模式
-//        else{
-//            if (strstr($keyword, "文本")){
-//                $content = "这是个文本消息\n".$openid;
-//
-//            }else if (strstr($keyword, "单图文")){
-//                $content = array();
-//                $content[] = array("Title"=>"单图文标题",  "Description"=>"单图文内容", "PicUrl"=>"http://files.cnblogs.com/files/txw1958/cartoon.gif", "Url" =>"http://m.cnblogs.com/?u=txw1958");
-//                $weixin = new \weixin\Wxapi();
-//                $template = array('touser' => $openid,
-//                    'template_id' => "_yFpVtfHd0pSWy6ffApi6isjY8HmmWC8aKW-Uqz8viU",
-//                    'url' => "http://www.baidu.com/",
-//                    'topcolor' => "#0000C6",
-//                    'data' => array('content'    => array('value' => "你妈妈\\n喊你\\n回家吃饭了！",
-//                        'color' => "#743A3A",
-//                    ),
-//                    )
-//                );
-//                $weixin->send_template_message($template);
-//            }else if (strstr($keyword, "图文") || strstr($keyword, "多图文")){
-//                $content = array();
-//                $content[] = array("Title"=>"多图文1标题", "Description"=>"", "PicUrl"=>"http://files.cnblogs.com/files/txw1958/cartoon.gif", "Url" =>"http://m.cnblogs.com/?u=txw1958");
-//                $content[] = array("Title"=>"多图文2标题", "Description"=>"", "PicUrl"=>"http://d.hiphotos.bdimg.com/wisegame/pic/item/f3529822720e0cf3ac9f1ada0846f21fbe09aaa3.jpg", "Url" =>"http://m.cnblogs.com/?u=txw1958");
-//                $content[] = array("Title"=>"多图文3标题", "Description"=>"", "PicUrl"=>"http://g.hiphotos.bdimg.com/wisegame/pic/item/18cb0a46f21fbe090d338acc6a600c338644adfd.jpg", "Url" =>"http://m.cnblogs.com/?u=txw1958");
-//
-//            }else if (strstr($keyword, "音乐")){
-//                $content = array();
-//                $content = array("Title"=>"最炫民族风", "Description"=>"歌手：凤凰传奇", "MusicUrl"=>"http://121.199.4.61/music/zxmzf.mp3", "HQMusicUrl"=>"http://121.199.4.61/music/zxmzf.mp3");
-//            }else{
-//                // $content = date("Y-m-d H:i:s",time())."\n".$object->FromUserName."";
-//                $content = $this->xiaoirobot($openid, $keyword);
-//            }
-//
-//            if(is_array($content)){
-//                if (isset($content[0]['PicUrl'])){
-//                    $result = $this->transmitNews($object, $content);
-//                }else if (isset($content['MusicUrl'])){
-//                    $result = $this->transmitMusic($object, $content);
-//                }
-//            }else{
-//                $result = $this->transmitText($object, $content);
-//            }
-//        }
-        $result = $this->transmitText($object, $content);
+        if (strstr($keyword, "在线客服_") || strstr($keyword, "你好_")){
+            $result = $this->transmitService($object);
+        }
+        //自动回复模式
+        else{
+            if (strstr($keyword, "文本")){
+                $content = "这是个文本消息\n".$openid;
+
+            }else if (strstr($keyword, "单图文")){
+                $content = array();
+                $content[] = array("Title"=>"单图文标题",  "Description"=>"单图文内容", "PicUrl"=>"http://files.cnblogs.com/files/txw1958/cartoon.gif", "Url" =>"http://m.cnblogs.com/?u=txw1958");
+                $weixin = new \weixin\Wxapi();
+                $template = array('touser' => $openid,
+                    'template_id' => "_yFpVtfHd0pSWy6ffApi6isjY8HmmWC8aKW-Uqz8viU",
+                    'url' => "http://www.baidu.com/",
+                    'topcolor' => "#0000C6",
+                    'data' => array('content'    => array('value' => "你妈妈\\n喊你\\n回家吃饭了！",
+                        'color' => "#743A3A",
+                    ),
+                    )
+                );
+                $weixin->send_template_message($template);
+            }else if (strstr($keyword, "图文") || strstr($keyword, "多图文")){
+                $content = array();
+                $content[] = array("Title"=>"多图文1标题", "Description"=>"", "PicUrl"=>"http://files.cnblogs.com/files/txw1958/cartoon.gif", "Url" =>"http://m.cnblogs.com/?u=txw1958");
+                $content[] = array("Title"=>"多图文2标题", "Description"=>"", "PicUrl"=>"http://d.hiphotos.bdimg.com/wisegame/pic/item/f3529822720e0cf3ac9f1ada0846f21fbe09aaa3.jpg", "Url" =>"http://m.cnblogs.com/?u=txw1958");
+                $content[] = array("Title"=>"多图文3标题", "Description"=>"", "PicUrl"=>"http://g.hiphotos.bdimg.com/wisegame/pic/item/18cb0a46f21fbe090d338acc6a600c338644adfd.jpg", "Url" =>"http://m.cnblogs.com/?u=txw1958");
+
+            }else if (strstr($keyword, "音乐")){
+                $content = array();
+                $content = array("Title"=>"最炫民族风", "Description"=>"歌手：凤凰传奇", "MusicUrl"=>"http://121.199.4.61/music/zxmzf.mp3", "HQMusicUrl"=>"http://121.199.4.61/music/zxmzf.mp3");
+            }else{
+                // $content = date("Y-m-d H:i:s",time())."\n".$object->FromUserName."";
+                $content = $this->xiaoirobot($openid, $keyword);
+            }
+
+            if(is_array($content)){
+                if (isset($content[0]['PicUrl'])){
+                    $result = $this->transmitNews($object, $content);
+                }else if (isset($content['MusicUrl'])){
+                    $result = $this->transmitMusic($object, $content);
+                }
+            }else{
+                $result = $this->transmitText($object, $content);
+            }
+        }
+
         return $result;
     }
 
