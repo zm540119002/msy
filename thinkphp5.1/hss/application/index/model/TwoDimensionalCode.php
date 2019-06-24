ca528948a826f79a5146662b45208dd9f271e4aa
yangguanbao <yangguanbao@qq.com>
2019-06-24 11:00:18 +0800

    'ygb'

--- thinkphp5.1/hss/application/index/model/TwoDimensionalCode.php
+++ thinkphp5.1/hss/application/index/model/TwoDimensionalCode.php
@@ -79,6 +79,7 @@ class TwoDimensionalCode extends \common\model\Base {
         if(($info && !$info['code_url']) || empty($info)){
             $weixinTools = new \common\component\payment\weixin\Jssdk(config('wx_config.appid'), config('wx_config.appsecret'));
             $codeUrl = $weixinTools-> create_qrcode('QR_SCENE', $user['id']);
+            print_r($codeUrl);exit;
             $shareQRCode = createLogoQRcode($codeUrl['url'],config('upload_dir.hss_user_QRCode'));
             if($info && !$info['code_url']){
                 $data = [
