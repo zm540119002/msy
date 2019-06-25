7948214cf6d512f03312256eb790256df73186c5
jing <2517187814@qq.com>
2019-06-25 13:56:03 +0800

    1

--- thinkphp5.1/msy/application/index/config/custom.php
+++ thinkphp5.1/msy/application/index/config/custom.php
@@ -44,6 +44,7 @@ return [
                 2 => 'https://hss.meishangyun.com/index/Wallet/index',  //钱包支付成功跳转
                 3 => 'https://hss.meishangyun.com/index/Franchise/applyFranchise', //申请加盟店家支付成功跳转
                 4 => 'https://hss.meishangyun.com/index/CityPartner/applicationList', //申请城市合伙人支付成功跳转
+                5 => 'https://hss.meishangyun.com/index/Index/cityPartnerIndex',      //申请城市合伙人尾款支付成功跳转
             ],
         ],
     ],
