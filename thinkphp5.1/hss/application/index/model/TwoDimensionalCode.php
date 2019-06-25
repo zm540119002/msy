20f06733973310e1e65e4695a09a3394e7cd711e
yangguanbao <yangguanbao@qq.com>
2019-06-25 16:26:25 +0800

    'ugb'

--- thinkphp5.1/hss/application/index/model/TwoDimensionalCode.php
+++ thinkphp5.1/hss/application/index/model/TwoDimensionalCode.php
@@ -93,7 +93,7 @@ class TwoDimensionalCode extends \common\model\Base {
                 return errorMsg('创建新目录失败');
             }
             //生产没有logo二维码图片
-            $filename = generateSN(5).'.jpg';
+            $filename = 'platform_user_id_'.$this->user['id'].'.jpg';
             $saveFile = $newPath.$filename;
             file_put_contents($saveFile, $imgdata);
             //保存数据库路径
