068783133e724e080c403ad8028ccf8584b51b58
jing <2517187814@qq.com>
2019-06-25 14:18:42 +0800

    1

--- thinkphp5.1/hss/public/static/index/js/cityPartner1.js
+++ thinkphp5.1/hss/public/static/index/js/cityPartner1.js
@@ -1,6 +1,3 @@
-var area_address,
-    applicantData={
-    };
 $(function(){
     //nav切换
     $('body').on('click','.apply-data-nav .switch-item',function(){
@@ -62,76 +59,9 @@ $(function(){
         $('.weui-flex li:eq(2),.weui-flex li:eq(3)').addClass('nodisabled');
     }
 
-
-
-
-
-     //初始化 未完成的申请
-/*    if(!$.isEmptyArray(apply)){
-        var statusType=apply[0].apply_status-1;
-        applicantData.id= apply[0].id;
-        applicantData = {
-            id:apply[0].id,
-            province:apply[0].province,
-            city:apply[0].city,
-            company_name:apply[0].company_name,
-            applicant:apply[0].applicant,
-            mobile:apply[0].mobile,
-            old_apply_status:apply[0].apply_status,
-            pay_id:apply[0].pay_id,
-        };
-        var region = [];
-        region.push(apply[0].province);
-        region.push(apply[0].city);
-        if(statusType<3){
-            //省市区初始化
-            $('.area_address').setArea(region);
-            //资料初始化
-            $('.company_name').val(apply[0].company_name);
-            $('.applicant').val(apply[0].applicant);
-            $('.mobile').val(apply[0].mobile);
-            //定位到已完成步骤
-            var index=apply[0].apply_status-1;
-            $('nav.apply-data-nav li:eq('+index+')').click(function(){
-                $(this).addClass('current').siblings().removeClass('current');
-            });
-            $('nav.apply-data-nav li:eq('+index+')').click();
-            $('.weui-flex li:eq(3)').addClass('nodisabled');
-
-        }else{
-            //待审核
-            //资格完款
-            if(statusType==4 || statusType==3){
-                $('nav.apply-data-nav li:eq(3)').click(function(){
-                    $(this).addClass('current').siblings().removeClass('current');
-                });
-                $('nav.apply-data-nav li:eq(3)').click();
-                $('.weui-flex li:eq(0),.weui-flex li:eq(1),.weui-flex li:eq(2)').addClass('nodisabled');
-                if(statusType == 4){
-                    //资格完款
-                    var start_pay_time = apply[0]['payment_time']*1000;
-                    var date=new Date(start_pay_time+(24*60*60*1000-1));
-                    countDown(date,$('#countDownBox'));
-                    $('.apply_city').setArea(region);
-                }
-            }
-        }
-    }*/
-
-
     //填写地址
     // 地区查询
     $('body').on('click','.search-city',function(){
-        //area_address =$('.area-address-name').val();
-
-/*        var province    = $('input[name="province"]').val();
-        var city        = $('input[name="city"]').val();
-        var area_address= $('.select-value').val();*/
-/*        if(!province || !city){
-            dialog.error('请先选择城市');
-            return false;
-        }*/
-
         var data=$('.applicant_form').serializeObject();
         step1(data);
         data.step = 1;
@@ -144,45 +74,6 @@ $(function(){
                 var _this = $(this);
                 submitApplicant(_this,data);
                 layer.close(index);
-
-/*                _this = $(".confirm span[type='1']");
-
-                _this.addClass("nodisabled");//防止重复提交
-                $.ajax({
-                    url: module + 'CityPartner/submitApplicant',
-                    data: data,
-                    type: 'post',
-                    beforeSend: function(){
-                        $('.loading').show();
-                    },
-                    error:function(){
-                        $('.loading').hide();
-                        dialog.error('AJAX错误');
-                    },
-                    success: function(data){
-                        _this.removeClass("nodisabled");//删除防止重复提交
-                        $('.loading').hide();
-                        if(data.status){
-                            // 开放下一步
-
-                            $('.weui-flex-item:eq(0)').removeClass('current');
-                            $('.weui-flex-item:eq(1)').addClass('current');
-                            $('.apply-module:eq(0)').hide();
-                            $('.apply-module:eq(1)').show();
-
-                            layer.close(index);
-                        }else{
-                            if(data.data.status){
-                                layer.open({
-                                    content: '所查询的城市暂时没有空缺<br/>备注：城市合伙人可能已被签约，或者正处于保留状态，建议过段时间再查询。',
-                                    time: 2 //2秒后自动关闭
-                                });
-                            }else{
-                                dialog.error(data.info);
-                            }
-                        }
-                    }
-                });*/
             }
         });
 
@@ -230,13 +121,7 @@ $(function(){
             dialog.error('请选择结算方式');
         }else{
 
-            /*        applicantData.company_name=data.company_name;
-             applicantData.applicant=data.applicant;
-             applicantData.mobile=data.mobile;*/
-            //data.step = 3;
-
             step1(data);
-
             step2(data);
 
             submitApplicant(_this,data);
