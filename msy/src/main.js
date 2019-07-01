999fcd279d2db84cc5f2d98ffe992da5cecdb218
Yuan Nan <554606686@qq.com>
2019-07-01 13:47:04 +0800

    yn

--- msy/src/main.js
+++ msy/src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入初始化文件
+import '../static/common/reset.css'
+
+// 导入mui的样式
+import '../static/css/mui.min.css'
+import '../static/css/icons-extra.css'
+
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+Vue.use(MintUI)
+
 Vue.config.productionTip = false
 
 new Vue({
