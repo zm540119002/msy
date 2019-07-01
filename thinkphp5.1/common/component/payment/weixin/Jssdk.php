5dabd914967c760d97db99261b210ae9ef4b3301
yangguanbao <yangguanbao@qq.com>
2019-07-01 10:28:40 +0800

    'ygb'

--- thinkphp5.1/common/component/payment/weixin/Jssdk.php
+++ thinkphp5.1/common/component/payment/weixin/Jssdk.php
@@ -188,7 +188,6 @@ class Jssdk {
   public function getOauthUserInfo()
   {
     $data = $this -> getAccessTokenAndOpenid();
-    print_r($data);exit;
     $access_token = $data['access_token'];
     $openid =  $data['openid'];
     $url = "https://api.weixin.qq.com/sns/userinfo?access_token=".$access_token."&openid=".$openid."&lang=zh_CN";
