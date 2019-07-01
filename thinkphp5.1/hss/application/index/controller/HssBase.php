8a69a539d2bc21d8d1d1b51e20c5817665a2a038
yangguanbao <yangguanbao@qq.com>
2019-07-01 10:11:44 +0800

    'ygb'

--- thinkphp5.1/hss/application/index/controller/HssBase.php
+++ thinkphp5.1/hss/application/index/controller/HssBase.php
@@ -9,6 +9,7 @@ class HssBase extends \common\controller\Base{
         parent::__construct();
         //微信处理
         if(isWxBrowser() && !request()->isAjax()) {//判断是否为微信浏览器
+            print_r(123) ;
             $weixinUserInfo =  session('weixinUserInfo');
             if(!$weixinUserInfo){
                 $weixnTools = new \common\component\payment\weixin\Jssdk(config('wx_config.appid'), config('wx_config.appsecret'));
