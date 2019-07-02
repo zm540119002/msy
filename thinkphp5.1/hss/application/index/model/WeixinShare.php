01d1793d2c4e5cb025a2344419337a0575303bc9
yangguanbao <yangguanbao@qq.com>
2019-07-02 14:06:44 +0800

    'ygb'

--- thinkphp5.1/hss/application/index/model/WeixinShare.php
+++ thinkphp5.1/hss/application/index/model/WeixinShare.php
@@ -18,7 +18,22 @@ class WeixinShare extends \common\model\Base {
     public function getShareInfo()
     {
         $link = strtolower(request()->module() . '/' . request()->controller() . '/' . request()->action());
-
-
+        $config = [
+            'where'=>[
+                ['status','=',0],
+                ['link','=',$link],
+            ],'field'=>[
+                'id','link','desc','title','thumb_img'
+            ]
+        ];
+        $info = $this -> getInfo($config);
+        $shareInfo = [
+            'title'=>$info['title'], //分享的标题
+            'shareLink'=>$this->host.'/'.$info['link'], //分享的url
+            'desc'=> $info['desc'], //分享的描述
+            'shareImgUrl'=>$this->host.'/'.config('upload_dir.upload_path').'/'.$info['top']['thumb_img'], //分享的图片
+            'backUrl'=>$this->host.'/'.$info['link'] //分享完成后跳转的url
+        ];
+         return $shareInfo;
 	}
 }
