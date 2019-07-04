7b452d4011d49e67984fc61c279a0e2b6ef518a6
zm <540119002@qq.com>
2019-07-04 09:17:39 +0800

    zm

--- msy/src/views/Factory.vue
+++ msy/src/views/Factory.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <div>供应商</div>
+    <div>     
+      <van-button type="primary" @click="showPopup"> 登录</van-button>
+      <van-popup class="aaa" v-model="show">
+        手机号<input type="text">
+        密码<input type="password">
+      </van-popup>
+    </div>
     <Tabbar msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -11,9 +17,24 @@ export default {
   name: 'home',
   components: {
     Tabbar
+  },
+ data() {
+    return {
+      show: false
+    }
+  },
+
+  methods: {
+    showPopup() {
+      this.show = true;
+    }
   }
+ 
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.aaa{
+  width: 80%;
+}
 </style>
