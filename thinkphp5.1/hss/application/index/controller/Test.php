5bb456bae51f313dbdb170d6d31a9fa2b9dde910
yangguanbao <yangguanbao@qq.com>
2019-07-05 09:39:40 +0800

    'ygb'

--- thinkphp5.1/hss/application/index/controller/Test.php
+++ thinkphp5.1/hss/application/index/controller/Test.php
@@ -3,6 +3,7 @@ namespace app\index\controller;
 
 // 前台首页
 use think\Console;
+use common\component\jwt\JWT;
 
 class Test extends HssBase{
     /**首页
@@ -136,44 +137,33 @@ class Test extends HssBase{
 
     }
 
-    public function weixin()
+
+    public function getToken()
     {
-        $mineTools = new \common\component\payment\weixin\Jssdk(config('wx_config.appid'), config('wx_config.appsecret'));
-        $info = $mineTools->getUserInfo();
-
-        $municipalities = array("北京", "上海", "天津", "重庆", "香港", "澳门");
-        $sexes = array("", "男", "女");
-        $data = array();
-        $data['openid'] = $info['openid'];
-        $data['nickname'] = str_replace("'", "", $info['nickname']);
-        $data['sex'] = $sexes[$info['sex']];
-        $data['country'] = $info['country'];
-        $data['province'] = $info['province'];
-        $data['city'] = (in_array($info['province'], $municipalities))?$info['province'] : $info['city'];
-        //$data['scene'] = (isset($object->EventKey) && (stripos(strval($object->EventKey),"qrscene_")))?str_replace("qrscene_","",$object->EventKey):"0";
-
-        $data['headimgurl'] = $info['headimgurl'];
-        $data['subscribe'] = $info['subscribe'];
-        $data['subscribe_time'] = $info['subscribe_time'];
-        $data['heartbeat'] = time();
-        $data['remark'] = $info['remark'];
-        $content = "欢迎关注，".$info['nickname'];
-        $userModel = new \app\index\model\WeixinUser();
-        $config = [
-            'where'=>[
-                ['openid','=',$info['openid']]
-            ],'field'=>[
-                'id','subscribe'
-            ]
+        $key = "huang";  //这里是自定义的一个随机字串，应该写在config文件中的，解密时也会用，相当    于加密中常用的 盐  salt
+        $token = [
+            "iss" => "",  //签发者 可以为空
+            "aud" => "", //面象的用户，可以为空
+            "iat" => time(), //签发时间
+            "nbf" => time() + 100, //在什么时候jwt开始生效  （这里表示生成100秒后才生效）
+            "exp" => time() + 7200, //token 过期时间
+            "uid" => 123 //记录的userid的信息，这里是自已添加上去的，如果有其它信息，可以再添加数组的键值对
         ];
-        $weixinUserInfo = $userModel->getInfo($config);
-        if($weixinUserInfo && !$weixinUserInfo['subscribe']){
-            $data['id'] = $weixinUserInfo['id'];
-        }
-        $userModel->edit($data);
-        echo $userModel->getLastSql();
+
+        $jwt = JWT::encode($token, $key, "HS256"); //根据参数生成了 token
+        return json([
+            "token" => $jwt
+        ]);
     }
 
+
+    public function check()
+    {
+        $jwt = input("token");  //上一步中返回给用户的token
+        $key = "huang";  //上一个方法中的 $key 本应该配置在 config文件中的
+        $info = JWT::decode($jwt, $key, ["HS256"]); //解密jwt
+        return json($info);
+    }
 
 
 }
