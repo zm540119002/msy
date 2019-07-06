6aa571920d6d6d01e6a552a0d88b301ba49a3152
yangguanbao <yangguanbao@qq.com>
2019-07-06 10:26:05 +0800

    'ygb'

--- thinkphp5.1/hss/application/index_admin/controller/SignUp.php
+++ thinkphp5.1/hss/application/index_admin/controller/SignUp.php
@@ -21,7 +21,29 @@ class SignUp extends Base {
         return $this->fetch('manage');
     }
 
+    /**
+     * 删除
+     */
+    public function del(){
+        if(!request()->isPost()){
+            return config('custom.not_post');
+        }
 
+        if(!input('?post.id')&&!input('?post.ids'))  return errorMsg('失败');
+
+        $condition = array();
+
+        if($id = input('post.id/d')){
+            $condition = [['id','=',$id]];
+        }
+        if($ids = input('post.ids/a')){
+            $condition = [['id','in',$ids]];
+        }
+
+        $result= $this->obj->del($condition);
+
+        return $result;
+    }
     /**
      *  分页查询
      */
@@ -43,80 +65,6 @@ class SignUp extends Base {
         return view('list_tpl');
     }
 
-    /**
-     * 关联促销方案
-     */
-    public function editProjectPromotion(){
 
-        if(request()->isPost()){
-
-            $id = input('id/d');
-            $promotion_ids  = input('promotion_ids/a');
-
-            if (!$id){
-                $this ->error('参数有误',url('manage'));
-            }
-
-            if ($promotion_ids){
-                foreach($promotion_ids as $k => $v){
-                    if ((int)$v){
-                        $data = ['project_id'=>$id,'promotion_id'=>$v];
-
-                        // 先删后增 -保证唯一
-                        $model = new \app\index_admin\model\ProjectPromotion();
-                        $model -> where($data)->delete();
-                        $model -> allowField(true) -> save($data);
-                    }
-                }
-            }
-            return successMsg('成功');
-
-        }else{
-
-            // 后面的页面需要场景id
-            if(!$id = input('param.id/d')){
-                $this ->error('参数有误',url('manage'));
-            }
-
-            $this->assign('id',$id);
-            return $this->fetch();
-        }
-    }
-
-    /**
-     * 获取所有促销方案&&已选中的
-     * @return \think\response\View
-     */
-    public function _getPromotionList(){
-        $list = Promotion::getListData();
-
-        // 其它业务 -标记已选中的
-        if($id = input('param.id/d')){
-            $Model= new \app\index_admin\model\ProjectPromotion();
-            $condition = [
-                'where' => [
-                    ['project_id','=', $id],
-                ],'field'=> [
-                    'promotion_id'
-                ]
-            ];
-            $promotionList = $Model->getlist($condition);
-
-            if ($promotionList){
-                $promotionIds = array_column($promotionList,'promotion_id');
-                // 取出交差值的数组
-                foreach($list as $k => $v){
-                    if ( in_array($v['id'],$promotionIds) ){
-                        $list[$k]['exist'] = 1;
-                    }
-                }
-            }
-        }
-
-        $this->assign('list',$list);
-
-        return view('/promotion/list_promotion_tpl');
-
-    }
 
 }
