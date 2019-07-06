01546fed9219ed23d02b5ce4b0540b2537156a63
yangguanbao <yangguanbao@qq.com>
2019-07-06 10:39:27 +0800

    'ygb'

--- thinkphp5.1/hss/application/index_admin/controller/SignUp.php
+++ thinkphp5.1/hss/application/index_admin/controller/SignUp.php
@@ -58,10 +58,7 @@ class SignUp extends Base {
 
         $keyword = input('get.keyword/s');
         if($keyword) $condition['where'] = ['mobile','like', '%' . trim($keyword) . '%'];
-        p($condition);
         $list = $this->obj->pageQuery($condition);
-
-        p($this->obj->getLastSql);
         $this->assign('list',$list);
 
         return view('list_tpl');
