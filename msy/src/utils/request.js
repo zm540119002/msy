e4d8ab1016b0de557a04c1ff073d1282b565911d
zhangmin <540119002@qq.com>
2019-07-09 15:37:50 +0800

    zm

---
+++ msy/src/utils/request.js
@@ -0,0 +1,44 @@
+import axios from '../../node_modules/axios'
+import { Message } from '../../node_modules/element-ui'
+
+// import store from '../../node_modules/@/store'
+// import { getStore } from '../../node_modules/@/utils'
+
+const baseURL = process.env.NODE_ENV === 'development' ? '/apis' : process.env.BASE_API
+const service = axios.create({
+  baseURL,
+  timeout: 15 * 1000
+})
+
+service.interceptors.request.use(
+  config => {
+    // 在发送请求之前做些什么
+    // if (store.state.user.token) {
+    //   // 让每个请求携带token-- ['token']为自定义key
+    //   config.headers.Authorization = getStore('token')
+    //   config.headers.Token = getStore('token')
+    // }
+    return config
+  },
+  error => {
+    console.log(error) // for debug
+    Promise.reject(error)
+  }
+)
+// 拦截响应
+service.interceptors.response.use(
+  response => response,
+  error => {
+    console.log('err' + error) // for debug
+    Message({
+      message: '请求异常',
+      type: 'error'
+    })
+    return Promise.reject(error)
+  }
+)
+
+service.all = axios.all
+service.spread = axios.spread
+
+export default service
