2fd2bdccde65c8e8b092dd9261f01aa0c944d070
zhangmin <540119002@qq.com>
2019-07-09 17:25:16 +0800

    zm

--- thinkphp5.1/common/controller/Base.php
+++ thinkphp5.1/common/controller/Base.php
@@ -8,8 +8,9 @@ class Base extends \think\Controller{
     protected $host = null;
     public function __construct(){
         parent::__construct();
-
-
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: token,Origin, X-Requested-With, Content-Type, Accept");
+        header('Access-Control-Allow-Methods: POST,GET');
         //登录验证后跳转回原验证发起页
         $this->http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])
                 && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
