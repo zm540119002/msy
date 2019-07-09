c40cdbe6948b8da95ccad23913f3a7d99c06871f
zhangmin <540119002@qq.com>
2019-07-09 17:23:06 +0800

    zm

--- thinkphp5.1/hss/application/index/controller/Test.php
+++ thinkphp5.1/hss/application/index/controller/Test.php
@@ -12,11 +12,9 @@ class Test extends HssBase{
         header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Headers: token,Origin, X-Requested-With, Content-Type, Accept");
         header('Access-Control-Allow-Methods: POST,GET');
-
-        $a =  strtolower(request()->module() . '/' . request()->controller() . '/' . request()->action());
         if(request()->isAjax()){
 
-            return $_POST;
+            return '123';
         }else{
             return $this->fetch();
         }
