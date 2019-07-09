d26dd448aa834346f3184071dfd66ce54e107008
zhangmin <540119002@qq.com>
2019-07-09 17:42:35 +0800

    zm

--- thinkphp5.1/common/controller/UserBase.php
+++ thinkphp5.1/common/controller/UserBase.php
@@ -19,41 +19,5 @@ class UserBase extends Base{
                 exit;
             }
         }
-        //微信处理
-//        if(isWxBrowser() && !request()->isAjax()) {//判断是否为微信浏览器
-//            $weiXinUserInfo =  session('weiXinUserInfo');
-//            if(!$weiXinUserInfo){
-//                $mineTools = new \common\component\payment\weixin\Jssdk(config('wx_config.appid'), config('wx_config.appsecret'));
-//                $weiXinUserInfo = $mineTools->getOauthUserInfo();
-//                session('weiXinUserInfo',$weiXinUserInfo);
-//            }
-//            $user = checkLogin();
-//            if((!$user['name'] || !$user['avatar']) && $user && isset($weiXinUserInfo['openid'])){
-//                //临时相对路径
-//                $relativeSavePath = config('upload_dir.user_avatar');
-//                $weiXinAvatarUrl = $weiXinUserInfo['headimgurl'];
-//                $avatar = saveImageFromHttp($weiXinAvatarUrl,$relativeSavePath);
-//                $data = [
-//                    'id'=>$user['id'],
-//                    'name'=>$weiXinUserInfo['nickname'],
-//                    'avatar'=>$avatar,
-//                ];
-//                if($user['avatar']){
-//                    unset($data['avatar']);
-//                }else{
-//                    $user['avatar'] = $data['avatar'];
-//                }
-//                if($user['name']){
-//                    unset($data['name']);
-//                }else{
-//                    $user['name'] = $data['name'];
-//                }
-//                $userModel = new \common\model\User();
-//                $result = $userModel->isUpdate(true)->save($data);
-//                setSession($user);
-//            }
-//
-//        }
-
     }
 }
