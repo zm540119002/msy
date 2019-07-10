34f97bb95810a0261bbbe5ef59ea52c2482e6e3f
zhangmin <540119002@qq.com>
2019-07-10 11:35:10 +0800

    zm

--- thinkphp5.1/hss/application/index/controller/Test.php
+++ thinkphp5.1/hss/application/index/controller/Test.php
@@ -10,6 +10,7 @@ class Test extends HssBase{
      */
     public function index(){
         $postData = input('post.');
+        print_r($postData);
         if(request()->isPost()){
             return json_encode($postData);
         }else{
