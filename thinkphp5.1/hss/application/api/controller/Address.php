975b3245d9f03fc2194f09bc2361f639938bdc52
yangguanbao <yangguanbao@qq.com>
2019-07-10 11:53:59 +0800

    'ygb'

--- thinkphp5.1/hss/application/api/controller/Address.php
+++ thinkphp5.1/hss/application/api/controller/Address.php
@@ -9,7 +9,7 @@ class Address extends \common\controller\Base {
         $model = new \common\model\Address();
         $userId = 16;
         $data = input('post.');
-        return $data;
+        return json_encode($data);
         if(input('?post.id') && !empty(input('post.id')) ){
             //开启事务
             $model -> startTrans();
